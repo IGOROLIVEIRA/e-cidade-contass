456823de01c1fa5d358469695c2f87e554db1eda
rodrigo <rodrigo@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2015-12-17 20:17:31 +0000

    Corrigido o SQL do Reg 10.

--- model/contabilidade/arquivos/sicom/mensal/balancete/2015/SicomArquivoBalancete.model.php
+++ model/contabilidade/arquivos/sicom/mensal/balancete/2015/SicomArquivoBalancete.model.php
@@ -218,7 +218,7 @@ class SicomArquivoBalancete extends SicomArquivoBase implements iPadArquivoBaseC
 						inner join conplanoreduz on c61_codcon = c60_codcon and c61_anousu = c60_anousu and c61_instit = " . db_getsession("DB_instit") . "
                         inner join conplanoexe on c62_reduz = c61_reduz and c61_anousu = c62_anousu
                         left join vinculopcasptce on substr(c60_estrut,1,9) = c209_pcaspestrut
-                             where c60_anousu = " . db_getsession("DB_anousu") . " and c60_nregobrig = 14) as x
+                             where c60_anousu = " . db_getsession("DB_anousu") . " ) as x
                         where debito != 0 or credito != 0 or saldoinicialano != 0 order by contacontabil";
 //where c60_anousu = " . db_getsession("DB_anousu") . " and substr(c60_estrut,1,9) = '622130300') as x
         $rsReg10 = db_query($sqlReg10) or die($sqlReg10);
