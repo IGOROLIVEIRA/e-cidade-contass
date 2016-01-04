dc201ff06c1dccbd04da0774def36474efa4ceef
marcelo <marcelo@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2016-01-04 15:47:49 +0000

    ajuste menu fornecedores

--- updatedb/script_alteramenusicom.sql
+++ updatedb/script_alteramenusicom.sql
@@ -1,5 +1,7 @@
 BEGIN;
 
-update db_itensmenu set descricao = 'Cadastros', help = 'Cadastros' where id_item = 3962;
+select fc_startsession();
+
+update db_itensmenu set descricao = 'Fornecedores', help = 'Fornecedores' where id_item = 3962;
 
 COMMIT;
