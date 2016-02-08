7cd206d302bf9ade06407925eb85686af8312c9d
marcelo <marcelo@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2016-02-08 14:41:47 +0000

    nova tela sicom

---
+++ upload_leis_lcf.php
@@ -0,0 +1,28 @@
+<?php
+
+$sNomeCampo = $_GET['nome_campo'];
+$iAnoReferencia = $_GET['ano_usu'];
+
+if($sNomeCampo == "LAO"){
+	    $sNomeArquivo = "LAO_{$iAnoReferencia[2]}{$iAnoReferencia[3]}.pdf";
+}
+elseif($sNomeCampo == "LAOP"){
+		$sNomeArquivo = "LAOP_{$iAnoReferencia[2]}{$iAnoReferencia[3]}.pdf";
+}
+elseif($sNomeCampo == "DEC"){
+		$sNomeArquivo = "DEC_{$iAnoReferencia[2]}{$iAnoReferencia[3]}.pdf";
+}
+
+
+if (strtolower(end(explode('.', $_FILES["$sNomeCampo"]['name']))) != "pdf") {
+	echo "<div style=\"color: red;\">Envie arquivos somente com extensão .pdf</div>";
+}else{
+	if (move_uploaded_file($_FILES["$sNomeCampo"]['tmp_name'], "$sNomeArquivo")) {
+		echo "<div style=\"color: blue;\">Arquivo enviado com sucesso!</div>";
+	} else {
+  	echo "<div style=\"color: blue;\">Não foi possível enviar o arquivo, tente novamente</div>";
+	}
+}
+
+
+?>
