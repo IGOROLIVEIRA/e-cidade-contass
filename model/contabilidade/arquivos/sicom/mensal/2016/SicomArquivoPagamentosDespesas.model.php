994f8e391bd4c416aed839cb7b1b253ab479429c
robson <robson@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2016-04-27 11:30:31 +0000

    corrigido problema no sql para pegar a conta do ctb

--- model/contabilidade/arquivos/sicom/mensal/2016/SicomArquivoPagamentosDespesas.model.php
+++ model/contabilidade/arquivos/sicom/mensal/2016/SicomArquivoPagamentosDespesas.model.php
@@ -900,7 +900,7 @@ class SicomArquivoPagamentosDespesas extends SicomArquivoBase implements iPadArq
 											si95_tipoconta::int8 = (case when c63_tipoconta in (2,3) then 2 else 1 end) join ctb202014 on si96_codctb = si95_codctb and si96_mes = si95_mes
 											        where  si95_instit =  ".db_getsession("DB_instit")." and c82_codlan =  {$oEmpPago->lancamento} and c61_anousu = ".db_getsession("DB_anousu")." 
 											        and si95_mes <=".$this->sDataFinal['5'].$this->sDataFinal['6'];
-			        	$sSqlContaPagFont .= "UNION select * from (select distinct si95_codctb  as contapag, o15_codtri as fonte from conplanoconta
+			        	$sSqlContaPagFont .= "UNION select distinct si95_codctb  as contapag, o15_codtri as fonte from conplanoconta
 											join conplanoreduz on c61_codcon = c63_codcon and c61_anousu = c63_anousu
 											join orctiporec on c61_codigo = o15_codigo
 											join conlancampag on  c82_reduz = c61_reduz and c82_anousu = c61_anousu
