62772fcb02d8cb7f1a10b6eeed192cd82fadf466
marcelo <marcelo@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2016-05-16 19:31:23 +0000

    ajuste sicom RH

--- model/contabilidade/arquivos/sicom/mensal/flpg/2013/SicomArquivoPessoa.model.php
+++ model/contabilidade/arquivos/sicom/mensal/flpg/2013/SicomArquivoPessoa.model.php
@@ -77,7 +77,11 @@ class SicomArquivoPessoa extends SicomArquivoBase implements iPadArquivoBaseCSV 
       $sSql  = "select distinct case when length(z01_cgccpf) < 11 then lpad(z01_cgccpf, 11, '0') else z01_cgccpf end as z01_cgccpf,
 					       z01_nome,
 					       z01_sexo,
-					       z01_nasc,
+					       case
+                           when z01_nasc is not null then z01_nasc
+                           else rh01_nasc
+                           end as z01_nasc,
+
 					       z01_ultalt, 
 					       z01_obs,
 					       z01_cadast 
@@ -111,7 +115,10 @@ class SicomArquivoPessoa extends SicomArquivoBase implements iPadArquivoBaseCSV 
       $sSql  = "select z01_cgccpf,
 		       z01_nome,
 		       z01_sexo,
-               z01_nasc,
+               case
+               when z01_nasc is not null then z01_nasc
+               else rh01_nasc
+               end as z01_nasc,
 		       z01_ultalt, 
 		       z01_obs,
 		       z01_cadast 
