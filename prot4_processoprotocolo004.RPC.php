91bf3e6a6f5ef8b7c83f0e7eb89f829e8ac426e1
rodrigo <rodrigo@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2016-07-05 21:37:07 +0000

    Ocorrência 1870 - Correção em histórico de Processos Apensados

--- prot4_processoprotocolo004.RPC.php
+++ prot4_processoprotocolo004.RPC.php
@@ -24,7 +24,6 @@
  *  Copia da licenca no diretorio licenca/licenca_en.txt
  *                                licenca/licenca_pt.txt
  */
-
 require_once("libs/db_stdlib.php");
 require_once("libs/db_utils.php");
 require_once("libs/db_conecta.php");
@@ -34,79 +33,109 @@ require_once("libs/db_app.utils.php");
 require_once("libs/JSON.php");
 require_once("std/db_stdClass.php");
 
-$oJson  = new services_json();
+$oJson = new services_json();
 $oParametro = $oJson->decode(str_replace("\\", "", $_POST['json']));
 
-$oRetorno            = new stdClass();
-$oRetorno->lErro     = false;
+$oRetorno = new stdClass();
+$oRetorno->lErro = false;
 $oRetorno->sMensagem = "";
 
 try {
 
-  switch ($oParametro->exec) {
+    switch ($oParametro->exec) {
 
-    /**
-     * Salva os dados da regra em um documento
-     */
-    case "getDadosProcessoProtocolo":
+        /**
+         * Salva os dados da regra em um documento
+         */
+        case "getDadosProcessoProtocolo":
 
-      $iAnoSessao     = db_getsession("DB_anousu");
-      $aDadosProcesso = explode("/", $oParametro->sNumeroProcesso);
+            $iAnoSessao = db_getsession("DB_anousu");
+            $aDadosProcesso = explode("/", $oParametro->sNumeroProcesso);
 
-      if (count($aDadosProcesso) == 2) {
-        $iAnoSessao = $aDadosProcesso[1];
-      }
-      $oInstituicao       = InstituicaoRepository::getInstituicaoByCodigo(db_getsession('DB_instit'));
-      $oProcessoProtocolo = processoProtocolo::getInstanciaPorNumeroEAno($aDadosProcesso[0], $iAnoSessao, $oInstituicao);
+            if (count($aDadosProcesso) == 2) {
+                $iAnoSessao = $aDadosProcesso[1];
+            }
+            $oInstituicao = InstituicaoRepository::getInstituicaoByCodigo(db_getsession('DB_instit'));
+            $oProcessoProtocolo = processoProtocolo::getInstanciaPorNumeroEAno($aDadosProcesso[0], $iAnoSessao, $oInstituicao);
 
-      if ( !$oProcessoProtocolo ) {
-        throw new Exception("Processo de protocolo ({$aDadosProcesso[0]}/{$iAnoSessao}) não encontrado.");
-      }
+            if (!$oProcessoProtocolo) {
+                throw new Exception("Processo de protocolo ({$aDadosProcesso[0]}/{$iAnoSessao}) não encontrado.");
+            }
 
-      $oRetorno->iSequencialProcesso = $oProcessoProtocolo->getCodProcesso();
-      $oRetorno->iNumeroProcesso     = $aDadosProcesso[0];
-      $oRetorno->iAnoProcesso        = $iAnoSessao;
-      $oRetorno->sRequerenteProcesso = urlencode($oProcessoProtocolo->getRequerente());
+            $oRetorno->iSequencialProcesso = $oProcessoProtocolo->getCodProcesso();
+            $oRetorno->iNumeroProcesso = $aDadosProcesso[0];
+            $oRetorno->iAnoProcesso = $iAnoSessao;
+            $oRetorno->sRequerenteProcesso = urlencode($oProcessoProtocolo->getRequerente());
 
-    break;
+            break;
 
-    case 'getMovimentacoesProcesso' :
+        case 'getMovimentacoesProcesso' :
 
-      require_once 'model/protocolo/RefactorConsultaProcessoProtocolo.model.php';
+            require_once 'model/protocolo/RefactorConsultaProcessoProtocolo.model.php';
+            $aDadposApensado = apensado($oParametro->iCodigoProcesso);
+            $aCodigosProcessos = array($oParametro->iCodigoProcesso, $aDadposApensado[0]['p30_procprincipal']);
+            $aMovimentacoes = array();
+            if (count($aCodigosProcessos) > 1) {
+                foreach ($aCodigosProcessos as $codigo) {
+                    $oRefactorProcessoProtocolo = new RefactorConsultaProcessoProtocolo($codigo);
+                    array_push($aMovimentacoes, $oRefactorProcessoProtocolo->getMovimentacoes());
+                }
+            } else {
+                $oRefactorProcessoProtocolo = new RefactorConsultaProcessoProtocolo($oParametro->iCodigoProcesso);
+                $aMovimentacoes = $oRefactorProcessoProtocolo->getMovimentacoes();
+            }
+            $oRetorno->aMovimentacoes = array();
 
-      $oRefactorProcessoProtocolo = new RefactorConsultaProcessoProtocolo($oParametro->iCodigoProcesso);
-      $aMovimentacoes = $oRefactorProcessoProtocolo->getMovimentacoes();
-      $oRetorno->aMovimentacoes = array();
+            /**
+             * Passa urlEncode() em todas as propriedades dos movimentos
+             */
+            if (count($aMovimentacoes) > 0) {
+                for ($i = 0; $i < count($aMovimentacoes); $i++) {
+                    foreach ($aMovimentacoes[$i] as $oDadosMovimentacao) {
+                        if ($i > 0) {
+                            if(date("Y-m-d",strtotime(implode('-', array_reverse(explode('/', $oDadosMovimentacao->sData))))) < date("Y-m-d",strtotime($aDadposApensado[0]['data_processo']))){
+                                continue;
+                            }
+                        }
+                        foreach ($oDadosMovimentacao as $sPropridade => $sValor) {
+                            $oDadosMovimentacao->$sPropridade = urlEncode($sValor);
+                        }
+                        $oRetorno->aMovimentacoes[] = $oDadosMovimentacao;
+                    }
+                }
+            }
 
-      /**
-       * Passa urlEncode() em todas as propriedades dos movimentos
-       */
-      if (count($aMovimentacoes) > 0) {
+            break;
 
-        foreach ($aMovimentacoes as $oDadosMovimentacao) {
+        default :
+            throw new Exception("Parâmetro inválido");
+            break;
 
-          foreach ( $oDadosMovimentacao as $sPropridade => $sValor ) {
-            $oDadosMovimentacao->$sPropridade = urlEncode($sValor);
-          }
-
-          $oRetorno->aMovimentacoes[] = $oDadosMovimentacao;
-        }
-      }
-
-    break;
-
-    default :
-      throw new Exception("Parâmetro inválido");
-    break;
-
-  }
+    }
 
 } catch (Exception $oErro) {
 
-  db_fim_transacao(true);
-  $oRetorno->lErro     = true;
-  $oRetorno->sMensagem = $oErro->getMessage();
+    db_fim_transacao(true);
+    $oRetorno->lErro = true;
+    $oRetorno->sMensagem = $oErro->getMessage();
 }
 
 $oRetorno->sMensagem = urlencode($oRetorno->sMensagem);
 echo $oJson->encode($oRetorno);
+
+/**
+ * Verifica se o processo está apensado em outro.
+ * @param $processo
+ * @return bool
+ */
+
+function apensado($processo)
+{
+    $oDaoProcessosApensados = db_utils::getDao('processosapensados');
+    $sCampos = "p30_procprincipal,p58_dtproc as data_processo, cast(p58_numero||'/'||p58_ano as varchar) as codigo_processo, z01_nome as titular,";
+    $sCampos .= 'p51_descr as tipo_processo';
+    $sWhere = "p30_procapensado = {$processo}";
+    $sSqlProcessosApensados = $oDaoProcessosApensados->sql_query_processo_apensado(null, $sCampos, "p58_codproc", $sWhere);
+    $rsProcessosApensados = $oDaoProcessosApensados->sql_record($sSqlProcessosApensados);
+    return pg_fetch_all($rsProcessosApensados);
+}
