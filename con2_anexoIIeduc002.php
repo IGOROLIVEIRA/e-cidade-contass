63696e1bc438c8b11af712f76ac4206f93dde59f
gabriel <gabriel@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2016-11-04 19:53:11 +0000

    HTML/PDF Relatório Anexo II

---
+++ con2_anexoIIeduc002.php
@@ -0,0 +1,340 @@
+<?
+/*
+ *     E-cidade Software Publico para Gestao Municipal                
+ *  Copyright (C) 2014  DBSeller Servicos de Informatica
+ *                            www.dbseller.com.br
+ *                         e-cidade@dbseller.com.br
+ *
+ *  Este programa e software livre; voce pode redistribui-lo e/ou
+ *  modifica-lo sob os termos da Licenca Publica Geral GNU, conforme
+ *  publicada pela Free Software Foundation; tanto a versao 2 da
+ *  Licenca como (a seu criterio) qualquer versao mais nova.
+ *
+ *  Este programa e distribuido na expectativa de ser util, mas SEM
+ *  QUALQUER GARANTIA; sem mesmo a garantia implicita de
+ *  COMERCIALIZACAO ou de ADEQUACAO A QUALQUER PROPOSITO EM
+ *  PARTICULAR. Consulte a Licenca Publica Geral GNU para obter mais
+ *  detalhes.
+ *
+ *  Voce deve ter recebido uma copia da Licenca Publica Geral GNU
+ *  junto com este programa; se nao, escreva para a Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307, USA.
+ *
+ *  Copia da licenca no diretorio licenca/licenca_en.txt
+ *                                licenca/licenca_pt.txt
+ */
+
+include("vendor/mpdf/mpdf/mpdf.php");
+include("libs/db_liborcamento.php");
+include("libs/db_libcontabilidade.php");
+include("libs/db_sql.php");
+
+/**
+ * mPDF
+ * @param string $mode              | padrão: BLANK
+ * @param mixed $format             | padrão: A4
+ * @param float $default_font_size  | padrão: 0
+ * @param string $default_font      | padrão: ''
+ * @param float $margin_left        | padrão: 15
+ * @param float $margin_right       | padrão: 15
+ * @param float $margin_top         | padrão: 16
+ * @param float $margin_bottom      | padrão: 16
+ * @param float $margin_header      | padrão: 9
+ * @param float $margin_footer      | padrão: 9
+ *
+ * Nenhum dos parâmetros é obrigatório
+ */
+
+$mPDF = new mpdf('', '', 0, '', 15, 15, 20, 15, 5, 11);
+
+
+$header = <<<HEADER
+<header>
+  <table style="width:100%;text-align:center;font-family:sans-serif;border-bottom:1px solid #000;padding-bottom:6px;">
+    <tr>
+      <th>PREFEITURA MUNICIPAL DE BOTUMIRIM</th>
+    </tr>
+    <tr>
+      <th>ANEXO II</th>
+    </tr>
+    <tr>
+      <td style="text-align:right;font-size:10px;font-style:oblique;">Período: De 01/01/2016 a 30/06/2016</td>
+    </tr>
+  </table>
+</header>
+HEADER;
+
+$footer = <<<FOOTER
+<div style='border-top:1px solid #000;width:100%;text-align:right;font-family:sans-serif;font-size:10px;height:10px;'>
+  {PAGENO}
+</div>
+FOOTER;
+
+
+$mPDF->WriteHTML(file_get_contents('estilos/tab_relatorio.css'), 1);
+$mPDF->setHTMLHeader(utf8_encode($header), 'O', true);
+$mPDF->setHTMLFooter(utf8_encode($footer), 'O', true);
+
+ob_start();
+
+?>
+  
+  <html>
+  <head>
+    
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <style type="text/css">
+      .ritz .waffle a { color : inherit; }
+      .ritz .waffle .s3 { background-color : #ffffff; border-right : 1px SOLID #000000; color : #000000; direction : ltr; font-family : 'Calibri',Arial; font-size : 10pt; padding : 2px 3px 2px 3px; text-align : left; vertical-align : bottom; white-space : nowrap; }
+      .ritz .waffle .s6 { background-color : #ffffff; border-bottom : 1px SOLID #000000; border-right : 1px SOLID #000000; color : #000000; direction : ltr; font-family : 'Calibri',Arial; font-size : 10pt; padding : 2px 3px 2px 3px; text-align : left; vertical-align : bottom; white-space : nowrap; }
+      .ritz .waffle .s4 { background-color : #ffffff; border-right : 1px SOLID #000000; color : #000000; direction : ltr; font-family : 'Calibri',Arial; font-size : 11pt; font-weight : bold; overflow : hidden; padding : 2px 3px 2px 3px; text-align : center; vertical-align : bottom; white-space : normal; word-wrap : break-word; }
+      .ritz .waffle .s10 { background-color : #ffffff; border-bottom : 1px SOLID #000000; color : #000000; direction : ltr; font-family : 'Calibri',Arial; font-size : 10pt; padding : 2px 3px 2px 3px; text-align : left; vertical-align : bottom; white-space : nowrap; }
+      .ritz .waffle .s18 { background-color : #d8d8d8; border-bottom : 1px SOLID #000000; border-right : 1px SOLID #000000; color : #000000; direction : ltr; font-family : 'Calibri',Arial; font-size : 11pt; font-weight : bold; padding : 2px 3px 2px 3px; text-align : right; vertical-align : bottom; white-space : nowrap; }
+      .ritz .waffle .s15 { background-color : #ffffff; border-bottom : 1px SOLID #000000; border-right : 1px SOLID #000000; color : #000000; direction : ltr; font-family : 'Calibri',Arial; font-size : 11pt; padding : 2px 3px 2px 3px; text-align : right; vertical-align : bottom; white-space : nowrap; }
+      .ritz .waffle .s2 { background-color : #ffffff; border-bottom : 1px SOLID #000000; color : #000000; direction : ltr; font-family : 'Calibri',Arial; font-size : 11pt; font-style : italic; padding : 2px 3px 2px 3px; text-align : right; vertical-align : bottom; white-space : nowrap; }
+      .ritz .waffle .s9 { background-color : #ffffff; border-right : 1px SOLID #000000; color : #000000; direction : ltr; font-family : 'Calibri',Arial; font-size : 11pt; padding : 2px 3px 2px 3px; text-align : center; vertical-align : bottom; white-space : nowrap; }
+      .ritz .waffle .s13 { background-color : #ffffff; border-right : 1px SOLID #000000; color : #000000; direction : ltr; font-family : 'Calibri',Arial; font-size : 11pt; padding : 2px 3px 2px 3px; text-align : right; vertical-align : bottom; white-space : nowrap; }
+      .ritz .waffle .s11 { background-color : #ffffff; border-bottom : 1px SOLID #000000; border-right : 1px SOLID #000000; color : #000000; direction : ltr; font-family : 'Calibri',Arial; font-size : 11pt; padding : 2px 3px 2px 3px; text-align : center; vertical-align : bottom; white-space : nowrap; }
+      .ritz .waffle .s8 { background-color : #ffffff; color : #000000; direction : ltr; font-family : 'Calibri',Arial; font-size : 11pt; padding : 2px 3px 2px 3px; text-align : center; vertical-align : bottom; white-space : nowrap; }
+      .ritz .waffle .s19 { background-color : #ffffff; border-right : 1px SOLID #000000; color : #000000; direction : ltr; font-family : 'Helvetica Neue',Arial; font-size : 9pt; padding : 2px 3px 2px 3px; text-align : left; vertical-align : bottom; white-space : nowrap; }
+      .ritz .waffle .s1 { background-color : #ffffff; color : #000000; direction : ltr; font-family : 'Calibri',Arial; font-size : 12pt; font-weight : bold; padding : 2px 3px 2px 3px; text-align : center; vertical-align : bottom; white-space : nowrap; }
+      .ritz .waffle .s16 { background-color : #ffffff; border-bottom : 1px SOLID #000000; border-right : 1px SOLID #000000; color : #000000; direction : ltr; font-family : 'Helvetica Neue',Arial; font-size : 9pt; padding : 2px 3px 2px 3px; text-align : left; vertical-align : bottom; white-space : nowrap; }
+      .ritz .waffle .s17 { background-color : #d8d8d8; border-bottom : 1px SOLID #000000; border-right : 1px SOLID transparent; color : #000000; direction : ltr; font-family : 'Calibri',Arial; font-size : 11pt; font-weight : bold; padding : 2px 3px 2px 3px; text-align : left; vertical-align : bottom; white-space : nowrap; }
+      .ritz .waffle .s14 { background-color : #ffffff; border-bottom : 1px SOLID #000000; border-right : 1px SOLID #000000; color : #000000; direction : ltr; font-family : 'Calibri',Arial; font-size : 11pt; padding : 2px 3px 2px 3px; text-align : left; vertical-align : bottom; white-space : nowrap; }
+      .ritz .waffle .s5 { background-color : #ffffff; border-right : 1px SOLID #000000; color : #000000; direction : ltr; font-family : 'Calibri',Arial; font-size : 11pt; font-weight : bold; padding : 2px 3px 2px 3px; text-align : center; vertical-align : bottom; white-space : nowrap; }
+      .ritz .waffle .s0 { background-color : #ffffff; color : #000000; direction : ltr; font-family : 'Calibri',Arial; font-size : 14pt; font-weight : bold; padding : 2px 3px 2px 3px; text-align : center; vertical-align : bottom; white-space : nowrap; }
+      .ritz .waffle .s7 { background-color : #ffffff; border-right : 1px SOLID #000000; color : #000000; direction : ltr; font-family : 'Calibri',Arial; font-size : 10pt; padding : 2px 3px 2px 3px; text-align : center; vertical-align : bottom; white-space : nowrap; }
+      .ritz .waffle .s12 { background-color : #ffffff; border-right : 1px SOLID #000000; color : #000000; direction : ltr; font-family : 'Calibri',Arial; font-size : 11pt; padding : 2px 3px 2px 3px; text-align : left; vertical-align : bottom; white-space : nowrap; }
+    </style>
+  </head>
+  <body>
+  
+  <div class="ritz grid-container" dir="ltr">
+    <table class="waffle" cellspacing="0" cellpadding="0">
+      <thead>
+      <tr>
+        <th id="0C0" style="width:72px" class="column-headers-background">&nbsp;</th>
+        <th id="0C1" style="width:92px" class="column-headers-background">&nbsp;</th>
+        <th id="0C2" style="width:87px" class="column-headers-background">&nbsp;</th>
+        <th id="0C3" style="width:100px" class="column-headers-background">&nbsp;</th>
+        <th id="0C4" style="width:100px" class="column-headers-background">&nbsp;</th>
+        <th id="0C5" style="width:100px" class="column-headers-background">&nbsp;</th>
+        <th id="0C6" style="width:100px" class="column-headers-background">&nbsp;</th>
+        <th id="0C7" style="width:100px" class="column-headers-background">&nbsp;</th>
+        <th id="0C8" style="width:100px" class="column-headers-background">&nbsp;</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr style='height:20px;'>
+        <td class="s3 bdleft" colspan="9">&nbsp;</td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s4 bdleft" colspan="9">DEMONSTRATIVO DOS GASTOS COM A MANUTENÇÃO E DESENVOLVIMENTO DO<br>ENSINO</td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s6 bdleft" colspan="9">&nbsp;</td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s7 bdleft">Função</td>
+        <td class="s7">SubFunções</td>
+        <td class="s7">Programas</td>
+        <td class="s8" colspan="5">Especificação</td>
+        <td class="s9">Despesa (1)</td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s6 bdleft"></td>
+        <td class="s6"></td>
+        <td class="s6"></td>
+        <td class="s10" colspan="5"></td>
+        <td class="s11">R$</td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s9 bdleft">12</td>
+        <td class="s3"></td>
+        <td class="s3"></td>
+        <td class="s12" colspan="5">EDUCAÇÃO</td>
+        <td class="s3"></td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s3 bdleft"></td>
+        <td class="s9">122</td>
+        <td class="s3"></td>
+        <td class="s12" colspan="5">Administração Geral</td>
+        <td class="s3"></td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s3 bdleft"></td>
+        <td class="s3"></td>
+        <td class="s9">0002</td>
+        <td class="s12" colspan="5">AÇOES DE ADMINISTRAÇÃO PUBLICA</td>
+        <td class="s13">84.679,90 </td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s3 bdleft"></td>
+        <td class="s3"></td>
+        <td class="s9">0018</td>
+        <td class="s12" colspan="5">TREINAMENTO CAPACIT DE RECURSOS HUMANOS</td>
+        <td class="s13">0,00 </td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s3 bdleft">&nbsp;</td>
+        <td class="s3"></td>
+        <td class="s3"></td>
+        <td class="s3" colspan="5"></td>
+        <td class="s3"></td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s3 bdleft"></td>
+        <td class="s9">272</td>
+        <td class="s3"></td>
+        <td class="s12" colspan="5">Previdência do Regime Estatutário</td>
+        <td class="s3"></td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s3 bdleft"></td>
+        <td class="s3"></td>
+        <td class="s9">0002</td>
+        <td class="s12" colspan="5">AÇOES DE ADMINISTRAÇÃO PUBLICA</td>
+        <td class="s13">28.830,85 </td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s3 bdleft">&nbsp;</td>
+        <td class="s3"></td>
+        <td class="s3"></td>
+        <td class="s3" colspan="5"></td>
+        <td class="s3"></td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s3 bdleft"></td>
+        <td class="s9">361</td>
+        <td class="s3"></td>
+        <td class="s12" colspan="5">Ensino Fundamental</td>
+        <td class="s3"></td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s3 bdleft"></td>
+        <td class="s3"></td>
+        <td class="s9">0020 </td>
+        <td class="s12" colspan="5">ENSINO FUNDAMENTAL</td>
+        <td class="s13">62.928,09 </td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s3 bdleft"></td>
+        <td class="s3"></td>
+        <td class="s9">0021</td>
+        <td class="s12" colspan="5">TRANSPORTE ESCOLAR</td>
+        <td class="s13">19.098,47 </td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s3 bdleft">&nbsp;</td>
+        <td class="s3"></td>
+        <td class="s3"></td>
+        <td class="s3" colspan="5"></td>
+        <td class="s3"></td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s3 bdleft"></td>
+        <td class="s9">365</td>
+        <td class="s3"></td>
+        <td class="s12" colspan="5">Ensino Infantil</td>
+        <td class="s3"></td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s3 bdleft"></td>
+        <td class="s3"></td>
+        <td class="s9">0024</td>
+        <td class="s12" colspan="5">AÇÕES DO ENSINO INFANTIL</td>
+        <td class="s13">6.697,33 </td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s3 bdleft">&nbsp;</td>
+        <td class="s3"></td>
+        <td class="s3"></td>
+        <td class="s3" colspan="5"></td>
+        <td class="s3"></td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s3 bdleft"></td>
+        <td class="s9">366</td>
+        <td class="s3"></td>
+        <td class="s12" colspan="5">Educação de Jovens e Adultos</td>
+        <td class="s3"></td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s3 bdleft"></td>
+        <td class="s3"></td>
+        <td class="s9">0025</td>
+        <td class="s12" colspan="5">EDUCAÇÃO DE JOVENS E ADULTOS</td>
+        <td class="s13">0,00 </td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s3 bdleft">&nbsp;</td>
+        <td class="s3"></td>
+        <td class="s3"></td>
+        <td class="s3" colspan="5"></td>
+        <td class="s3"></td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s3 bdleft"></td>
+        <td class="s9">367</td>
+        <td class="s3"></td>
+        <td class="s12" colspan="5">Educação Especial</td>
+        <td class="s3"></td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s6 bdleft"></td>
+        <td class="s6"></td>
+        <td class="s11">0041</td>
+        <td class="s14" colspan="5">EDUCAÇÃO ESPECIAL</td>
+        <td class="s15">0,00 </td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s15 bdleft" colspan="8">SUBTOTAL</td>
+        <td class="s15">202.234,64 </td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s16 bdleft" colspan="8">Contribuição ao FUNDEB - art. 1º, Lei Federal n. 11 .494/07 (2)</td>
+        <td class="s15">1.004.695,85 </td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s16 bdleft" colspan="8">Restos a Pagar Não Processados de Exercícios Anteriores</td>
+        <td class="s3"></td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s16 bdleft" colspan="8">Processados no Exercício Atual (3)</td>
+        <td class="s15">240,52 </td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s17 bdleft" colspan="8">TOTAL</td>
+        <td class="s18">1.207.171,01 </td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s19 bdleft" colspan="9">(1) Art. 70 da Lei Federal n. 9394/96.</td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s19 bdleft" colspan="9">(2) O valor a ser demonstrado corresponderá à contribuição ao FUNDEB, contabilizado</td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s19 bdleft" colspan="9">como conta retificadora da receita.</td>
+      </tr>
+      <tr style='height:20px;'>
+        <td class="s16 bdleft" colspan="9">(3) Parágrafo Único do Artigo 6º da Instrução Normativa Nº 13/2008</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+  
+  
+  </body>
+  </html>
+
+<?php
+
+$html = ob_get_contents();
+ob_end_clean();
+
+$mPDF->WriteHTML(utf8_encode($html));
+$mPDF->Output();
+
+?>
