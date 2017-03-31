b5c952c918e979001ff86f27166e3fced45d54a7
rodrigo <rodrigo@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2017-03-31 20:03:11 +0000

    Controle de versoes

--- model/contabilidade/arquivos/sicom/mensal/2017/SicomArquivoItem.model.php
+++ model/contabilidade/arquivos/sicom/mensal/2017/SicomArquivoItem.model.php
@@ -195,13 +195,13 @@ WHERE DATE_PART ('MONTH' , si172_dataassinatura) = " . $this->sDataFinal['5'] . 
       $oDados10 = db_utils::fieldsMemory($rsResult10, $iCont10);
 
       //$sSqlitem="select si43_coditem,si43_unidademedida from item102017  where si43_coditem=".$oDados10->coditem." and si43_unidademedida='{$oDados10->unidademedida}'";
-      $sSqlitem = "select si43_coditem,si43_unidademedida from item102017  where si43_coditem=" . $oDados10->coditem . " and si43_mes <= " . $this->sDataFinal['5'] . $this->sDataFinal['6'];
+      $sSqlitem = "select si43_coditem,si43_unidademedida from item102017  where si43_instit = ".db_getsession('DB_instit')." and si43_coditem=" . $oDados10->coditem . " and si43_mes <= " . $this->sDataFinal['5'] . $this->sDataFinal['6'];
       $sSqlitem .= " union
-    	select si43_coditem,si43_unidademedida from item102016  where si43_coditem=" . $oDados10->coditem;
+    	select si43_coditem,si43_unidademedida from item102016  where si43_instit = ".db_getsession('DB_instit')." si43_coditem=" . $oDados10->coditem;
       $sSqlitem .= " union
-    	select si43_coditem,si43_unidademedida from item102015  where si43_coditem=" . $oDados10->coditem;
+    	select si43_coditem,si43_unidademedida from item102015  where si43_instit = ".db_getsession('DB_instit')." si43_coditem=" . $oDados10->coditem;
       $sSqlitem .= " union
-    	select si43_coditem,si43_unidademedida from item102014  where si43_coditem=" . $oDados10->coditem;
+    	select si43_coditem,si43_unidademedida from item102014  where si43_instit = ".db_getsession('DB_instit')." si43_coditem=" . $oDados10->coditem;
       $rsResultitem = db_query($sSqlitem);//db_criatabela($rsResultitem);echo $sSqlitem;
       /**
        * verifica se já nao existe o registro  na base de dados do sicom
