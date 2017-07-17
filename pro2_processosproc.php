f4c82969307f70e7959a8a154764ee53e9afcc02
gabriel <gabriel@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2017-07-17 11:53:45 +0000

    OC 3777 - Saída de relatório de processos em .csv

--- pro2_processosproc.php
+++ pro2_processosproc.php
@@ -1,28 +1,28 @@
 <?
 /*
- *     E-cidade Software Publico para Gestao Municipal                
- *  Copyright (C) 2013  DBselller Servicos de Informatica             
- *                            www.dbseller.com.br                     
- *                         e-cidade@dbseller.com.br                   
- *                                                                    
- *  Este programa e software livre; voce pode redistribui-lo e/ou     
- *  modifica-lo sob os termos da Licenca Publica Geral GNU, conforme  
- *  publicada pela Free Software Foundation; tanto a versao 2 da      
- *  Licenca como (a seu criterio) qualquer versao mais nova.          
- *                                                                    
- *  Este programa e distribuido na expectativa de ser util, mas SEM   
- *  QUALQUER GARANTIA; sem mesmo a garantia implicita de              
- *  COMERCIALIZACAO ou de ADEQUACAO A QUALQUER PROPOSITO EM           
- *  PARTICULAR. Consulte a Licenca Publica Geral GNU para obter mais  
- *  detalhes.                                                         
- *                                                                    
- *  Voce deve ter recebido uma copia da Licenca Publica Geral GNU     
- *  junto com este programa; se nao, escreva para a Free Software     
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA          
- *  02111-1307, USA.                                                  
- *  
- *  Copia da licenca no diretorio licenca/licenca_en.txt 
- *                                licenca/licenca_pt.txt 
+ *     E-cidade Software Publico para Gestao Municipal
+ *  Copyright (C) 2013  DBselller Servicos de Informatica
+ *                            www.dbseller.com.br
+ *                         e-cidade@dbseller.com.br
+ *
+ *  Este programa e software livre; voce pode redistribui-lo e/ou
+ *  modifica-lo sob os termos da Licenca Publica Geral GNU, conforme
+ *  publicada pela Free Software Foundation; tanto a versao 2 da
+ *  Licenca como (a seu criterio) qualquer versao mais nova.
+ *
+ *  Este programa e distribuido na expectativa de ser util, mas SEM
+ *  QUALQUER GARANTIA; sem mesmo a garantia implicita de
+ *  COMERCIALIZACAO ou de ADEQUACAO A QUALQUER PROPOSITO EM
+ *  PARTICULAR. Consulte a Licenca Publica Geral GNU para obter mais
+ *  detalhes.
+ *
+ *  Voce deve ter recebido uma copia da Licenca Publica Geral GNU
+ *  junto com este programa; se nao, escreva para a Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307, USA.
+ *
+ *  Copia da licenca no diretorio licenca/licenca_en.txt
+ *                                licenca/licenca_pt.txt
  */
 
 require("libs/db_stdlib.php");
@@ -183,8 +183,9 @@ $cl_protprocesso->rotulo->label();
 			</tr>
 			<tr>
 				<td align='center' align="center" nowrap colspan=3>
-					<input type="button" value="Relatório" onclick="js_self()">
-				</td>
+          <input type="button" value="Relatório" onclick="js_self('pdf')">
+					<input type="button" value="Relatório CSV" onclick="js_self('csv')">
+        </td>
 			</tr>
 		</table>
 	</form>
@@ -192,7 +193,17 @@ $cl_protprocesso->rotulo->label();
 
 
 <script>
-	function js_self(){
+	function js_self(tipo){
+
+    var tipos = {
+      pdf: 'pro2_processos002.php',
+      csv: 'pro2_processos002_csv.php'
+    };
+
+    if (!tipos[tipo]) {
+      return;
+    }
+
 		vir="";
 		listacgm = "";
 		for(x=0;x<parent.iframe_s2.document.form1.cgm.length;x++){
@@ -234,7 +245,7 @@ $cl_protprocesso->rotulo->label();
 		var iTipoCGMProcesso = document.form1.cboTitularProcesso.value;
 
 		/* Monta URL */
-		var sUrlOpen  = 'pro2_processos002.php?';
+		var sUrlOpen  = tipos[tipo] + '?';
 		sUrlOpen += 'listacgm='+listacgm;
 		sUrlOpen += '&listadept='+listadept;
 		sUrlOpen += '&listatipo='+listatipo;
