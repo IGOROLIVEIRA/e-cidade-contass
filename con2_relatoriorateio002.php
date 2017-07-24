4997dde1c7dd1133c6b06a65f9235b512317d580
gabriel <gabriel@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2017-07-24 21:21:12 +0000

    OC 4091 - Relatório

---
+++ con2_relatoriorateio002.php
@@ -0,0 +1,203 @@
+<?php
+
+require_once "libs/db_stdlib.php";
+require_once "libs/db_conecta.php";
+include_once "libs/db_sessoes.php";
+include_once "libs/db_usuariosonline.php";
+
+function gerarSQL($sMes, $sEnte) {
+
+  $nEnte  = intval($sEnte);
+  $nMes   = intval($sMes);
+  $nAno   = intval(db_getsession('DB_anousu'));
+
+  return "
+
+    SELECT DISTINCT lpad(desmes.c217_funcao,2,0) funcao,
+                    lpad(desmes.c217_subfuncao,3,0) subfuncao,
+                    desmes.c217_natureza,
+                    desmes.c217_subelemento,
+                    desmes.c217_fonte,
+
+        (SELECT sum(desatemes.c217_valorempenhado)
+         FROM despesarateioconsorcio desatemes
+         WHERE desmes.c217_funcao=desatemes.c217_funcao
+             AND desmes.c217_subfuncao=desatemes.c217_subfuncao
+             AND desmes.c217_natureza=desatemes.c217_natureza
+             AND desmes.c217_subelemento =desatemes.c217_subelemento
+             AND desmes.c217_fonte =desatemes.c217_fonte
+             AND desatemes.c217_mes = {$nMes}
+             AND desmes.c217_anousu = desatemes.c217_anousu) AS empenhomes,
+
+        (SELECT sum(desatemes.c217_valorempenhado)
+         FROM despesarateioconsorcio desatemes
+         WHERE desmes.c217_funcao=desatemes.c217_funcao
+             AND desmes.c217_subfuncao=desatemes.c217_subfuncao
+             AND desmes.c217_natureza=desatemes.c217_natureza
+             AND desmes.c217_subelemento =desatemes.c217_subelemento
+             AND desmes.c217_fonte =desatemes.c217_fonte
+             AND desatemes.c217_mes < {$nMes}
+             AND desmes.c217_anousu = desatemes.c217_anousu) AS empenhoatemes,
+
+        (SELECT sum(desatemes.c217_valorempenhadoanulado)
+         FROM despesarateioconsorcio desatemes
+         WHERE desmes.c217_funcao=desatemes.c217_funcao
+             AND desmes.c217_subfuncao=desatemes.c217_subfuncao
+             AND desmes.c217_natureza=desatemes.c217_natureza
+             AND desmes.c217_subelemento =desatemes.c217_subelemento
+             AND desmes.c217_fonte =desatemes.c217_fonte
+             AND desatemes.c217_mes = {$nMes}
+             AND desmes.c217_anousu = desatemes.c217_anousu) AS anuladomes,
+
+        (SELECT sum(desatemes.c217_valorempenhadoanulado)
+         FROM despesarateioconsorcio desatemes
+         WHERE desmes.c217_funcao=desatemes.c217_funcao
+             AND desmes.c217_subfuncao=desatemes.c217_subfuncao
+             AND desmes.c217_natureza=desatemes.c217_natureza
+             AND desmes.c217_subelemento =desatemes.c217_subelemento
+             AND desmes.c217_fonte =desatemes.c217_fonte
+             AND desatemes.c217_mes < {$nMes}
+             AND desmes.c217_anousu = desatemes.c217_anousu) AS anuladoatemes,
+
+        (SELECT sum(desatemes.c217_valorliquidado)
+         FROM despesarateioconsorcio desatemes
+         WHERE desmes.c217_funcao=desatemes.c217_funcao
+             AND desmes.c217_subfuncao=desatemes.c217_subfuncao
+             AND desmes.c217_natureza=desatemes.c217_natureza
+             AND desmes.c217_subelemento =desatemes.c217_subelemento
+             AND desmes.c217_fonte =desatemes.c217_fonte
+             AND desatemes.c217_mes = {$nMes}
+             AND desmes.c217_anousu = desatemes.c217_anousu) AS liquidadomes,
+
+        (SELECT sum(desatemes.c217_valorliquidado)
+         FROM despesarateioconsorcio desatemes
+         WHERE desmes.c217_funcao=desatemes.c217_funcao
+             AND desmes.c217_subfuncao=desatemes.c217_subfuncao
+             AND desmes.c217_natureza=desatemes.c217_natureza
+             AND desmes.c217_subelemento =desatemes.c217_subelemento
+             AND desmes.c217_fonte =desatemes.c217_fonte
+             AND desatemes.c217_mes < {$nMes}
+             AND desmes.c217_anousu = desatemes.c217_anousu) AS liquidadoatemes,
+
+        (SELECT sum(desatemes.c217_valorliquidadoanulado)
+         FROM despesarateioconsorcio desatemes
+         WHERE desmes.c217_funcao=desatemes.c217_funcao
+             AND desmes.c217_subfuncao=desatemes.c217_subfuncao
+             AND desmes.c217_natureza=desatemes.c217_natureza
+             AND desmes.c217_subelemento =desatemes.c217_subelemento
+             AND desmes.c217_fonte =desatemes.c217_fonte
+             AND desatemes.c217_mes = {$nMes}
+             AND desmes.c217_anousu = desatemes.c217_anousu) AS liquidadoanualdomes,
+
+        (SELECT sum(desatemes.c217_valorliquidadoanulado)
+         FROM despesarateioconsorcio desatemes
+         WHERE desmes.c217_funcao=desatemes.c217_funcao
+             AND desmes.c217_subfuncao=desatemes.c217_subfuncao
+             AND desmes.c217_natureza=desatemes.c217_natureza
+             AND desmes.c217_subelemento =desatemes.c217_subelemento
+             AND desmes.c217_fonte =desatemes.c217_fonte
+             AND desatemes.c217_mes < {$nMes}
+             AND desmes.c217_anousu = desatemes.c217_anousu) AS liquidadoanualdoatemes,
+
+        (SELECT sum(desatemes.c217_valorpago)
+         FROM despesarateioconsorcio desatemes
+         WHERE desmes.c217_funcao=desatemes.c217_funcao
+             AND desmes.c217_subfuncao=desatemes.c217_subfuncao
+             AND desmes.c217_natureza=desatemes.c217_natureza
+             AND desmes.c217_subelemento =desatemes.c217_subelemento
+             AND desmes.c217_fonte =desatemes.c217_fonte
+             AND desatemes.c217_mes = {$nMes}
+             AND desmes.c217_anousu = desatemes.c217_anousu) AS pagomes,
+
+        (SELECT sum(desatemes.c217_valorpago)
+         FROM despesarateioconsorcio desatemes
+         WHERE desmes.c217_funcao=desatemes.c217_funcao
+             AND desmes.c217_subfuncao=desatemes.c217_subfuncao
+             AND desmes.c217_natureza=desatemes.c217_natureza
+             AND desmes.c217_subelemento =desatemes.c217_subelemento
+             AND desmes.c217_fonte =desatemes.c217_fonte
+             AND desatemes.c217_mes < {$nMes}
+             AND desmes.c217_anousu = desatemes.c217_anousu) AS pagoatemes,
+
+        (SELECT sum(desatemes.c217_valorpagoanulado)
+         FROM despesarateioconsorcio desatemes
+         WHERE desmes.c217_funcao=desatemes.c217_funcao
+             AND desmes.c217_subfuncao=desatemes.c217_subfuncao
+             AND desmes.c217_natureza=desatemes.c217_natureza
+             AND desmes.c217_subelemento =desatemes.c217_subelemento
+             AND desmes.c217_fonte =desatemes.c217_fonte
+             AND desatemes.c217_mes = {$nMes}
+             AND desmes.c217_anousu = desatemes.c217_anousu) AS pagoanuladomes,
+
+        (SELECT sum(desatemes.c217_valorpagoanulado)
+         FROM despesarateioconsorcio desatemes
+         WHERE desmes.c217_funcao=desatemes.c217_funcao
+             AND desmes.c217_subfuncao=desatemes.c217_subfuncao
+             AND desmes.c217_natureza=desatemes.c217_natureza
+             AND desmes.c217_subelemento =desatemes.c217_subelemento
+             AND desmes.c217_fonte =desatemes.c217_fonte
+             AND desatemes.c217_mes < {$nMes}
+             AND desmes.c217_anousu = desatemes.c217_anousu) AS pagoanuladoatemes
+    FROM despesarateioconsorcio desmes
+    WHERE desmes.c217_anousu = {$nAno}
+        AND desmes.c217_enteconsorciado = {$nEnte}
+    ORDER BY funcao,
+             subfuncao,
+             desmes.c217_natureza,
+             desmes.c217_subelemento,
+             desmes.c217_fonte
+
+    ";
+
+}
+
+$aMeses = array(
+  1 => 'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro'
+);
+
+try {
+
+  $rsRelatorio = db_query(gerarSQL($_GET['mes'], $_GET['c215_sequencial']));
+
+  $oInfoRelatorio = new stdClass();
+  $oInfoRelatorio->aDados = db_utils::getCollectionByRecord($rsRelatorio);
+
+  switch ($_GET['tipoarquivo']) {
+    case 'pdf':
+
+      require_once('classes/db_entesconsorciados_classe.php');
+
+      $oEntesCon  = new cl_entesconsorciados();
+      $rsEntesCon = $oEntesCon->sql_record($oEntesCon->sql_query($_GET['c215_sequencial'], 'z01_nome'));
+      $oEnte      = db_utils::fieldsMemory($rsEntesCon, 0);
+
+      $oInfoRelatorio->aHeader = array();
+      $oInfoRelatorio->aHeader[1] = 'Relatório de Rateio';
+      $oInfoRelatorio->aHeader[3] = 'Ente: ' . $oEnte->z01_nome;
+      $oInfoRelatorio->aHeader[4] = 'Mês: ' . $aMeses[intval($_GET['mes'])];
+
+      require_once('con2_relatoriorateio002_pdf.php');
+
+      break;
+
+    default:
+      throw new Exception("Tipo de arquivo não suportado", 1);
+      break;
+  }
+
+} catch (Exception $e) {
+
+  db_redireciona('db_erros.php?fechar=true&db_erro=' . $e->getMessage());
+
+}
---
+++ con2_relatoriorateio002_pdf.php
@@ -0,0 +1,212 @@
+<?php
+
+require_once("model/relatorios/Relatorio.php");
+
+$mPDF = new Relatorio('', 'A4-L');
+
+foreach ($oInfoRelatorio->aHeader as $key => $value) {
+  $mPDF->addInfo($value, $key);
+}
+
+
+$aTotal = array(
+  'empenhado' => array(
+    'no_mes'    => 0,
+    'ate_o_mes' => 0
+  ),
+  'emp_anualdo' => array(
+    'no_mes'    => 0,
+    'ate_o_mes' => 0
+  ),
+  'liquidado' => array(
+    'no_mes'    => 0,
+    'ate_o_mes' => 0
+  ),
+  'liq_anulado' => array(
+    'no_mes'    => 0,
+    'ate_o_mes' => 0
+  ),
+  'pago' => array(
+    'no_mes'    => 0,
+    'ate_o_mes' => 0
+  ),
+  'pago_anulado' => array(
+    'no_mes'    => 0,
+    'ate_o_mes' => 0
+  )
+);
+
+
+ob_start();
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+  <style type="text/css">
+  .ritz .waffle a { color: inherit; }
+  .ritz .waffle .s0 { border-bottom: 1px SOLID #000000; border-right: 1px SOLID #000000; background-color: #ffffff; text-align: center; font-weight: bold; color: #000000; font-family: 'Arial'; font-size: 10pt; vertical-align: bottom; white-space: nowrap; padding: 2px 3px 2px 3px; }
+  .ritz .waffle .s5 { border-bottom: 1px SOLID #000000; border-right: 1px SOLID #000000; background-color: #ffffff; text-align: right; font-weight: bold; color: #000000; font-family: 'Arial'; font-size: 10pt; vertical-align: bottom; white-space: nowrap; padding: 2px 3px 2px 3px; }
+  .ritz .waffle .s2 { border-left: none; border-right: none; background-color: #ffffff; text-align: center; font-weight: bold; color: #000000; font-family: 'Arial'; font-size: 10pt; vertical-align: bottom; white-space: nowrap; padding: 2px 3px 2px 3px; }
+  .ritz .waffle .s1 { border-right: none; background-color: #ffffff; text-align: center; font-weight: bold; color: #000000; font-family: 'Arial'; font-size: 10pt; vertical-align: bottom; white-space: nowrap; padding: 2px 3px 2px 3px; }
+  .ritz .waffle .s3 { border-left: none; background-color: #ffffff; text-align: center; font-weight: bold; color: #000000; font-family: 'Arial'; font-size: 10pt; vertical-align: bottom; white-space: nowrap; padding: 2px 3px 2px 3px; }
+  .ritz .waffle .s4 { border-bottom: 1px SOLID #000000; border-right: 1px SOLID #000000; text-align: right; color: #000000; font-family: 'Arial'; font-size: 10pt; vertical-align: bottom; white-space: nowrap; padding: 2px 3px 2px 3px; }
+
+  .ritz .waffle .borda1 {
+    border-bottom: 1px SOLID #000000;
+  }
+  .ritz .waffle .borda2 {
+    border-right: 1px SOLID #000000;
+    border-bottom: 1px SOLID #000000;
+  }
+  .ritz .waffle .borda3 {
+    border-left: 1px SOLID #000000;
+  }
+
+  .ritz .waffle .bg_1 {
+    background-color: #f3f3f3;
+  }
+  </style>
+
+  <title>Relatório de Rateio - <?= date("d-m-Y H:i:s") ?></title>
+</head>
+
+<body>
+  <div class="ritz grid-container" dir="ltr">
+    <table class="waffle" cellspacing="0" cellpadding="0">
+      <thead>
+        <tr>
+          <th class="borda1" style="width:70px">&nbsp;</th>
+          <th class="borda1" style="width:78px">&nbsp;</th>
+          <th class="borda1" style="width:66px">&nbsp;</th>
+          <th class="borda1" style="width:91px">&nbsp;</th>
+          <th class="borda1" style="width:42px">&nbsp;</th>
+          <th class="borda1" style="width:100px">&nbsp;</th>
+          <th class="borda1" style="width:100px">&nbsp;</th>
+          <th class="borda1" style="width:100px">&nbsp;</th>
+          <th class="borda1" style="width:100px">&nbsp;</th>
+          <th class="borda1" style="width:100px">&nbsp;</th>
+          <th class="borda1" style="width:100px">&nbsp;</th>
+          <th class="borda1" style="width:100px">&nbsp;</th>
+          <th class="borda1" style="width:100px">&nbsp;</th>
+          <th class="borda1" style="width:100px">&nbsp;</th>
+          <th class="borda1" style="width:100px">&nbsp;</th>
+          <th class="borda1" style="width:100px">&nbsp;</th>
+          <th class="borda1" style="width:100px">&nbsp;</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr style='height:20px;'>
+          <td class="s0 borda3" colspan="5">Classifica&ccedil;&atilde;o</td>
+          <td class="s0" colspan="2">Empenhado</td>
+          <td class="s0" colspan="2">Emp. Anualdo</td>
+          <td class="s0" colspan="2">Liquidado</td>
+          <td class="s0" colspan="2">Liq. Anulado</td>
+          <td class="s0" colspan="2">Pago</td>
+          <td class="s0" colspan="2">Pago Anulado</td>
+        </tr>
+
+        <tr style='height:20px;'>
+          <td class="s1 borda2 borda3">Fun&ccedil;&atilde;o</td>
+          <td class="s2 borda2">SubFun&ccedil;&atilde;o</td>
+          <td class="s2 borda2">Elemento</td>
+          <td class="s3 borda2">SubElemento</td>
+          <td class="s0">Fonte</td>
+          <td class="s0">No M&ecirc;s</td>
+          <td class="s0">At&eacute; o M&ecirc;s</td>
+          <td class="s0">No M&ecirc;s</td>
+          <td class="s0">At&eacute; o M&ecirc;s</td>
+          <td class="s0">No M&ecirc;s</td>
+          <td class="s0">At&eacute; o M&ecirc;s</td>
+          <td class="s0">No M&ecirc;s</td>
+          <td class="s0">At&eacute; o M&ecirc;s</td>
+          <td class="s0">No M&ecirc;s</td>
+          <td class="s0">At&eacute; o M&ecirc;s</td>
+          <td class="s0">No M&ecirc;s</td>
+          <td class="s0">At&eacute; o M&ecirc;s</td>
+        </tr>
+
+
+        <?php foreach ($oInfoRelatorio->aDados as $key => $oRegistro):
+
+          $aTotal['empenhado']['no_mes']        += $oRegistro->empenhomes;
+          $aTotal['empenhado']['ate_o_mes']     += $oRegistro->empenhoatemes;
+          $aTotal['emp_anualdo']['no_mes']      += $oRegistro->anuladomes;
+          $aTotal['emp_anualdo']['ate_o_mes']   += $oRegistro->anuladoatemes;
+          $aTotal['liquidado']['no_mes']        += $oRegistro->liquidadomes;
+          $aTotal['liquidado']['ate_o_mes']     += $oRegistro->liquidadoatemes;
+          $aTotal['liq_anulado']['no_mes']      += $oRegistro->liquidadoanualdomes;
+          $aTotal['liq_anulado']['ate_o_mes']   += $oRegistro->liquidadoanualdoatemes;
+          $aTotal['pago']['no_mes']             += $oRegistro->pagomes;
+          $aTotal['pago']['ate_o_mes']          += $oRegistro->pagoatemes;
+          $aTotal['pago_anulado']['no_mes']     += $oRegistro->pagoanuladomes;
+          $aTotal['pago_anulado']['ate_o_mes']  += $oRegistro->pagoanuladoatemes;
+
+        ?>
+
+          <tr style='height:20px;' class="bg_<?= ($key % 2) == 0 ?>">
+            <td class="s4 borda3"><?= $oRegistro->funcao ?></td>
+            <td class="s4"><?= $oRegistro->subfuncao ?></td>
+            <td class="s4"><?= $oRegistro->c217_natureza ?></td>
+            <td class="s4"><?= $oRegistro->c217_subelemento ?></td>
+            <td class="s4"><?= $oRegistro->c217_fonte ?></td>
+            <td class="s4"><?= db_formatar($oRegistro->empenhomes, 'f') ?></td>
+            <td class="s4"><?= db_formatar($oRegistro->empenhoatemes, 'f') ?></td>
+            <td class="s4"><?= db_formatar($oRegistro->anuladomes, 'f') ?></td>
+            <td class="s4"><?= db_formatar($oRegistro->anuladoatemes, 'f') ?></td>
+            <td class="s4"><?= db_formatar($oRegistro->liquidadomes, 'f') ?></td>
+            <td class="s4"><?= db_formatar($oRegistro->liquidadoatemes, 'f') ?></td>
+            <td class="s4"><?= db_formatar($oRegistro->liquidadoanualdomes, 'f') ?></td>
+            <td class="s4"><?= db_formatar($oRegistro->liquidadoanualdoatemes, 'f') ?></td>
+            <td class="s4"><?= db_formatar($oRegistro->pagomes, 'f') ?></td>
+            <td class="s4"><?= db_formatar($oRegistro->pagoatemes, 'f') ?></td>
+            <td class="s4"><?= db_formatar($oRegistro->pagoanuladomes, 'f') ?></td>
+            <td class="s4"><?= db_formatar($oRegistro->pagoanuladoatemes, 'f') ?></td>
+          </tr>
+
+        <?php endforeach ?>
+
+
+        <tr style='height:20px;'>
+          <td class="s5 borda3" colspan="5">TOTAL</td>
+          <td class="s5"><?= db_formatar($aTotal['empenhado']['no_mes'], 'f') ?></td>
+          <td class="s5"><?= db_formatar($aTotal['empenhado']['ate_o_mes'], 'f') ?></td>
+          <td class="s5"><?= db_formatar($aTotal['emp_anualdo']['no_mes'], 'f') ?></td>
+          <td class="s5"><?= db_formatar($aTotal['emp_anualdo']['ate_o_mes'], 'f') ?></td>
+          <td class="s5"><?= db_formatar($aTotal['liquidado']['no_mes'], 'f') ?></td>
+          <td class="s5"><?= db_formatar($aTotal['liquidado']['ate_o_mes'], 'f') ?></td>
+          <td class="s5"><?= db_formatar($aTotal['liq_anulado']['no_mes'], 'f') ?></td>
+          <td class="s5"><?= db_formatar($aTotal['liq_anulado']['ate_o_mes'], 'f') ?></td>
+          <td class="s5"><?= db_formatar($aTotal['pago']['no_mes'], 'f') ?></td>
+          <td class="s5"><?= db_formatar($aTotal['pago']['ate_o_mes'], 'f') ?></td>
+          <td class="s5"><?= db_formatar($aTotal['pago_anulado']['no_mes'], 'f') ?></td>
+          <td class="s5"><?= db_formatar($aTotal['pago_anulado']['ate_o_mes'], 'f') ?></td>
+        </tr>
+
+
+      </tbody>
+    </table>
+  </div>
+</body>
+</html>
+
+<?php
+
+$html = ob_get_contents();
+
+ob_end_clean();
+
+try {
+
+  $mPDF->WriteHTML(utf8_encode($html));
+  $mPDF->Output();
+
+} catch (Exception $e) {
+
+  print_r($e->getMessage());
+
+}
+
+?>
