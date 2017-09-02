c0dc1bd8d5aa80be0ef6647a2532da0fb14c2eba
elvio <elvio@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2017-09-02 21:03:54 +0000

    Ajuste para pegar sequenciais dos menus corretos das tabelas.

--- updatedb/script_add_excluir_autorizaempenho.sql
+++ updatedb/script_add_excluir_autorizaempenho.sql
@@ -1,10 +1,10 @@
 BEGIN;
 
-INSERT INTO db_itensmenu (id_item,descricao,help,funcao,itemativo,manutencao,desctec,libcliente) values (
-3000115,'Exclusão','Exclusão','emp1_empautoriza003.php',1,1,'Exclusão','t');
+INSERT INTO db_itensmenu (id_item,descricao,help,funcao,itemativo,manutencao,desctec,libcliente) VALUES (
+ (SELECT max(id_item)+1 FROM db_itensmenu),'Exclusão','Exclusão','emp1_empautoriza003.php',1,1,'Exclusão','t');
 INSERT INTO db_menu (id_item,id_item_filho,menusequencia,modulo) values 
-(2567,3000115,3,398);
+(2567,(SELECT id_item FROM db_itensmenu WHERE funcao = 'emp1_empautoriza003.php'),3,398);
 INSERT INTO db_menu (id_item,id_item_filho,menusequencia,modulo) values 
-(2567,3000115,3,28);
+(2567,(SELECT id_item FROM db_itensmenu WHERE funcao = 'emp1_empautoriza003.php'),3,28);
 
 COMMIT;
