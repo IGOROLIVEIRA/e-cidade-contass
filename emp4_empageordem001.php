26bc43a592d2f3616b8c16d84ca3b004a6bb11a6
elvio <elvio@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2017-10-04 12:57:44 +0000

    Ajuste da tela de consulta de OP Auxiliar.

--- emp4_empageordem001.php
+++ emp4_empageordem001.php
@@ -1,28 +1,28 @@
 <?php
 /*
- *     E-cidade Software Publico para Gestao Municipal                
- *  Copyright (C) 2009  DBselller Servicos de Informatica             
- *                            www.dbseller.com.br                     
- *                         e-cidade@dbseller.com.br                   
- *                                                                    
- *  Este programa e software livre; voce pode redistribui-lo e/ou     
- *  modifica-lo sob os termos da Licenca Publica Geral GNU, conforme  
- *  publicada pela Free Software Foundation; tanto a versao 2 da      
- *  Licenca como (a seu criterio) qualquer versao mais nova.          
- *                                                                    
- *  Este programa e distribuido na expectativa de ser util, mas SEM   
- *  QUALQUER GARANTIA; sem mesmo a garantia implicita de              
- *  COMERCIALIZACAO ou de ADEQUACAO A QUALQUER PROPOSITO EM           
- *  PARTICULAR. Consulte a Licenca Publica Geral GNU para obter mais  
- *  detalhes.                                                         
- *                                                                    
- *  Voce deve ter recebido uma copia da Licenca Publica Geral GNU     
- *  junto com este programa; se nao, escreva para a Free Software     
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA          
- *  02111-1307, USA.                                                  
- *  
- *  Copia da licenca no diretorio licenca/licenca_en.txt 
- *                                licenca/licenca_pt.txt 
+ *     E-cidade Software Publico para Gestao Municipal
+ *  Copyright (C) 2009  DBselller Servicos de Informatica
+ *                            www.dbseller.com.br
+ *                         e-cidade@dbseller.com.br
+ *
+ *  Este programa e software livre; voce pode redistribui-lo e/ou
+ *  modifica-lo sob os termos da Licenca Publica Geral GNU, conforme
+ *  publicada pela Free Software Foundation; tanto a versao 2 da
+ *  Licenca como (a seu criterio) qualquer versao mais nova.
+ *
+ *  Este programa e distribuido na expectativa de ser util, mas SEM
+ *  QUALQUER GARANTIA; sem mesmo a garantia implicita de
+ *  COMERCIALIZACAO ou de ADEQUACAO A QUALQUER PROPOSITO EM
+ *  PARTICULAR. Consulte a Licenca Publica Geral GNU para obter mais
+ *  detalhes.
+ *
+ *  Voce deve ter recebido uma copia da Licenca Publica Geral GNU
+ *  junto com este programa; se nao, escreva para a Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307, USA.
+ *
+ *  Copia da licenca no diretorio licenca/licenca_en.txt
+ *                                licenca/licenca_pt.txt
  */
 
 require("libs/db_stdlib.php");
@@ -65,27 +65,27 @@ $oRotuloCampos->label("e42_dtpagamento");
             </legend>
             <table>
               <tr>
-                <td> 
+                <td>
                    <b>
                    <? db_ancora("<b>Código da Ordem Auxiliar</b>","js_pesquisae43_ordempagamento(true);",1);  ?>
-                     
+
                    </b>
                 </td>
                 <td>
                   <?
-                    db_input("e43_ordempagamento", 10, $Ie43_ordempagamento, true,"text",1,"onchange='js_pesquisae43_ordempagamento(false)';");     
+                    db_input("e43_ordempagamento", 10, $Ie43_ordempagamento, true,"text",1,"onchange='js_pesquisae43_ordempagamento(false)';");
                   ?>
                 </td>
               </tr>
               <tr>
-                <td> 
+                <td>
                    <b>
                      Data da Emissão
                    </b>
                 </td>
                 <td>
                   <?
-                    db_inputdata("e42_dtpagamento", null,null,null,true,"text",1);     
+                    db_inputdata("e42_dtpagamento", null,null,null,true,"text",1);
                   ?>
                 </td>
               </tr>
@@ -98,7 +98,7 @@ $oRotuloCampos->label("e42_dtpagamento");
          <input type="button" value='Continuar' onclick="js_carregaOPauxiliar()">
          <input type="button" value='Incluir Nova' onclick="js_novaOPAuxiliar()">
        </td>
-     </tr>   
+     </tr>
   </table>
   </center>
   </form>
@@ -118,7 +118,7 @@ function js_pesquisae43_ordempagamento(mostra){
                         true,
                         22,
                         0,
-                        document.width-12,
+                        document.width,
                         document.body.scrollHeight-30
                         );
   } else {
@@ -130,38 +130,38 @@ function js_pesquisae43_ordempagamento(mostra){
                          false,
                          22,
                          0,
-                         document.width-12,
+                         document.width,
                          document.body.scrollHeight-30);
     } else {
       $('e43_ordempagamento').value = '';
-    }   
+    }
   }
 }
 
 function js_mostraordem1(chave1,chave2){
-  
+
   document.form1.e43_ordempagamento.value = chave1;
   document.form1.e42_dtpagamento.value = js_formatar(chave2,"d");
   func_nome.hide();
-  
+
 }
 
 function js_mostraordemagenda(chave,erro){
-  
-  if(!erro) { 
+
+  if(!erro) {
     document.form1.e42_dtpagamento.value = chave;
   } else {
-  
-    document.form1.e43_ordempagamento.value  = ''; 
+
+    document.form1.e43_ordempagamento.value  = '';
     document.form1.e42_dtpagamento.value = '';
-    
+
   }
 }
 
 function js_carregaOPauxiliar() {
 
   if ($F('e43_ordempagamento') != "") {
-    
+
     var iAlturaViewPort   = document.body.scrollHeight-30;
     var iLArguraViewPort  = document.body.scrollWidth-12;
     js_OpenJanelaIframe('top.corpo','db_iframe_op',
@@ -179,34 +179,34 @@ function js_carregaOPauxiliar() {
 function js_novaOPAuxiliar() {
 
   if ($F('e42_dtpagamento') == "") {
-    
+
     alert('Data de pagamento não informada!');
     return false;
-   
+
   }
   var oParametros             = new Object();
   oParametros.exec            = "incluirOP";
   oParametros.e42_dtpagamento = $F('e42_dtpagamento');
   js_divCarregando("Aguarde, Gerando Autorização.","msgBox");
   var oAjax   = new Ajax.Request(
-                           sUrl, 
+                           sUrl,
                            {
-                            method    : 'post', 
-                            parameters: 'json='+Object.toJSON(oParametros), 
+                            method    : 'post',
+                            parameters: 'json='+Object.toJSON(oParametros),
                             onComplete: function(oResponse) {
-                            
+
                               js_removeObj("msgBox");
                               oRetorno = eval ("("+oResponse.responseText+")");
                               if (oRetorno.status == 1) {
-                              
+
                                  $('e43_ordempagamento').value = oRetorno.iCodigoOPaxiliar;
                                  js_carregaOPauxiliar();
-                                 
+
                               } else {
                                 alert(oRetorno.message.urlDecode());
                               }
                             }
                           }
                           );
-} 
+}
 </script>
