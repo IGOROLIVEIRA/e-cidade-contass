aa35ae352dcc65bea8d0c67638c6e0f3a08ccb18
rafhael <rafhael@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2017-10-30 12:57:15 +0000

    Correção do script de menus

--- updatedb/1508848723_OC3414.sql
+++ updatedb/1508848723_OC3414.sql
@@ -5,10 +5,11 @@ BEGIN;
 
 SELECT fc_startsession();
 
-INSERT INTO db_itensmenu VALUES (4001010, 'Excluir Empenhos', 'Excluir Empenhos', 'emp1_exclusaoempenho003.php', 1, 1, 'Excluir Empenhos', 't');
-INSERT INTO db_itensmenu VALUES (4001011, 'Empenhos Excluídos', 'Empenhos Excluídos', 'emp2_empenhosexcluidos001.php', 1, 1, 'Empenhos Excluídos', 't');
+INSERT INTO db_itensmenu VALUES ((select max(id_item) + 1 from db_itensmenu), 'Excluir Empenhos', 'Excluir Empenhos', 'emp1_exclusaoempenho003.php', 1, 1, 'Excluir Empenhos', 't');
+INSERT INTO db_itensmenu VALUES ((select max(id_item) + 1 from db_itensmenu), 'Empenhos Excluídos', 'Empenhos Excluídos', 'emp2_empenhosexcluidos001.php', 1, 1, 'Empenhos Excluídos', 't');
 
-INSERT INTO db_menu VALUES (4021, 4001010, 110, 398);
-INSERT INTO db_menu VALUES (5603, 4001011, 111, 398);
+INSERT INTO db_menu VALUES (4021, (select max(id_item) + 1 from db_itensmenu), 110, 398);
+INSERT INTO db_menu VALUES (5603, (select max(id_item) + 1 from db_itensmenu), 111, 398);
 
 COMMIT;
+
