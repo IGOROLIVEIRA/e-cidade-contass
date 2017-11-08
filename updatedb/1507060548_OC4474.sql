46d8a5444af951223898a073a480ab16416d4aee
rafhael <rafhael@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2017-11-08 10:11:51 +0000

    OC4474 - Correção do script de menus

--- updatedb/1507060548_OC4474.sql
+++ updatedb/1507060548_OC4474.sql
@@ -5,14 +5,18 @@ BEGIN;
 
 SELECT fc_startsession();
 
-INSERT INTO db_itensmenu VALUES (4001110, 'Protocolo', 'Protocolos', '', 1, 1, 'Protocolos', 't');
-INSERT INTO db_itensmenu VALUES (4001111, 'Incluir', 'Protocolos', 'pro1_protocoloinclusao001.php', 1, 1, 'Cadastro Protocolos', 't');
-INSERT INTO db_itensmenu VALUES (4001112, 'Localizar', 'Protocolos', 'pro3_consultaprocesso_aut_emp_oc_op001.php', 1, 1, 'Localizar protocolos', 't');
+INSERT INTO db_itensmenu VALUES ((select max(id_item) + 1 from db_itensmenu), 'Protocolo', 'Protocolos', '', 1, 1, 'Protocolos', 't');
+INSERT INTO db_menu VALUES (32, (select max(id_item) from db_itensmenu), 410, 604);
 
-INSERT INTO db_menu VALUES (32, 4001110, 410, 604);
-INSERT INTO db_menu VALUES (4001110, 4001111, 1, 604);
-INSERT INTO db_menu VALUES (4001110, 4001112, 2, 604);
+INSERT INTO db_itensmenu VALUES ((select max(id_item) + 1 from db_itensmenu), 'Incluir', 'Protocolos', 'pro1_protocoloinclusao001.php', 1, 1, 'Cadastro Protocolos', 't');
+INSERT INTO db_menu VALUES ((select max(id_item) - 1 from db_itensmenu), (select max(id_item) + 1 from db_itensmenu), 1, 604);
+
+INSERT INTO db_itensmenu VALUES ((select max(id_item) + 1 from db_itensmenu), 'Localizar', 'Protocolos', 'pro3_consultaprocesso_aut_emp_oc_op001.php', 1, 1, 'Localizar protocolos', 't');
+INSERT INTO db_menu VALUES ((select max(id_item) - 2 from db_itensmenu), (select max(id_item) + 1 from db_itensmenu), 2, 604);
 
 COMMIT;
+
+
+
 
 
