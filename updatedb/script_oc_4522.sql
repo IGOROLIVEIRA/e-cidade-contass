76b0003506ff24f0799bffb4ed1fdb33397d5c83
mario <mario@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2017-11-10 18:35:40 +0000

    correção erro onde o nome da função estava errada

--- updatedb/script_oc_4522.sql
+++ updatedb/script_oc_4522.sql
@@ -1,7 +1,7 @@
 BEGIN;
 SELECT fc_startsession();
 
-INSERT INTO db_itensmenu VALUES ((select max(id_item)+1 from db_itensmenu), 'Ratificação', 'Ratificação de Processo', 'rat_ratificacaoproc001.php', 1, 1, 'Ratificação de Processo', 't');
+INSERT INTO db_itensmenu VALUES ((select max(id_item)+1 from db_itensmenu), 'Ratificação', 'Ratificação de Processo', 'rat2_ratificacaoproc001.php', 1, 1, 'Ratificação de Processo', 't');
 INSERT INTO db_menu VALUES (1797, (select max(id_item) from db_itensmenu), 1001, 381);
 COMMIT;
 
