1bdabcf4384d069bbda1e9d98a41ea3784162733
marcelo <marcelo@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2018-01-04 19:52:41 +0000

    OC4995

--- updatedb/1515093932_OC4995.sql
+++ updatedb/1515093932_OC4995.sql
@@ -5,7 +5,7 @@ SELECT fc_startsession();
 
 -- Início do script
 
-alter table pcfornecertifdoc alter column pc75_numdocumento type varchar(30);
+alter table pcfornecertifdoc alter column pc75_numdocumento type varchar(40);
 
 -- Fim do script
 
