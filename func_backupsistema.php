2943a9591dc1dda32efaa8cd5df9b6cd9ccf80b2
marcelo <marcelo@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2018-01-10 19:50:58 +0000

    ajuste backup do sistema

--- func_backupsistema.php
+++ func_backupsistema.php
@@ -7,7 +7,7 @@ include("libs/db_usuariosonline.php");
 include("dbforms/db_funcoes.php");
 
 system("rm -R /var/www/e-cidade/backup_e-cidade_*");
-system("pg_dump -U dbportal e-cidade | bzip2 -c > /var/www/e-cidade/backup_e-cidade_".date("dmY").".sql.bz2");
+system("pg_dump -U dbportal e-cidade | bzip2 -c > /var/www/e-cidade/tmp/backup_e-cidade_".date("dmY").".sql.bz2");
 
 // As linhas abaixo forçam o download do arquivo
 /*$link = "/var/www/e-cidade/backup_e-cidade_".date("dmY").".sql.bz2";
@@ -17,6 +17,6 @@ header ("Content-Length: ".filesize($link));
 readfile($link);
 */
 
-echo json_encode("/var/www/e-cidade/backup_e-cidade_".date("dmY").".sql.bz2");
+echo json_encode("/var/www/e-cidade/tmp/backup_e-cidade_".date("dmY").".sql.bz2");
 
 ?>
