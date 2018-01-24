bfbf347996d835f58db9b6c97b3e6489620aa0d1
rodrigo <rodrigo@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2018-01-24 17:50:52 +0000

    Include da classe.

--- iss1_isscadsimples001.php
+++ iss1_isscadsimples001.php
@@ -33,12 +33,13 @@ require_once("libs/db_usuariosonline.php");
 require_once("classes/db_isscadsimples_classe.php");
 require_once("classes/db_issbase_classe.php");
 require_once("dbforms/db_funcoes.php");
-
+require_once("classes/db_tabativ_classe.php");
 db_postmemory($HTTP_POST_VARS);
 $post                 = db_utils::postmemory($_POST);
 
 $clisscadsimples      = new cl_isscadsimples();
 $clissbase            = new cl_issbase();
+$cltabativ = new cl_tabativ;
 (int)  $db_opcao      = 1;
 (int)  $db_opcaoinscr = 1;
 (bool) $db_botao      = true;
@@ -48,102 +49,102 @@ $clissbase            = new cl_issbase();
 if(isset($incluir)){
 
   $rsIss = 	$clisscadsimples->sql_record($clisscadsimples->sql_query_baixa(null,'q38_dtinicial,q38_sequencial,q38_inscr,z01_nome',null
-	                              ,"q38_inscr = $q38_inscr and q39_isscadsimples is null"));
+    ,"q38_inscr = $q38_inscr and q39_isscadsimples is null"));
 
   if (isset($post->q38_dtinicial)){
 
-      $dataIni  = split("/",$post->q38_dtinicial);
-      $dataIni2 = $dataIni[2]."-".$dataIni[1]."-".$dataIni[0];
+    $dataIni  = split("/",$post->q38_dtinicial);
+    $dataIni2 = $dataIni[2]."-".$dataIni[1]."-".$dataIni[0];
   }else{
-      $dataIni2 = $post->q38_dtinicial_ano."-".$post->q38_dtinicial_mes."-".$post->q38_dtinicial_dia;
+    $dataIni2 = $post->q38_dtinicial_ano."-".$post->q38_dtinicial_mes."-".$post->q38_dtinicial_dia;
   }
 
-	if ($clisscadsimples->numrows > 0){
+  if ($clisscadsimples->numrows > 0){
 
-      $oIss                         = db_utils::fieldsMemory($rsIss,0);
-		  $clisscadsimples->erro_status = 0;
-			$clisscadsimples->erro_msg    = "Inscricão já cadastrada no simples \\nData Início: ".db_formatar($oIss->q38_dtinicial,"d");
-			$clisscadsimples->erro_msg   .= "\\nCódigo Simples : ".$oIss->q38_sequencial;
+    $oIss                         = db_utils::fieldsMemory($rsIss,0);
+    $clisscadsimples->erro_status = 0;
+    $clisscadsimples->erro_msg    = "Inscricão já cadastrada no simples \\nData Início: ".db_formatar($oIss->q38_dtinicial,"d");
+    $clisscadsimples->erro_msg   .= "\\nCódigo Simples : ".$oIss->q38_sequencial;
 
-	}else{
+  }else{
 
     $rsAtivPrint = $cltabativ->sql_record($cltabativ->sql_query_princ($post->q38_inscr));
 
-//    if ($cltabativ->numrows > 0){
-//
-//      $oAtiv = db_utils::fieldsMemory($rsAtivPrint,0);
-//      $oDataIni2 = new DBDate($dataIni2);
-//      $oDataIni  = new DBDate($oAtiv->q07_datain);
-//      if ($oDataIni2->getTimeStamp() < $oDataIni->getTimeStamp()){
-//
-//        $erro_msg = "Data Inicial menor que a data do início da atividade principal.";
-//        $lSqlErro = true;
-//        $clisscadsimples->erro_msg    = $erro_msg;
-//        $clisscadsimples->erro_campo  = "q38_dtinicial";
-//        $clisscadsimples->erro_status = 0;
-//      }
-//    } else {
-//
-//      $rsIss = $clissbase->sql_record($clissbase->sql_query($post->q38_inscr));
-//
-//      if ($clissbase->numrows > 0) {
-//
-//        $oIss = db_utils::fieldsMemory($rsIss, 0);
-//        if (db_strtotime($dataIni2) < db_strtotime($oIss->q02_dtinic)) {
-//
-//          $erro_msg = "Data Inicial menor que a data do início da inscricao.";
-//          $lSqlErro = true;
-//          $clisscadsimples->erro_msg = $erro_msg;
-//          $clisscadsimples->erro_campo = "q38_dtinicial";
-//          $clisscadsimples->erro_status = 0;
-//        }
-//      }
-//    }
-     $sSqlBaixa    = $clisscadsimples->sql_query_baixa(null,'q38_dtinicial,q38_sequencial,q38_inscr,z01_nome',null,"q38_inscr = $q38_inscr and q39_dtbaixa > '$dataIni2'::date");
-     $rsIssSimples = db_query($sSqlBaixa);
+    if ($cltabativ->numrows > 0){
 
-     if( $rsIssSimples ){
+      $oAtiv = db_utils::fieldsMemory($rsAtivPrint,0);
+      $oDataIni2 = new DBDate($dataIni2);
+      $oDataIni  = new DBDate($oAtiv->q07_datain);
+      if ($oDataIni2->getTimeStamp() < $oDataIni->getTimeStamp()){
 
-       if(pg_num_rows($rsIssSimples) > 0){
+        $erro_msg = "Data Inicial menor que a data do início da atividade principal.";
+        $lSqlErro = true;
+        $clisscadsimples->erro_msg    = $erro_msg;
+        $clisscadsimples->erro_campo  = "q38_dtinicial";
+        $clisscadsimples->erro_status = 0;
+      }
+    } else {
 
-          $erro_msg = "Data de Inclusão deve ser maior que a data da Baixa do Simples.";
-  				$lSqlErro = true;
-  				$clisscadsimples->erro_msg    = $erro_msg;
-  				$clisscadsimples->erro_campo  = "q38_dtinicial";
-  				$clisscadsimples->erro_status = 0;
-       }
-     }
+      $rsIss = $clissbase->sql_record($clissbase->sql_query($post->q38_inscr));
 
-		 if (!$lSqlErro){
+      if ($clissbase->numrows > 0) {
 
-        db_inicio_transacao();
-        $clisscadsimples->incluir($q38_sequencial);
-        db_fim_transacao($lSqlErro);
-		 }
-	}
+        $oIss = db_utils::fieldsMemory($rsIss, 0);
+        if (db_strtotime($dataIni2) < db_strtotime($oIss->q02_dtinic)) {
+
+          $erro_msg = "Data Inicial menor que a data do início da inscricao.";
+          $lSqlErro = true;
+          $clisscadsimples->erro_msg = $erro_msg;
+          $clisscadsimples->erro_campo = "q38_dtinicial";
+          $clisscadsimples->erro_status = 0;
+        }
+      }
+    }
+    $sSqlBaixa    = $clisscadsimples->sql_query_baixa(null,'q38_dtinicial,q38_sequencial,q38_inscr,z01_nome',null,"q38_inscr = $q38_inscr and q39_dtbaixa > '$dataIni2'::date");
+    $rsIssSimples = db_query($sSqlBaixa);
+
+    if( $rsIssSimples ){
+
+      if(pg_num_rows($rsIssSimples) > 0){
+
+        $erro_msg = "Data de Inclusão deve ser maior que a data da Baixa do Simples.";
+        $lSqlErro = true;
+        $clisscadsimples->erro_msg    = $erro_msg;
+        $clisscadsimples->erro_campo  = "q38_dtinicial";
+        $clisscadsimples->erro_status = 0;
+      }
+    }
+
+    if (!$lSqlErro){
+
+      db_inicio_transacao();
+      $clisscadsimples->incluir($q38_sequencial);
+      db_fim_transacao($lSqlErro);
+    }
+  }
 }
 ?>
 <html>
 <head>
-<title>DBSeller Inform&aacute;tica Ltda - P&aacute;gina Inicial</title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<meta http-equiv="Expires" CONTENT="0">
-<script language="JavaScript" type="text/javascript" src="scripts/scripts.js"></script>
-<link href="estilos.css" rel="stylesheet" type="text/css">
+  <title>DBSeller Inform&aacute;tica Ltda - P&aacute;gina Inicial</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+  <meta http-equiv="Expires" CONTENT="0">
+  <script language="JavaScript" type="text/javascript" src="scripts/scripts.js"></script>
+  <link href="estilos.css" rel="stylesheet" type="text/css">
 </head>
 <body class="body-default">
-  <div class="container">
-	<?php
-	  include("forms/db_frmisscadsimples.php");
-	?>
-  </div>
+<div class="container">
   <?php
-    db_menu(db_getsession("DB_id_usuario"),db_getsession("DB_modulo"),db_getsession("DB_anousu"),db_getsession("DB_instit"));
+  include("forms/db_frmisscadsimples.php");
   ?>
+</div>
+<?php
+db_menu(db_getsession("DB_id_usuario"),db_getsession("DB_modulo"),db_getsession("DB_anousu"),db_getsession("DB_instit"));
+?>
 </body>
 </html>
 <script type="text/javascript">
-js_tabulacaoforms("form1","q38_inscr",true,1,"q38_inscr",true);
+  js_tabulacaoforms("form1","q38_inscr",true,1,"q38_inscr",true);
 </script>
 <?php
 
