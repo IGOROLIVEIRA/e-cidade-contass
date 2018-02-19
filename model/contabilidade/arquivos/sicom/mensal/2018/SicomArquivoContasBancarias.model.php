c11012a261c062d179b7ba6fb38b50152da4f167
elvio <elvio@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2018-02-19 16:45:46 +0000

    Adicionada consulta de registros para o ano de 2017 para o arquivo CTB, registro 10.

--- model/contabilidade/arquivos/sicom/mensal/2018/SicomArquivoContasBancarias.model.php
+++ model/contabilidade/arquivos/sicom/mensal/2018/SicomArquivoContasBancarias.model.php
@@ -223,6 +223,9 @@ class SicomArquivoContasBancarias extends SicomArquivoBase implements iPadArquiv
           AND si95_agencia = '$oRegistro10->c63_agencia' AND si95_digitoverificadoragencia = '$oRegistro10->c63_dvagencia' AND si95_contabancaria = '$oRegistro10->c63_conta'
           AND si95_digitoverificadorcontabancaria = '$oRegistro10->c63_dvconta' AND si95_tipoconta = '$oRegistro10->tipoconta'
           AND si95_mes < " . $this->sDataFinal['5'] . $this->sDataFinal['6'] . " and si95_instit = " . db_getsession('DB_instit');
+          $sSqlVerifica .= " UNION SELECT * FROM ctb102017 WHERE si95_codorgao = '$oRegistro10->si09_codorgaotce' AND si95_banco = '$oRegistro10->c63_banco'
+          AND si95_agencia = '$oRegistro10->c63_agencia' AND si95_digitoverificadoragencia = '$oRegistro10->c63_dvagencia' AND si95_contabancaria = '$oRegistro10->c63_conta'
+          AND si95_digitoverificadorcontabancaria = '$oRegistro10->c63_dvconta' AND si95_tipoconta = '$oRegistro10->tipoconta'" . " and si95_instit = " . db_getsession('DB_instit');
           $sSqlVerifica .= " UNION SELECT * FROM ctb102016 WHERE si95_codorgao = '$oRegistro10->si09_codorgaotce' AND si95_banco = '$oRegistro10->c63_banco'
           AND si95_agencia = '$oRegistro10->c63_agencia' AND si95_digitoverificadoragencia = '$oRegistro10->c63_dvagencia' AND si95_contabancaria = '$oRegistro10->c63_conta'
           AND si95_digitoverificadorcontabancaria = '$oRegistro10->c63_dvconta' AND si95_tipoconta = '$oRegistro10->tipoconta'" . " and si95_instit = " . db_getsession('DB_instit');
