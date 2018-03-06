3fabd66db78f0ced032ffdfe3ab6dfddd38d71f9
marcelo <marcelo@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2018-03-06 16:41:42 +0000

    OC4604

--- updatedb/1520343075_OCsicomflpgo2018.sql
+++ updatedb/1520343075_OCsicomflpgo2018.sql
@@ -311,23 +311,3 @@ CONSTRAINT viap102018_sequ_pk PRIMARY KEY (si198_sequencial));
 -- INDICES
 
 COMMIT;
-
-
-BEGIN;
-
-select fc_startsession();
-
-ALTER TABLE tetoremuneratorio add column te01_nrleiteto int4 NOT NULL default 0;
-ALTER TABLE tetoremuneratorio add column te01_dtpublicacaolei date;
-ALTER TABLE tetoremuneratorio ALTER COLUMN column te01_dtfinal DROP NOT NULL;
-
-ALTER TABLE terem102013 ALTER COLUMN si194_dtfinal DROP NOT NULL;
-ALTER TABLE terem102014 ALTER COLUMN si194_dtfinal DROP NOT NULL;
-ALTER TABLE terem102015 ALTER COLUMN si194_dtfinal DROP NOT NULL;
-ALTER TABLE terem102016 ALTER COLUMN si194_dtfinal DROP NOT NULL;
-ALTER TABLE terem102017 ALTER COLUMN si194_dtfinal DROP NOT NULL;
-ALTER TABLE terem102018 ALTER COLUMN si194_dtfinal DROP NOT NULL;
-
-
-
-COMMIT;
