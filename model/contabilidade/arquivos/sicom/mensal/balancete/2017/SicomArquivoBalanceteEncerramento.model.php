923ab5ee208e7e22d1eeb8b8766054bb4d79c6bb
igor <igor@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2018-03-23 19:21:34 +0000

    Correção de registro 17 para gerar a fonte conforme foi gerado no mês 12 do ano corrente.

--- model/contabilidade/arquivos/sicom/mensal/balancete/2017/SicomArquivoBalanceteEncerramento.model.php
+++ model/contabilidade/arquivos/sicom/mensal/balancete/2017/SicomArquivoBalanceteEncerramento.model.php
@@ -266,7 +266,7 @@ class SicomArquivoBalanceteEncerramento extends SicomArquivoBase implements iPad
 						inner join conplanoreduz on c61_codcon = c60_codcon and c61_anousu = c60_anousu and c61_instit = " . db_getsession("DB_instit") . "
                         inner join conplanoexe on c62_reduz = c61_reduz and c61_anousu = c62_anousu
                         left join vinculopcasptce on substr(c60_estrut,1,9) = c209_pcaspestrut
-                             where  c60_nregobrig=17 and c60_anousu = " . db_getsession("DB_anousu") . ") as x
+                             where   c60_anousu = " . db_getsession("DB_anousu") . ") as x
                         where debito != 0 or credito != 0 or saldoinicialano != 0 order by contacontabil";
 //where c60_anousu = " . db_getsession("DB_anousu") . " and substr(c60_estrut,1,9) = '622130500') as x
 
