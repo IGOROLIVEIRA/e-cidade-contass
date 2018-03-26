8432dc6ada1ebcdf56a6af1fbf5ffa7fe33c8323
marcony <marcony@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2018-03-26 20:02:38 +0000

    OC5942 - Alteração da validacao de código

--- forms/db_frmtpcontra.php
+++ forms/db_frmtpcontra.php
@@ -1,28 +1,28 @@
 <?
 /*
- *     E-cidade Software Publico para Gestao Municipal                
- *  Copyright (C) 2009  DBselller Servicos de Informatica             
- *                            www.dbseller.com.br                     
- *                         e-cidade@dbseller.com.br                   
- *                                                                    
- *  Este programa e software livre; voce pode redistribui-lo e/ou     
- *  modifica-lo sob os termos da Licenca Publica Geral GNU, conforme  
- *  publicada pela Free Software Foundation; tanto a versao 2 da      
- *  Licenca como (a seu criterio) qualquer versao mais nova.          
- *                                                                    
- *  Este programa e distribuido na expectativa de ser util, mas SEM   
- *  QUALQUER GARANTIA; sem mesmo a garantia implicita de              
- *  COMERCIALIZACAO ou de ADEQUACAO A QUALQUER PROPOSITO EM           
- *  PARTICULAR. Consulte a Licenca Publica Geral GNU para obter mais  
- *  detalhes.                                                         
- *                                                                    
- *  Voce deve ter recebido uma copia da Licenca Publica Geral GNU     
- *  junto com este programa; se nao, escreva para a Free Software     
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA          
- *  02111-1307, USA.                                                  
- *  
- *  Copia da licenca no diretorio licenca/licenca_en.txt 
- *                                licenca/licenca_pt.txt 
+ *     E-cidade Software Publico para Gestao Municipal
+ *  Copyright (C) 2009  DBselller Servicos de Informatica
+ *                            www.dbseller.com.br
+ *                         e-cidade@dbseller.com.br
+ *
+ *  Este programa e software livre; voce pode redistribui-lo e/ou
+ *  modifica-lo sob os termos da Licenca Publica Geral GNU, conforme
+ *  publicada pela Free Software Foundation; tanto a versao 2 da
+ *  Licenca como (a seu criterio) qualquer versao mais nova.
+ *
+ *  Este programa e distribuido na expectativa de ser util, mas SEM
+ *  QUALQUER GARANTIA; sem mesmo a garantia implicita de
+ *  COMERCIALIZACAO ou de ADEQUACAO A QUALQUER PROPOSITO EM
+ *  PARTICULAR. Consulte a Licenca Publica Geral GNU para obter mais
+ *  detalhes.
+ *
+ *  Voce deve ter recebido uma copia da Licenca Publica Geral GNU
+ *  junto com este programa; se nao, escreva para a Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307, USA.
+ *
+ *  Copia da licenca no diretorio licenca/licenca_en.txt
+ *                                licenca/licenca_pt.txt
  */
 
 //MODULO: recursos humanos
@@ -35,9 +35,9 @@ $cltpcontra->rotulo->label();
     <td nowrap title="<?=@$Th13_codigo?>">
        <?=@$Lh13_codigo?>
     </td>
-    <td> 
+    <td>
 <?
-db_input('h13_codigo',2,$Ih13_codigo,true,'text',$db_opcao,"")
+db_input('h13_codigo',4,'',true,'text',$db_opcao,"","","","",6)
 ?>
     </td>
   </tr>
@@ -45,7 +45,7 @@ db_input('h13_codigo',2,$Ih13_codigo,true,'text',$db_opcao,"")
     <td nowrap title="<?=@$Th13_regime?>">
        <?=@$Lh13_regime?>
     </td>
-    <td> 
+    <td>
 <?
 $result_regime = $clrhcadregime->sql_record($clrhcadregime->sql_query_file());
 db_selectrecord("h13_regime", $result_regime, true, $db_opcao);
@@ -56,7 +56,7 @@ db_selectrecord("h13_regime", $result_regime, true, $db_opcao);
     <td nowrap title="<?=@$Th13_tpcont?>">
        <?=@$Lh13_tpcont?>
     </td>
-    <td> 
+    <td>
 <?
 db_input('h13_tpcont',2,$Ih13_tpcont,true,'text',$db_opcao,"")
 ?>
@@ -66,7 +66,7 @@ db_input('h13_tpcont',2,$Ih13_tpcont,true,'text',$db_opcao,"")
     <td nowrap title="<?=@$Th13_descr?>">
        <?=@$Lh13_descr?>
     </td>
-    <td> 
+    <td>
 <?
 db_input('h13_descr',40,$Ih13_descr,true,'text',$db_opcao,"")
 ?>
