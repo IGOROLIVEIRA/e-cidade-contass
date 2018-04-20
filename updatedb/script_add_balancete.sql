689608b03f4bd3359523249b88114839625e9169
mario <mario@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2018-04-20 20:29:16 +0000

    correção do scrip que ja existia um codigo no cliente

--- updatedb/script_add_balancete.sql
+++ updatedb/script_add_balancete.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-INSERT INTO db_itensmenu (id_item,descricao,help,funcao,itemativo,manutencao,desctec,libcliente) values (3000093,'Balancete','Balancete','con4_gerarbalancete.php',1,1,'Balancete','t');
-INSERT INTO db_menu (id_item,id_item_filho,menusequencia,modulo) values (8987,3000093,4,2000018);
+INSERT INTO db_itensmenu values ((select max(id_item) + 1 from db_itensmenu),'Balancete','Balancete','con4_gerarbalancete.php',1,1,'Balancete','t');
+INSERT INTO db_menu values (8987,(select max(id_item) from db_itensmenu),(select max(menusequencia)+1 from db_menu where id_item = 8987 and modulo = 2000018),2000018);
 
 COMMIT;
