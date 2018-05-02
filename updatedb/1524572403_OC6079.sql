e7df559c39ac7ac4f0a790e13e0e7b21aa792f65
rafhael <rafhael@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2018-05-02 12:58:35 +0000

    Ajuste do script 2

--- updatedb/1524572403_OC6079.sql
+++ updatedb/1524572403_OC6079.sql
@@ -6,7 +6,7 @@ SELECT fc_startsession();
 -- Início do script
 
 INSERT INTO db_itensmenu VALUES ((select max(id_item) + 1 from db_itensmenu), 'Relatório Financeiro', 'Protocolos', 'pro2_protocolorelfinanceiro001.php', 1, 1, 'Protocolos', 't');
-INSERT INTO db_menu VALUES ((select id_item from db_itensmenu where descricao = 'Protocolo Financeiro' and help = 'Protocolos' and funcao = '')
+INSERT INTO db_menu VALUES ((select id_item from db_itensmenu where descricao = 'Protocolo' and help = 'Protocolos' and funcao = '')
                             ,(select max(id_item) from db_itensmenu), 3, 604);
 
 -- Fim do script
