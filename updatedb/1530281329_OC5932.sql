370c07551879c9b8acb2f739de4ba0b5d430b769
rafhael <rafhael@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2018-06-29 14:29:44 +0000

    OC5932 - Correção de texto de menu

--- updatedb/1530281329_OC5932.sql
+++ updatedb/1530281329_OC5932.sql
@@ -8,10 +8,8 @@ SELECT fc_startsession();
 update db_itensmenu set descricao = 'Licitacao de Outros Orgaos', help = 'Licitacao de Outros Orgaos', desctec = 'Licitacao de Outros Orgaos'
  where descricao = 'Licitacao de Outros Orgao'
   and help = 'Licitacao de Outros Orgao'
-  and desctec = 'Licitacao de Outros Orgao'
+  and desctec = 'Licitacao de Outros Orgao';
 
 -- Fim do script
 
 COMMIT;
-
-
