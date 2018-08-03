fc8df709236c90d9ebb5417bc3420e58f9e9ddfa
felipe <felipe@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2018-08-03 14:31:01 +0000

    Correcao OC. 6986

--- updatedb/1533221262_OC6986.sql
+++ updatedb/1533221262_OC6986.sql
@@ -1,4 +1,4 @@
-select fc_startsession()
 begin;
-update db_itensmenu set funcao = 'emp2_empenhospagos001_new.php' where id_item = 9357
+select fc_startsession();
+update db_itensmenu set funcao = 'emp2_empenhospagos001_new.php' where id_item = 9357;
 commit;
