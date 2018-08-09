90da365e99890bf899f6a0a06e1c2d047f0cf95c
felipe <felipe@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2018-08-09 16:46:16 +0000

    Alteracao SQL - consulta precoReferencia

--- sic1_precoreferencia001.php
+++ sic1_precoreferencia001.php
@@ -36,8 +36,9 @@ if(isset($incluir)){
                         join pcorcamitemproc on pc81_codprocitem = pc31_pcprocitem
                         join pcorcamitem on pc31_orcamitem = pc22_orcamitem
                         join pcorcamval on pc22_orcamitem = pc23_orcamitem
-                        where pc80_codproc = $si01_processocompra group by pc23_orcamitem";
+                        where pc80_codproc = $si01_processocompra and pc23_vlrun > 0 group by pc23_orcamitem";
 
+     // die($sSql);
      $rsResult = db_query($sSql);
 
      for ($iCont = 0; $iCont < pg_num_rows($rsResult); $iCont++) {
