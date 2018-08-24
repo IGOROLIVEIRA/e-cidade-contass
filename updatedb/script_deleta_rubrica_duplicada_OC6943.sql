0b23d5bb2dad7196a43c94d7292f9fa03ec79c2a
marcelo <marcelo@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2018-08-24 17:10:35 +0000

    6943 -- script errado

--- updatedb/script_deleta_rubrica_duplicada_OC6943.sql
+++ updatedb/script_deleta_rubrica_duplicada_OC6943.sql
@@ -1,4 +1,4 @@
-BEGIN;
+/*BEGIN;
 
 select fc_startsession();
 
@@ -6,3 +6,4 @@ delete from rhpessoalmovcontabancaria where rh138_rhpessoalmov = 15512;
 delete from rhpessoalmov where rh02_seqpes = 15512;
 
 COMMIT;
+*/
