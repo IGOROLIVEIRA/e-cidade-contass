53e753499d7d3b64e8f1c79a42b7548dd67b29e8
felipe <felipe@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2019-02-07 16:05:58 +0000

    OC8458 - Correção do SicomArquivoAfast 2019

--- model/contabilidade/arquivos/sicom/mensal/flpg/2019/SicomArquivoAfast.model.php
+++ model/contabilidade/arquivos/sicom/mensal/flpg/2019/SicomArquivoAfast.model.php
@@ -108,7 +108,7 @@ class SicomArquivoAfast extends SicomArquivoBase implements iPadArquivoBaseCSV {
                            WHEN r45_situac = 4 OR r45_codafa = 'R' THEN 2
                            WHEN r45_situac = 7 OR r45_codafa = 'X' THEN 3
                            WHEN r45_codafa = 'W' THEN 4
-                           WHEN r45_situac = 6 AND rh02_tbprev <> 1 THEN 7 
+                           WHEN r45_situac = 6 AND rh02_tbprev <> 1 THEN 7
                            WHEN r45_situac = 6 AND rh02_tbprev = 1 THEN 8
                            ELSE 99
                        END AS si199_tipoafastamento
@@ -118,7 +118,7 @@ class SicomArquivoAfast extends SicomArquivoBase implements iPadArquivoBaseCSV {
             WHERE DATE_PART('YEAR',r45_dtreto) = 2019
             AND (
                                         (DATE_PART('YEAR',r45_dtafas) = 2019 and DATE_PART('MONTH',r45_dtafas) = " . $this->sDataFinal['5'] . $this->sDataFinal['6'] . ")
-                                        
+
                                     )
                 AND r45_situac <> 5
             GROUP BY r45_regist,si199_tipoafastamento
@@ -134,57 +134,57 @@ class SicomArquivoAfast extends SicomArquivoBase implements iPadArquivoBaseCSV {
 
                 $sSql = "SELECT distinct r45_regist as si199_codvinculopessoa,
                       r45_codigo as si199_codafastamento,
-                      r45_dtafas AS si199_dtinicioafastamento,     
+                      r45_dtafas AS si199_dtinicioafastamento,
                       r45_dtreto as si199_dtretornoafastamento,
                       CASE
                            WHEN r45_situac = 4 THEN 2
                            WHEN r45_situac = 4 OR r45_codafa = 'R' THEN 2
                            WHEN r45_situac = 7 OR r45_codafa = 'X' THEN 3
                            WHEN r45_codafa = 'W' THEN 4
-                           WHEN r45_situac = 6 AND rh02_tbprev <> 1 THEN 7 
+                           WHEN r45_situac = 6 AND rh02_tbprev <> 1 THEN 7
                            WHEN r45_situac = 6 AND rh02_tbprev = 1 THEN 8
                            ELSE 99
                        END AS si199_tipoafastamento,
-                      
-                      case when r45_situac = 4 and r45_codafa <> 'W' then '' 
+
+                      case when r45_situac = 4 and r45_codafa <> 'W' then ''
                       when r45_situac in (2,7) and r45_codafa <> 'W' then ''
                       when r45_codafa = 'W' then ''
                       else r45_obs
                       end as si199_dscoutrosafastamentos,
-                      
+
                       r45_situac
-                      
+
                          FROM afasta
-                         join rhpessoal on r45_regist = rh01_regist 
+                         join rhpessoal on r45_regist = rh01_regist
                          join rhpessoalmov on rh02_regist = rh01_regist and rh02_anousu = 2019
                             WHERE r45_regist = $oDados->r45_regist
-                                AND 
-                                
-                                (  
-           
-                                                r45_dtafas NOT IN           
+                                AND
+
+                                (
+
+                                                r45_dtafas NOT IN
                                                      (SELECT si199_dtinicioafastamento
                                                       FROM afast102019
                                                       WHERE si199_codvinculopessoa = $oDados->r45_regist
-                                                          AND si199_mes < " . $this->sDataFinal['5'] . $this->sDataFinal['6'] . " )  
-                 
+                                                          AND si199_mes < " . $this->sDataFinal['5'] . $this->sDataFinal['6'] . " )
+
                                                     and
-                                        
+
                                                 r45_dtreto NOT IN
                                                      (SELECT si199_dtretornoafastamento
                                                       FROM afast102019
                                                       WHERE si199_codvinculopessoa = $oDados->r45_regist
-                                                          AND si199_mes < " . $this->sDataFinal['5'] . $this->sDataFinal['6'] . " )    
-                                )  
-                                
-                                
+                                                          AND si199_mes < " . $this->sDataFinal['5'] . $this->sDataFinal['6'] . " )
+                                )
+
+
                                 AND (
                                         (DATE_PART('YEAR',r45_dtafas) = 2019 and DATE_PART('MONTH',r45_dtafas) = " . $this->sDataFinal['5'] . $this->sDataFinal['6'] . ")
-                                        
-                                    ) 
-                                
-                                
-                                
+
+                                    )
+
+
+
                                 AND (
                                         (DATE_PART('YEAR',r45_dtafas) = 2019 and DATE_PART('MONTH',r45_dtafas) = 01)
                                         or
@@ -197,12 +197,12 @@ class SicomArquivoAfast extends SicomArquivoBase implements iPadArquivoBaseCSV {
                                         (DATE_PART('YEAR',r45_dtafas) = 2014 and DATE_PART('MONTH',r45_dtafas) <= 12)
                                         or
                                         (DATE_PART('YEAR',r45_dtafas) = 2013 and DATE_PART('MONTH',r45_dtafas) <= 12)
-                                    ) 
-                                
+                                    )
+
                                 AND DATE_PART('DAY',r45_dtreto) >= 1
                                 AND DATE_PART('MONTH',r45_dtreto) >= 1
                                 AND DATE_PART('YEAR',r45_dtreto) >= 2019
-                                AND r45_situac <> 5 
+                                AND r45_situac <> 5
                                 AND rh01_instit =  " . db_getsession("DB_instit") . "
                                 AND   rh01_sicom = 1
                                 ";
@@ -210,58 +210,58 @@ class SicomArquivoAfast extends SicomArquivoBase implements iPadArquivoBaseCSV {
 
                 $sSql = "SELECT distinct r45_regist as si199_codvinculopessoa,
                       r45_codigo as si199_codafastamento,
-                      r45_dtafas AS si199_dtinicioafastamento,     
+                      r45_dtafas AS si199_dtinicioafastamento,
                       r45_dtreto as si199_dtretornoafastamento,
                       CASE
                            WHEN r45_situac = 4 THEN 2
                            WHEN r45_situac = 4 OR r45_codafa = 'R' THEN 2
                            WHEN r45_situac = 7 OR r45_codafa = 'X' THEN 3
                            WHEN r45_codafa = 'W' THEN 4
-                           WHEN r45_situac in (6,3) AND rh02_tbprev <> 1 THEN 7 
+                           WHEN r45_situac in (6,3) AND rh02_tbprev <> 1 THEN 7
                            WHEN r45_situac in (6,3) AND rh02_tbprev = 1 THEN 8
                            ELSE 99
                        END AS si199_tipoafastamento,
-                      
-                      case when r45_situac = 4 and r45_codafa <> 'W' then '' 
+
+                      case when r45_situac = 4 and r45_codafa <> 'W' then ''
                       when r45_situac in (2,7) and r45_codafa <> 'W' then ''
                       when r45_codafa = 'W' then ''
                       else r45_obs
                       end as si199_dscoutrosafastamentos,
-                      
+
                       r45_situac
-                      
+
                          FROM afasta
-                         join rhpessoal on r45_regist = rh01_regist 
+                         join rhpessoal on r45_regist = rh01_regist
                          join rhpessoalmov on rh02_regist = rh01_regist and rh02_anousu = 2019
                             WHERE r45_regist = $oDados->r45_regist
-                                AND 
-                                
-                                (  
-                                                r45_dtafas NOT IN           
+                                AND
+
+                                (
+                                                r45_dtafas NOT IN
                                                      (SELECT si199_dtinicioafastamento
                                                       FROM afast102019
                                                       WHERE si199_codvinculopessoa = $oDados->r45_regist
-                                                          AND si199_mes < " . $this->sDataFinal['5'] . $this->sDataFinal['6'] . " )  
-                 
+                                                          AND si199_mes < " . $this->sDataFinal['5'] . $this->sDataFinal['6'] . " )
+
                                                     and
-                                        
+
                                                 r45_dtreto NOT IN
                                                      (SELECT si199_dtretornoafastamento
                                                       FROM afast102019
                                                       WHERE si199_codvinculopessoa = $oDados->r45_regist
-                                                          AND si199_mes < " . $this->sDataFinal['5'] . $this->sDataFinal['6'] . " )    
-                                )  
-                                
-                                
+                                                          AND si199_mes < " . $this->sDataFinal['5'] . $this->sDataFinal['6'] . " )
+                                )
+
+
                                 AND (
                                         (DATE_PART('YEAR',r45_dtafas) = 2019 and DATE_PART('MONTH',r45_dtafas) = " . $this->sDataFinal['5'] . $this->sDataFinal['6'] . ")
-                                        
-                                    ) 
-                                
+
+                                    )
+
                                 AND DATE_PART('DAY',r45_dtreto) >= 1
                                 AND DATE_PART('MONTH',r45_dtreto) >= 1
                                 AND DATE_PART('YEAR',r45_dtreto) >= 2019
-                                AND r45_situac <> 5 
+                                AND r45_situac <> 5
                                 AND rh01_instit =  " . db_getsession("DB_instit") . "
                                 AND   rh01_sicom = 1
                                 ";
@@ -349,20 +349,20 @@ class SicomArquivoAfast extends SicomArquivoBase implements iPadArquivoBaseCSV {
                   WHERE si199_codvinculopessoa = $oDados->r45_regist) as codafastamento,
                       r45_dtafas as dtinicioafastamento,
                       r45_dtreto as dtretornoafastamento,
-                      case when r45_situac = 4 and r45_codafa <> 'W' then 2 
+                      case when r45_situac = 4 and r45_codafa <> 'W' then 2
                       when r45_situac in (2,7) and r45_codafa <> 'W' then 3
                       when r45_codafa = 'W' then 4
                       else 99
                       end as tipoafastamento,
-                      
-                      case when r45_situac = 4 and r45_codafa <> 'W' then '' 
+
+                      case when r45_situac = 4 and r45_codafa <> 'W' then ''
                       when r45_situac in (2,7) and r45_codafa <> 'W' then ''
                       when r45_codafa = 'W' then ''
                       else r45_obs
                       end as dscoutrosafastamentos
-                         FROM afasta 
+                         FROM afasta
                             WHERE r45_regist = $oDados->r45_regist
-                                AND 
+                                AND
                                 (
                                               r45_regist IN
                                                     (SELECT si199_codvinculopessoa
@@ -373,9 +373,9 @@ class SicomArquivoAfast extends SicomArquivoBase implements iPadArquivoBaseCSV {
                                                                                       WHERE si199_codvinculopessoa = $oDados->r45_regist)
                                                          and si199_mes < " . $this->sDataFinal['5'] . $this->sDataFinal['6'] . "
                                                          order by si199_codafastamento desc limit 1)
-                                
+
                                                      AND
-                                                     
+
                                                      r45_dtreto < (SELECT si199_dtretornoafastamento
                                                      FROM afast102019
                                                      WHERE si199_codvinculopessoa = $oDados->r45_regist
@@ -383,11 +383,11 @@ class SicomArquivoAfast extends SicomArquivoBase implements iPadArquivoBaseCSV {
                                                                                       FROM afast102019
                                                                                       WHERE si199_codvinculopessoa = $oDados->r45_regist
                                                                                       order by si199_codafastamento desc limit 1)
-                                                         and si199_mes < " . $this->sDataFinal['5'] . $this->sDataFinal['6'] . " limit 1 )  
+                                                         and si199_mes < " . $this->sDataFinal['5'] . $this->sDataFinal['6'] . " limit 1 )
                                 )
-                                
+
                                 AND r45_regist::varchar||r45_dtreto not in (select si200_codvinculopessoa::varchar||si200_dtterminoafastamento from afast202019 )
-                                
+
                                 AND DATE_PART('DAY',r45_dtreto) >= 2
                                 AND DATE_PART('MONTH',r45_dtreto) >= 1
                                 AND DATE_PART('YEAR',r45_dtreto) = 2019
@@ -418,15 +418,23 @@ class SicomArquivoAfast extends SicomArquivoBase implements iPadArquivoBaseCSV {
 
                 }
             }
+          }
+
+
+
+           for ($iCont = 0; $iCont < pg_num_rows($rsResult); $iCont++) {
+
+            $oDados = db_utils::fieldsMemory($rsResult, $iCont);
+
             if($oDados->si199_tipoafastamento == 8 || $oDados->si199_tipoafastamento == 7){
 
                 $sSql = "SELECT distinct r45_regist as codvinculopessoa,
                   10||r45_codigo as codafastamento,
                   r45_dtafas as dtinicioafastamento,
                   r45_dtreto as dtretornoafastamento
-                     FROM afasta 
+                     FROM afasta
                         WHERE r45_regist = $oDados->r45_regist
-                            AND 
+                            AND
                             (
                                           r45_regist IN
                                                 (SELECT si199_codvinculopessoa
@@ -439,11 +447,11 @@ class SicomArquivoAfast extends SicomArquivoBase implements iPadArquivoBaseCSV {
                                                  r45_dtreto <= (SELECT si199_dtretornoafastamento
                                                  FROM afast102019
                                                  WHERE si199_codvinculopessoa = $oDados->r45_regist
-                                                     and si199_mes <= " . $this->sDataFinal['5'] . $this->sDataFinal['6'] . " )  
+                                                     and si199_mes <= " . $this->sDataFinal['5'] . $this->sDataFinal['6'] . " )
                             )
-                            
+
                             AND r45_regist::varchar||r45_dtreto not in (select si200_codvinculopessoa::varchar||si200_dtterminoafastamento from afast202019 )
-                            
+
                             AND DATE_PART('DAY',r45_dtreto) >= 2
                             AND DATE_PART('MONTH',r45_dtreto) >= 1
                             AND DATE_PART('YEAR',r45_dtreto) = 2019
@@ -451,7 +459,9 @@ class SicomArquivoAfast extends SicomArquivoBase implements iPadArquivoBaseCSV {
 
                 $rsResult3 = db_query($sSql);//echo $sSql;db_criatabela($rsResult3);
 
-                $oDadosAfast3 = db_utils::fieldsMemory($rsResult3, $iCont2);
+
+                for ($iCont3 = 0; $iCont3 < pg_num_rows($rsResult2); $iCont3++) {
+                $oDadosAfast3 = db_utils::fieldsMemory($rsResult3, $iCont3);
 
                 if ( pg_num_rows($rsResult3) > 0 ) {
 
@@ -463,21 +473,19 @@ class SicomArquivoAfast extends SicomArquivoBase implements iPadArquivoBaseCSV {
                     $clafast->si201_dtretornoafastamento = $oDadosAfast3->dtretornoafastamento;
                     $clafast->si201_mes = $this->sDataFinal['5'] . $this->sDataFinal['6'];
                     $clafast->si201_inst = db_getsession("DB_instit");
-
                     $clafast->incluir(null);
                     if ($clafast->erro_status == 0) {
                         throw new Exception($clafast->erro_msg);
                     }
 
+                  }
                 }
+              }
             }
 
 
             db_fim_transacao();
 
-
-
-        }
 
         $oGerarAfast = new GerarAFAST();
         $oGerarAfast->iMes = $this->sDataFinal['5'] . $this->sDataFinal['6'];
