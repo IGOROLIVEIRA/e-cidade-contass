0e2c0bdffe7cd93895f7caaee6879481ccd5b155
vitor.pereira <vitor.pereira@e58ba742-51ee-46bf-bb4e-4b6ea8db88bb>
2019-03-25 19:30:11 +0000

    OC8822 - Solicitação de alteração: Relatório Repasse Câmara

--- con2_repassecamara011.php
+++ con2_repassecamara011.php
@@ -44,28 +44,8 @@ $anousu = db_getsession("DB_anousu");
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
     <meta http-equiv="Expires" CONTENT="0">
     <script language="JavaScript" type="text/javascript" src="scripts/scripts.js"></script>
-
-    <script>
-        variavel = 1;
-        function js_emite(tipo) {
-            sel_instit = new Number(document.form1.db_selinstit.value);
-            if (sel_instit == 0) {
-                alert('Você não escolheu nenhuma Instituição. Verifique!');
-                return false;
-            }
-            if (tipo == 2) {
-                document.form1.action = "orc2_despitemdesdobramento002_csv.php";
-            } else {
-                document.form1.action = "con2_repassecamara002.php";
-            }
-            // pega dados da func_selorcdotacao_aba.php
-            jan = window.open('', 'safo' + variavel, 'width=' + (screen.availWidth - 5) + ',height=' + (screen.availHeight - 40) + ',scrollbars=1,location=0 ');
-            document.form1.target = 'safo' + variavel++;
-            setTimeout("document.form1.submit()", 1000);
-            return true;
-        }
-    </script>
     <link href="estilos.css" rel="stylesheet" type="text/css">
+    <?php db_app::load('prototype.js, scripts.js, strings.js'); ?>
 </head>
 <body bgcolor=#CCCCCC leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" onLoad="a=1" bgcolor="#cccccc">
 <table align="center">
@@ -96,12 +76,54 @@ $anousu = db_getsession("DB_anousu");
         </tr>
         <tr>
             <td colspan="2" align="center">
-                <input name="emiterel" value="Gera Relatório" type="button" onclick="js_emite(1);">
+                <input name="emiterel" value="Gera Relatório" type="button" onclick="js_consultaDataDaSessao(1);">
                 <!--<input name="emiterelcsv" value="Gera CSV" type="button" onclick="js_emite(2);">-->
             </td>
         </tr>
 
     </form>
 </table>
+
+<script>
+  variavel = 1;
+
+  function js_consultaDataDaSessao(iTipo){
+
+    let oParams = new Object()
+    oParams.iTipo = iTipo
+    oParams.sExec = 'consultarDataDaSessao'
+
+    var oAjax   = new Ajax.Request( 'con2_repassecamaraRPC.php', {
+        method: 'post',
+        parameters: 'json='+JSON.stringify(oParams),
+        onComplete: js_emite
+      }
+    );
+  }
+
+  function js_emite(oParams) {
+    const { iAno, iTipo } = JSON.parse(oParams.responseText)
+
+    sel_instit = new Number(document.form1.db_selinstit.value);
+    if (sel_instit == 0) {
+      alert('Você não escolheu nenhuma Instituição. Verifique!');
+      return false;
+    }
+
+    document.form1.action = "con2_repassecamara002.php";
+    if(iAno <= 2018){
+      document.form1.action = "con2_repassecamara002_anterior_a_2019.php";
+    }
+
+    if (iTipo == 2) {
+      document.form1.action = "orc2_despitemdesdobramento002_csv.php";
+    }
+    // pega dados da func_selorcdotacao_aba.php
+    jan = window.open('', 'safo' + variavel, 'width=' + (screen.availWidth - 5) + ',height=' + (screen.availHeight - 40) + ',scrollbars=1,location=0 ');
+    document.form1.target = 'safo' + variavel++;
+    setTimeout("document.form1.submit()", 1000);
+    return true;
+  }
+</script>
 </body>
 </html>
