98f54cfbca52c04e8aaae8bddfed9a9bc9716948
Victor Oliveira <felipe.oliveira@contassconsultoria.com.br>
2019-08-01 17:09:19 -0300

    OC10204 - Retorno do valor anterior do Multicell

--- fpdf151/impmodelos/mod_imprime42.php
+++ fpdf151/impmodelos/mod_imprime42.php
@@ -40,7 +40,7 @@ $this->objpdf->setXY(12, $this->objpdf->getY());
 $this->objpdf->cell(0, 0, ' ' , 0, 1, 'L',0,'', '-');
 $this->objpdf->ln(1);
 $this->objpdf->Setfont("times", "B", 12);
-$this->objpdf->cell(35, 6, "Protocolo Nº: ", 0, 0, 'R');
+$this->objpdf->cell(35, 6, "Protocolo N: ", 0, 0, 'R');
 $this->objpdf->Setfont("times", "", 11);
 $this->objpdf->cell(30, 6, $sNumeroProtocolo, 0, 1, 'L');
 $this->objpdf->Setfont("times", "B", 12);
@@ -81,14 +81,14 @@ if (trim($this->p58_requer ) != trim($this->z01_nome )) {
 
 
 $this->objpdf->Setfont("times", "B", 12);
-$this->objpdf->cell(35, 6, "Endereço: ", 0, 0, 'R');
+$this->objpdf->cell(35, 6, "Endereo: ", 0, 0, 'R');
 $this->objpdf->Setfont("times", "", 11);
 $this->objpdf->cell(75, 6, $this->z01_ender . ($this->z01_numero  != "" ? ", " : "").$this->z01_numero .
   ($this->z01_compl  != "" ? " - " : "").$this->z01_compl.($this->z01_bairro != "" ? ", " : "").$this->z01_bairro, 0, 1, 'L');
 
 
 $this->objpdf->Setfont("times", "B", 12);
-$this->objpdf->cell(35, 6, "Município: ", 0, 0, 'R');
+$this->objpdf->cell(35, 6, "Municpio: ", 0, 0, 'R');
 $this->objpdf->Setfont("times", "", 11);
 $this->objpdf->cell(75, 6, $this->z01_munic , 0, 1, 'L');
 $this->objpdf->Setfont("times", "B", 30);
@@ -99,7 +99,7 @@ $this->objpdf->cell(0, 0, ' ' , 0, 1, 'L',0,'', '-');
 // $this->objpdf->sety(92);
 $this->objpdf->Setfont("times", "B", 12);
 $this->objpdf->setxy(13, $this->objpdf->gety()+4);
-$this->objpdf->cell(50, 6, "Observações: ", 0, 1, 'L');
+$this->objpdf->cell(50, 6, "Observaes: ", 0, 1, 'L');
 $this->objpdf->Setfont("times", "", 10);
 
 $result_impusu = pg_exec("select p90_impusuproc from protparam where p90_instit=".db_getsession("DB_instit"));
@@ -108,7 +108,7 @@ if (pg_numrows($result_impusu) > 0) {
   if ($p90_impusuproc == 't') {
     if ($this->nome  != "") {
       $this->objpdf->Setfont("Times", "B", 12);
-      $this->objpdf->cell(90, 5, ("USUÁRIO QUE CRIOU O PROCESSO: "), 0, 0, 'L');
+      $this->objpdf->cell(90, 5, ("USURIO QUE CRIOU O PROCESSO: "), 0, 0, 'L');
       $this->objpdf->Setfont("Times", "", 11);
       $this->objpdf->cell(75, 5, $this->nome , 0, 1, 'L');
     }
@@ -128,7 +128,7 @@ if (pg_num_rows($resproc)) {
   $impdepto = pg_result($resultdepto,0,0);
   if ($impdepto == 't') {
     $this->objpdf->setx(13);
-    $this->objpdf->cell(0, 6, "DEPARTAMENTO PADRÃO: $coddepto - $descrdepto", 0, 1, 'L', 1);
+    $this->objpdf->cell(0, 6, "DEPARTAMENTO PADRO: $coddepto - $descrdepto", 0, 1, 'L', 1);
   }
 }
 
@@ -137,7 +137,7 @@ $this->objpdf->Setfont("times", "", 10);
 $this->objpdf->setx(13);
 
 $texto = db_formatatexto(strlen($this->p58_obs)*0.03, 0, $this->p58_obs, "t");
-$this->objpdf->multicell(0, 5,$texto,0,1,"L");
+$this->objpdf->multicell(185, 5,$texto,0,1,"L");
 
 // Variaveis
 if ($this->result_vars != ""){
@@ -185,7 +185,7 @@ if ($numrows_doc > 0) {
 //-------------------------------------------------------------------------------------------------
 $this->objpdf->Setfont("Times", "", 12);
 $this->objpdf->SetXY(60, 210);
-$this->objpdf->cell(60, 8, "Instituição", 'T', 0, 'C');
+$this->objpdf->cell(60, 8, "Instituio", 'T', 0, 'C');
 $this->objpdf->cell(20);
 $this->objpdf->cell(50, 8, "Requerente / Titular", 'T', 1, 'C');
 $this->objpdf->SetXY(140, 220);
