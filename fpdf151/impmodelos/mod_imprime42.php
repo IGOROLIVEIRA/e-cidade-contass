4bd21b51c34d0b773bc51f07db4ccae6f6ef06a7
Victor Felipe <felipe.oliveira@contassconsultoria.com.br>
2019-09-20 11:13:55 -0300

    OC10204 - Implementação do novo modelo de capa de processo

--- fpdf151/impmodelos/mod_imprime42.php
+++ fpdf151/impmodelos/mod_imprime42.php
@@ -10,18 +10,20 @@ if(strlen(trim($this->telefinstit)) == 10 ){
 
 $this->objpdf->sety(20);
 $this->objpdf->setx(85);
-$this->objpdf->Setfont("times", "", 10);
-if($telefone)
-  $this->objpdf->cell(40, 5, "Telefone: ".$telefone, 0, 0, 'R');
-
+$this->objpdf->Setfont("times", "b", 10);
+if($telefone){
+  $this->objpdf->cell(40, 5, "TELEFONE: ".$telefone, 0, 0, 'R');
+}
 
 $this->objpdf->sety(10);
 $this->objpdf->setx(170);
 $this->objpdf->Setfont("times", "B", 12);
-$sNumeroProtocolo = $this->p58_numero."/".$this->p58_ano;
-$this->objpdf->cell(20, 5, $sNumeroProtocolo, 0, 0, 'L');
+$this->objpdf->cell(20, 5, $this->p58_numero, 0, 0, 'R');
+$this->objpdf->setxy(180, 15);
+$this->objpdf->cell(20, 5, '/'.$this->p58_ano, 0, 0, 'L');
 $descrdepto = pg_result($resproc,0,1);
 
+$sNumeroProtocolo = $this->p58_numero."/".$this->p58_ano;
 $this->objpdf->sety(15);
 $this->objpdf->setx(160);
 
@@ -33,73 +35,73 @@ $sqlproc = "select coddepto,descrdepto,p51_descr
 $resproc = pg_query($sqlproc);
 $descrdepto = pg_result($resproc,0,1);
 
+$this->objpdf->setLeftMargin(17);
 $this->objpdf->sety(40);
 $this->objpdf->Setfont("times", "B", 30);
-$this->objpdf->cell(0, 0, ' ' , 0, 0, 'L',0,'', '-');
-$this->objpdf->setXY(12, $this->objpdf->getY());
-$this->objpdf->cell(0, 0, ' ' , 0, 1, 'L',0,'', '-');
+$this->objpdf->rect(15, 40, 178, 60, '');
 $this->objpdf->ln(1);
 $this->objpdf->Setfont("times", "B", 12);
-$this->objpdf->cell(35, 6, "Protocolo N: ", 0, 0, 'R');
+
+$this->objpdf->cell(36, 6, "PROTOCOLO Nº: ", 0, 0, 'L');
 $this->objpdf->Setfont("times", "", 11);
 $this->objpdf->cell(30, 6, $sNumeroProtocolo, 0, 1, 'L');
 $this->objpdf->Setfont("times", "B", 12);
-$this->objpdf->Cell(35, 6, "Assunto: ", 0, 0, "R");
+
+$this->objpdf->Cell(23, 6, "ASSUNTO: ", 0, 0, "L");
 $this->objpdf->Setfont("times", "", 11);
 $this->objpdf->multicell(100, 6, $this->p51_descr , 0, 1, 'L');
 
 if ($this->p58_dtproc  != "") {
   $this->objpdf->Setfont("times", "B", 12);
-  $this->objpdf->cell(35, 6, ("Data: "), 0, 0, 'R');
+  $this->objpdf->cell(15, 6, ("DATA: "), 0, 0, 'L');
   $this->objpdf->Setfont("times", "", 11);
   $this->objpdf->cell(75, 6, db_formatar($this->p58_dtproc , 'd'), 0, 1, 'L');
 }
 
 $this->objpdf->Setfont("times", "B", 12);
-$this->objpdf->cell(35, 6, "Departamento: ", 0, 0, 'R');
+$this->objpdf->cell(40, 6, "DEPARTAMENTO: ", 0, 0, 'L');
 $this->objpdf->Setfont("times", "", 11);
 $this->objpdf->cell(75, 6, $descrdepto , 0, 1, 'L');
 
 $this->objpdf->Setfont("times", "B", 12);
-$this->objpdf->cell(35, 6, "Titular: ", 0, 0, 'R');
+$this->objpdf->cell(23, 6, "TITULAR: ", 0, 0, 'L');
 $this->objpdf->Setfont("times", "", 11);
 $this->objpdf->cell(75, 6, $this->z01_nome , 0, 1, 'L');
 
-if ($this->z01_cgccpf  != "") {
+if ($this->z01_cgccpf != "") {
   $this->objpdf->Setfont("times", "B", 12);
-  $this->objpdf->cell(35, 6, (strlen($this->z01_cgccpf ) == 11 ? "CPF: " : "CNPJ: "), 0, 0, 'R');
+  $this->objpdf->cell(13, 6, (strlen($this->z01_cgccpf ) == 11 ? "CPF: " : "CNPJ: "), 0, 0, 'L');
   $this->objpdf->Setfont("times", "", 11);
   $this->objpdf->cell(40, 6, $this->z01_cgccpf , 0, 1, 'L');
 }
 
 if (trim($this->p58_requer ) != trim($this->z01_nome )) {
   $this->objpdf->Setfont("times", "B", 12);
-  $this->objpdf->cell(35, 6, "Requerente: ", 0, 0, 'R');
+  $this->objpdf->cell(34, 6, "REQUERENTE: ", 0, 0, 'L');
   $this->objpdf->Setfont("times", "", 11);
   $this->objpdf->cell(75, 6, $this->p58_requer , 0, 1, 'L');
 }
 
 
 $this->objpdf->Setfont("times", "B", 12);
-$this->objpdf->cell(35, 6, "Endereço: ", 0, 0, 'R');
+$this->objpdf->cell(28, 6, "ENDEREÇO: ", 0, 0, 'L');
 $this->objpdf->Setfont("times", "", 11);
 $this->objpdf->cell(75, 6, $this->z01_ender . ($this->z01_numero  != "" ? ", " : "").$this->z01_numero .
   ($this->z01_compl  != "" ? " - " : "").$this->z01_compl.($this->z01_bairro != "" ? ", " : "").$this->z01_bairro, 0, 1, 'L');
 
 
 $this->objpdf->Setfont("times", "B", 12);
-$this->objpdf->cell(35, 6, "Município: ", 0, 0, 'R');
+$this->objpdf->cell(28, 6, "MUNICÍPIO: ", 0, 0, 'L');
 $this->objpdf->Setfont("times", "", 11);
 $this->objpdf->cell(75, 6, $this->z01_munic , 0, 1, 'L');
 $this->objpdf->Setfont("times", "B", 30);
-$this->objpdf->cell(0, 0, ' ' , 0, 0, 'L',0,'', '-');
-$this->objpdf->setXY(12, $this->objpdf->getY());
-$this->objpdf->cell(0, 0, ' ' , 0, 1, 'L',0,'', '-');
 
-// $this->objpdf->sety(92);
+$this->objpdf->Setfont("times", "B", 30);
+$this->objpdf->rect(15, 110, 178, 120, '');
+
 $this->objpdf->Setfont("times", "B", 12);
-$this->objpdf->setxy(13, $this->objpdf->gety()+4);
-$this->objpdf->cell(50, 6, "Observações: ", 0, 1, 'L');
+$this->objpdf->setxy(17, $this->objpdf->gety()+17);
+$this->objpdf->cell(50, 6, "OBSERVAÇÕES: ", 0, 1, 'L');
 $this->objpdf->Setfont("times", "", 10);
 
 $result_impusu = pg_exec("select p90_impusuproc from protparam where p90_instit=".db_getsession("DB_instit"));
@@ -108,7 +110,7 @@ if (pg_numrows($result_impusu) > 0) {
   if ($p90_impusuproc == 't') {
     if ($this->nome  != "") {
       $this->objpdf->Setfont("Times", "B", 12);
-      $this->objpdf->cell(90, 5, ("USUÁRIO QUE CRIOU O PROCESSO: "), 0, 0, 'L');
+      $this->objpdf->cell(77, 5, ("USUÁRIO QUE CRIOU O PROCESSO: "), 0, 0, 'L');
       $this->objpdf->Setfont("Times", "", 11);
       $this->objpdf->cell(75, 5, $this->nome , 0, 1, 'L');
     }
@@ -137,7 +139,8 @@ $this->objpdf->Setfont("times", "", 10);
 $this->objpdf->setx(13);
 
 $texto = db_formatatexto(strlen($this->p58_obs)*0.03, 0, $this->p58_obs, "t");
-$this->objpdf->multicell(185, 5,$texto,0,1,"L");
+$this->objpdf->setx(17);
+$this->objpdf->multicell(174, 5, str_replace(',', ', ', $texto), 0, 1, "L");
 
 // Variaveis
 if ($this->result_vars != ""){
@@ -184,11 +187,17 @@ if ($numrows_doc > 0) {
 }
 //-------------------------------------------------------------------------------------------------
 $this->objpdf->Setfont("Times", "", 12);
-$this->objpdf->SetXY(60, 210);
-$this->objpdf->cell(60, 8, "Instituição", 'T', 0, 'C');
-$this->objpdf->cell(20);
-$this->objpdf->cell(50, 8, "Requerente / Titular", 'T', 1, 'C');
-$this->objpdf->SetXY(140, 220);
-$this->objpdf->cell(50, 8, "____/____/_______", 0, 1, 'R');
+$this->objpdf->rect(15, 240, 89, 25, '');
+$this->objpdf->SetXY(15, 240);
+$this->objpdf->cell(89, 25, "Requerente / Titular", 0, 0, 'C');
+$this->objpdf->SetXY(18, 239);
+$this->objpdf->cell(80, 20, " ", 0, 1, 'L', 0, '', '_');
+$this->objpdf->rect(104, 240, 89, 25, '');
+$this->objpdf->SetXY(104, 240);
+$this->objpdf->cell(89, 25, "Instituição", 0, 0, 'C');
+$this->objpdf->SetXY(108, 239);
+$this->objpdf->cell(80, 20, " ", 0, 1, 'L', 0, '', '_');
+$this->objpdf->SetXY(120, 258);
+$this->objpdf->cell(50, 8, "Data ____/____/_______", 0, 1, 'R');
 
 ?>
