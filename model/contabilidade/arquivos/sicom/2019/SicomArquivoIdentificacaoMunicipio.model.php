fd9fd3a70de3bb90e2dc8d05df94a54abd31c1e0
marcelo <marcelo@contassconsultoria.com.br>
2019-11-22 11:08:17 -0300

    teste OC11052

--- model/contabilidade/arquivos/sicom/2019/SicomArquivoIdentificacaoMunicipio.model.php
+++ model/contabilidade/arquivos/sicom/2019/SicomArquivoIdentificacaoMunicipio.model.php
@@ -95,6 +95,7 @@ class SicomArquivoIdentificacaoMunicipio extends SicomArquivoBase implements iPa
     $sSql .= "	WHERE prefeitura = 't'";
 
 	$rsInst = db_query($sSql);
+  echo $sSql;
   db_criatabela($rsInst);
 	$oInst = db_utils::fieldsMemory($rsInst, 0);
 
