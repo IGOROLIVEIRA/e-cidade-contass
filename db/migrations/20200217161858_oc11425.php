9c51fb38f1ea44f94045e2da0eda6892354dac86
Marcelo <marcelo@contassconsultoria.com.br>
2020-02-18 08:15:21 -0300

    continuando

--- db/migrations/20200217161858_oc11425.php
+++ db/migrations/20200217161858_oc11425.php
@@ -7,14 +7,31 @@ class Oc11425 extends AbstractMigration
     public function up()
     {
         $sql = <<<SQL
-
-        ALTER TABLE bpdcasp102019 DROP COLUMN si208_vlativonaocircucredilongoprazo;
-        ALTER TABLE bpdcasp102019 DROP COLUMN si208_vlativonaocircuinvestemplongpraz;
-        ALTER TABLE bpdcasp102019 DROP COLUMN si208_vlativonaocircuestoques;
-        ALTER TABLE bpdcasp102019 DROP COLUMN si208_vlativonaocircuvpdantecipada;
         
-        ALTER TABLE bpdcasp102019 DROP COLUMN si208_vlativonaocircurlp type ;
+        BEGIN;
+        
+        DROP TABLE bpdcasp102019;
+        
+        CREATE TABLE bpdcasp102019 (
+          si208_sequencial integer DEFAULT 0 NOT NULL,
+          si208_tiporegistro integer DEFAULT 0 NOT NULL,
+          si208_exercicio integer DEFAULT 0 NOT NULL,
+          si208_vlativocircucaixaequicaixa double precision DEFAULT 0 NOT NULL,
+          si208_vlativocircucredicurtoprazo double precision DEFAULT 0 NOT NULL,
+          si208_vlativocircuinvestapliccurtoprazo double precision DEFAULT 0 NOT NULL,
+          si208_vlativocircuestoques double precision DEFAULT 0 NOT NULL,
+          si208_vlativocircuvpdantecipada double precision DEFAULT 0 NOT NULL,
+          si208_vlativonaocircurlp double precision DEFAULT 0 NOT NULL,
+          si208_vlativonaocircuinvestimentos double precision DEFAULT 0 NOT NULL,
+          si208_vlativonaocircuimobilizado double precision DEFAULT 0 NOT NULL,
+          si208_vlativonaocircuintagivel double precision DEFAULT 0 NOT NULL,
+          si208_vltotalativo double precision DEFAULT 0,
+          si208_ano integer DEFAULT 0 NOT NULL,
+          si208_periodo integer DEFAULT 0 NOT NULL,
+          si208_institu integer DEFAULT 0 NOT NULL
+      );
 
+      COMMIT;
 SQL;
         $this->execute($sql);
     }
