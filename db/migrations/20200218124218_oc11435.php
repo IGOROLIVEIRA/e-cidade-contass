ffe356b435c05b00ed8261680913532d278bbd72
Marcelo <marcelo@contassconsultoria.com.br>
2020-02-18 11:03:08 -0300

    ajustando migration

--- db/migrations/20200218124218_oc11435.php
+++ db/migrations/20200218124218_oc11435.php
@@ -4,29 +4,35 @@ use Phinx\Migration\AbstractMigration;
 
 class Oc11435 extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
-    public function change()
+    public function up()
     {
+        $sql = <<<SQL
+        
+        BEGIN;
+        
+        DROP TABLE dfcdcasp102019;
+        
+        CREATE TABLE dfcdcasp102019 (
+          si219_sequencial integer DEFAULT 0 NOT NULL,
+          si219_tiporegistro integer DEFAULT 0 NOT NULL,
+          
+          si219_vlreceitatributaria double precision DEFAULT 0 NOT NULL,
+          si219_vlreceitacontribuicao double precision DEFAULT 0 NOT NULL,
+          si219_vlreceitapatrimonial double precision DEFAULT 0 NOT NULL,
+          si219_vlreceitaagropecuaria double precision DEFAULT 0 NOT NULL,
+          si219_vlreceitaindustrial double precision DEFAULT 0 NOT NULL,
+          si219_vlreceitaservicos double precision DEFAULT 0 NOT NULL,
+          si219_vlremuneracaodisponibilidade double precision DEFAULT 0 NOT NULL,
+          si219_vltransferenciarecebidas double precision DEFAULT 0 NOT NULL,
+          
+          si219_vltotalingressosativoperacionais double precision DEFAULT 0,
+          si219_anousu integer DEFAULT 0 NOT NULL,
+          si219_periodo integer DEFAULT 0 NOT NULL,
+          si219_instit integer DEFAULT 0 NOT NULL
+      );
 
+      COMMIT;
+SQL;
+        $this->execute($sql);
     }
 }
