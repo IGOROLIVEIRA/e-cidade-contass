9eefcff46f66682e13043edbe1fe0000c84a24a7
Wanderson Alves <wandersonalb@gmail.com>
2020-03-11 09:39:11 -0300

    OC11552 - Cria consultas para encerramento msc

--- con4_gerarmsc.php
+++ con4_gerarmsc.php
@@ -9,7 +9,7 @@ require_once("dbforms/db_funcoes.php");
 require_once("libs/JSON.php");
 
 $aMeses = array(
-  "01" => "Janeiro", "02" => "Fevereiro", "03" => "Março", "04" => "Abril", "05" => "Maio", "06" => "Junho", "07" => "Julho", "08" => "Agosto", "09" => "Setembro", "10" => "Outubro", "11" => "Novembro", "12" => "Dezembro"
+  "01" => "Janeiro", "02" => "Fevereiro", "03" => "Março", "04" => "Abril", "05" => "Maio", "06" => "Junho", "07" => "Julho", "08" => "Agosto", "09" => "Setembro", "10" => "Outubro", "11" => "Novembro", "12" => "Dezembro", "13" => "Encerramento"
 );
 
 ?>
