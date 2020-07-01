6a5862ac11b9dabc159ad82d32c4e5f77449be5e
marcelo <marcelo@contassconsultoria.com.br>
2020-07-01 16:35:54 -0300

    notask

--- db/migrations/20200616134146_oc12528.php
+++ db/migrations/20200616134146_oc12528.php
@@ -8,15 +8,14 @@ class Oc12528 extends AbstractMigration
     {
         $sql = "
 
-        INSERT INTO db_itensmenu VALUES ((SELECT max(id_item)+1 FROM db_itensmenu), 'AlteraÃ§Ã£o de Documento', 'AlteraÃ§Ã£o de Documento', 'm4_altdocumento.php', 1, 1, 'AlteraÃ§Ã£o de Documento', 't');
+        INSERT INTO db_itensmenu VALUES ((SELECT max(id_item)+1 FROM db_itensmenu), 'Alteração de Documento', 'Alteração de Documento', 'm4_altdocumento.php', 1, 1, 'Alteração de Documento', 't');
 
         INSERT INTO db_menu VALUES (
-            (SELECT id_item from db_itensmenu where descricao = 'ManutenÃ§Ã£o de dados'), 
+            (SELECT id_item from db_itensmenu where descricao = 'Manutenção de dados'), 
             (SELECT max(id_item) FROM db_itensmenu), 
             (SELECT max(menusequencia)+1 as count FROM db_menu  WHERE id_item = 32), 
             (1));
 ";
-
         $this->execute($sql);
     }
 }
