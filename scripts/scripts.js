4341c4d4e3bbb0f569e61c8ca7fe6876a0279a58
MarioJunior <mario210755@gmail.com>
2020-09-01 13:32:47 -0300

    OC13120 - adicionada validação a inclusao de situacao de obra e ajustado tambenho do gif carregando.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3006,7 +3006,7 @@ function js_divCarregando(mensagem,id, lBloqueia){
                      +'        '+mensagem+''
                      +'      </td> '
                      +'      <td > '
-                     +'        <img src="imagens/files/loading.gif" /> '
+                     +'        <img src="imagens/files/loading.gif" height="25" width="25"/> '
                      +'      </td> '
                      +'    </tr> '
                      +' </table> ';
