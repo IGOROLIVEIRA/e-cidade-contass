e7c1ba244bcd036c10bd13c84f49b8ff749a725c
Wanderson Alves <wanderson.alves@contassconsultoria.com.br>
2020-10-26 10:06:31 -0300

    OC12846 - Cria novo menu para relatório com as modificações

--- db/migrations/20200821181334_oc12846.php
+++ db/migrations/20200821181334_oc12846.php
@@ -14,6 +14,10 @@ class Oc12846 extends PostgresMigration
         
         UPDATE conhistdoc SET c53_tipo = 201 WHERE c53_coddoc = 215;
 
+        INSERT INTO db_itensmenu VALUES ((SELECT max(id_item)+1 FROM db_itensmenu),'Despesa por Item/Desdobramento - PBH' ,'Despesa por Item/Desdobramento - PBH' ,'orc2_despitemdesdobramentopbh001.php' ,'1' ,'1' ,'' ,'true' );
+
+        INSERT INTO db_menu VALUES (4189, (SELECT id_item FROM db_itensmenu WHERE descricao = 'Despesa por Item/Desdobramento - PBH' LIMIT 1), (SELECT max(menusequencia)+1 FROM db_menu WHERE id_item = 4189), 209);
+
         COMMIT;
 
 SQL;
