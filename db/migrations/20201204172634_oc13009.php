e34cc244df3b4795408186db5063ab73aa164361
Felipe <felipe.oliveira@contassconsultoria.com.br>
2020-12-08 08:57:22 -0300

    OC13009 - Acentuação do menu Licitação de Outros Órgãos

--- db/migrations/20201204172634_oc13009.php
+++ db/migrations/20201204172634_oc13009.php
@@ -7,7 +7,11 @@ class Oc13009 extends AbstractMigration
     public function up(){
     	$sql = "
     		UPDATE db_itensmenu SET descricao = 'Inclusão', help = 'Inclusão' WHERE funcao = 'lic1_liclicitaoutrosorgaos001.php'; 
-			UPDATE db_itensmenu SET descricao = 'Alteração', help = 'Alteração' WHERE funcao = 'lic1_liclicitaoutrosorgaos002.php'; 
+			UPDATE db_itensmenu SET descricao = 'Alteração', help = 'Alteração' WHERE funcao = 'lic1_liclicitaoutrosorgaos002.php';
+			UPDATE db_itensmenu SET descricao = 'Licitação de Outros Órgãos', 
+									help = 'Licitação de Outros Órgãos',
+									desctec = 'Licitação de Outros Órgãos'
+					WHERE descricao = 'Licitacao de Outros Orgaos'; 
     	";
 
 		$this->execute($sql);
@@ -17,7 +21,11 @@ class Oc13009 extends AbstractMigration
     public function down(){
 		$sql = "
     		UPDATE db_itensmenu SET descricao = 'Inclusao', help = 'Inclusao' WHERE funcao = 'lic1_liclicitaoutrosorgaos001.php'; 
-			UPDATE db_itensmenu SET descricao = 'Alteracao', help = 'Alteracao' WHERE funcao = 'lic1_liclicitaoutrosorgaos002.php'; 
+			UPDATE db_itensmenu SET descricao = 'Alteracao', help = 'Alteracao' WHERE funcao = 'lic1_liclicitaoutrosorgaos002.php';
+			UPDATE db_itensmenu SET descricao = 'Licitacao de Outros Orgãos', 
+									help = 'Licitacao de Outros Orgãos',
+									desctec = 'Licitacao de Outros Orgãos'
+					WHERE descricao = 'Licitação de Outros Órgãos';  
     	";
 
 		$this->execute($sql);
