717424cf310a370cccc211200e3aa936a7b72ad0
MarioJunior <mario210755@gmail.com>
2021-01-07 09:55:04 -0300

    OC13705 - alteração e exclusão item obras

--- obr1_licitemobra003.php
+++ obr1_licitemobra003.php
@@ -10,42 +10,36 @@ db_postmemory($HTTP_POST_VARS);
 $cllicitemobra = new cl_licitemobra;
 $db_botao = false;
 $db_opcao = 33;
-if(isset($excluir)){
-  db_inicio_transacao();
-  $db_opcao = 3;
-  $cllicitemobra->excluir($obr06_sequencial);
-  db_fim_transacao();
-}else if(isset($chavepesquisa)){
-  $db_opcao = 3;
-  $result = $cllicitemobra->sql_record($cllicitemobra->sql_query($chavepesquisa));
-  db_fieldsmemory($result,0);
-  $db_botao = true;
-}
+//if(isset($excluir)){
+//  db_inicio_transacao();
+//  $db_opcao = 3;
+//  $cllicitemobra->excluir($obr06_sequencial);
+//  db_fim_transacao();
+//}else if(isset($chavepesquisa)){
+//  $db_opcao = 3;
+//  $result = $cllicitemobra->sql_record($cllicitemobra->sql_query($chavepesquisa));
+//  db_fieldsmemory($result,0);
+//  $db_botao = true;
+//}
 ?>
 <html>
 <head>
   <title>DBSeller Inform&aacute;tica Ltda - P&aacute;gina Inicial</title>
-  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-  <meta http-equiv="Expires" CONTENT="0">
-  <script language="JavaScript" type="text/javascript" src="scripts/scripts.js"></script>
-  <link href="estilos.css" rel="stylesheet" type="text/css">
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <meta http-equiv="Expires" CONTENT="0">
+    <script language="JavaScript" type="text/javascript" src="scripts/scripts.js"></script>
+    <script language="JavaScript" type="text/javascript" src="scripts/prototype.js"></script>
+    <script language="JavaScript" type="text/javascript" src="scripts/AjaxRequest.js"></script>
+    <link href="estilos.css" rel="stylesheet" type="text/css">
+    <link href="estilos/grid.style.css" rel="stylesheet" type="text/css">
 </head>
-<style>
-  #obr06_descricaotabela{
-    width: 393px;
-    height: 49px;
-  }
-</style>
+</head>
 <body bgcolor=#CCCCCC leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" onLoad="a=1" >
-<table>
-  <tr>
-    <td>
-      <?
-      include("forms/db_frmlicitemobra.php");
-      ?>
-    </td>
-  </tr>
-</table>
+<center>
+    <?
+    include("forms/db_frmlicitemobra.php");
+    ?>
+</center>
 <?
 db_menu(db_getsession("DB_id_usuario"),db_getsession("DB_modulo"),db_getsession("DB_anousu"),db_getsession("DB_instit"));
 ?>
