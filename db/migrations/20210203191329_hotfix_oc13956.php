1eb8dddb149493f4f7c45a8d676329edc8f27bff
Felipe <felipe.oliveira@contassconsultoria.com.br>
2021-02-03 18:06:41 -0300

    HOTFIX_13956 - Associação dos submenus do antigo menu Cadastro para o novo menu Cadastro nos módulos TCE e STN

--- db/migrations/20210203191329_hotfix_oc13956.php
+++ db/migrations/20210203191329_hotfix_oc13956.php
@@ -10,12 +10,16 @@ class HotfixOc13956 extends AbstractMigration
 
             select fc_startsession();
 
+            -- Pega todos os submenus do menu Cadastro e associa ao novo menu Cadastro nos módulos TCE/MG e STN
+
+            UPDATE db_menu SET id_item = 29 WHERE modulo = 2000018 AND id_item = 3962;
+            UPDATE db_menu SET id_item = 29 WHERE modulo = 2000025 AND id_item = 3962;
+
             -- Exclui o menu Fornecedor nos módulos TCE/MG e STN
 
             DELETE FROM db_menu 
                 WHERE id_item IN (2000018, 2000025) 
                 AND id_item_filho = 3962;
-
 
             -- Cadastra o menu 'Cadastro' nos módulos TCE/MG e STN respectivamente
             INSERT INTO db_menu(id_item, id_item_filho, menusequencia, modulo)
