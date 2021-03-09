88dfe1421c2d42d03031e7e7718105dc449c59b6
Wanderson Alves <wanderson.alves@contassconsultoria.com.br>
2021-03-09 18:23:03 -0300

    OC14254 - Inclusão de filtro em relatório de suplementações

--- db/migrations/20210309211754_oc14254.php
+++ db/migrations/20210309211754_oc14254.php
@@ -7,13 +7,13 @@ class Oc14254 extends PostgresMigration
     public function up()
     {
         $sql = <<<SQL
-
-        UPDATE db_itensmenu SET descricao = 'Emissão de Decreto' WHERE descricao = 'Emissão do Projeto';
   
         BEGIN;
         SELECT fc_startsession();
 
-COMMIT;
+        UPDATE db_itensmenu SET descricao = 'Emissão de Decreto' WHERE descricao = 'Emissão do Projeto';
+
+        COMMIT;
 
 SQL;
     $this->execute($sql);
