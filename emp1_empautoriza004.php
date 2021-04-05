9ad73ade5407fe6ea361a77a84c0f82aa90e9936
widouglas <widouglasmail@gmail.com>
2021-04-05 09:16:18 -0300

    OC11152 - Correcao de parametro

--- emp1_empautoriza004.php
+++ emp1_empautoriza004.php
@@ -183,16 +183,18 @@ if (isset($incluir)) {
             $sqlerro = true;
         }
 
-        $resultParamento = db_query("SELECT e30_controleprestacao FROM empparametro WHERE e39_anousu = " . date("Y", db_getsession("DB_datausu")));
-        db_fieldsmemory($resultParamento, 0)->e30_controleprestacao;
+        if ($e44_tipo == "4") {
+            $resultParamento = db_query("SELECT e30_controleprestacao FROM empparametro WHERE e39_anousu = " . date("Y", db_getsession("DB_datausu")));
+            db_fieldsmemory($resultParamento, 0)->e30_controleprestacao;
 
-        if ($e30_controleprestacao == 't') {
-            $resultPrestacaoContas = db_query("SELECT e60_numemp FROM empempenho e LEFT JOIN emppresta er ON e.e60_numemp = er.e45_numemp WHERE e60_numcgm = {$e54_numcgm} AND e45_acerta IS NULL AND er.e45_tipo = 4 LIMIT 1");
-            db_fieldsmemory($resultPrestacaoContas, 0)->e60_numemp;
+            if ($e30_controleprestacao == 't') {
+                $resultPrestacaoContas = db_query("SELECT e60_numemp FROM empempenho e LEFT JOIN emppresta er ON e.e60_numemp = er.e45_numemp WHERE e60_numcgm = {$e54_numcgm} AND e45_acerta IS NULL AND er.e45_tipo = 4 LIMIT 1");
+                db_fieldsmemory($resultPrestacaoContas, 0)->e60_numemp;
 
-            if ($e60_numemp) {
-                $erro_msg = "Usuário: Existe Prestação de Contas pendente para o Credor selecionado";
-                $sqlerro = true;
+                if ($e60_numemp) {
+                    $erro_msg = "Não é possível emitir Autorização de Empenho para credor que possua Prestação de Contas pendente!";
+                    $sqlerro = true;
+                }
             }
         }
     }
