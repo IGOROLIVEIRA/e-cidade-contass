686420b511aabe0142f3cc48cd1c3b8efa0d07c8
MarioJunior <mario210755@gmail.com>
2021-05-05 14:26:00 -0300

    hotfix

--- com1_pcmater002.php
+++ com1_pcmater002.php
@@ -44,7 +44,6 @@ $clpcgrupo = new cl_pcgrupo;
 $clpcsubgrupo = new cl_pcsubgrupo;
 $clhistoricoitem = new cl_historicoitem;
 $clcondataconf = new cl_condataconf;
-
 $db_opcao = 22;
 $db_botao = false;
 
@@ -63,15 +62,17 @@ if((isset($HTTP_POST_VARS["db_opcao"]) && $HTTP_POST_VARS["db_opcao"])=="Alterar
     $sqlerro  = true;
     db_msgbox($erro_msg);
   }
+  $result_pesquisa_material = $clpcmater->sql_record($clpcmater->sql_query(null,"pc01_descrmater as pc01_descrmateranterior",null,"pc01_codmater = $pc01_codmater"));
+  db_fieldsmemory($result_pesquisa_material,0);
 
-
-  if(!empty($pc01_data)){
+  if($pc01_descrmateranterior != $pc01_descrmater){
+      $dt_session = date("Y-m-d", db_getsession("DB_datausu"));
       $anousu = db_getsession('DB_anousu');
       $instituicao = db_getsession('DB_instit');
       $result = $clcondataconf->sql_record($clcondataconf->sql_query_file(db_getsession('DB_anousu'),db_getsession('DB_instit'),"c99_datapat",null,null));
       $c99_datapat = db_utils::fieldsMemory($result, 0)->c99_datapat;
 
-      if (strtotime($pc01_data) <= strtotime($c99_datapat)) {
+      if (strtotime($dt_session) <= strtotime($c99_datapat)) {
           $erro_msg = "O período já foi encerrado para envio do SICOM. Verifique os dados do lançamento e entre em contato com o suporte.";
           $sqlerro  = true;
           db_msgbox($erro_msg);
