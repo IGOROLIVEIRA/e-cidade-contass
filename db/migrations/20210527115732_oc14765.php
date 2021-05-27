ade01ba2a334cb0f360c9f0627828410570e0344
Wanderson Alves <wanderson.alves@contassconsultoria.com.br>
2021-05-27 09:49:46 -0300

    OC14765 - Alteraçao na contabilizaçao da baixa de bens patrimoniais

---
+++ db/migrations/20210527115732_oc14765.php
@@ -0,0 +1,48 @@
+<?php
+
+use Classes\PostgresMigration;
+
+class Oc14765 extends PostgresMigration
+{
+
+    public function up()
+    {
+        $this->insertCampoCodFundoTceMg();
+    }
+
+    public function insertCampoCodFundoTceMg()
+    {
+        $sql = <<<SQL
+
+        BEGIN;
+        SELECT fc_startsession();
+
+        INSERT INTO contrans
+        SELECT nextval('contabilidade.contrans_c45_seqtrans_seq'),
+               2021,
+               704,
+            (SELECT codigo
+             FROM db_config
+             WHERE prefeitura = 't');
+
+        INSERT INTO contranslan
+        VALUES (nextval('contabilidade.contranslan_c46_seqtranslan_seq'),
+               (SELECT c45_seqtrans FROM contrans
+                WHERE c45_coddoc = 704
+                  AND c45_anousu = 2021
+                LIMIT 1), 
+               9701,
+               'PRIMEIRO LANCAMENTO',
+               0,
+               FALSE,
+               0,
+               'PRIMEIRO LANCAMENTO',
+               1);
+               
+        COMMIT;
+
+SQL;
+        $this->execute($sql);
+
+    }
+}
