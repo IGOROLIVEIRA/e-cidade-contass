3972cf8b011bbe8cd3ae42ee3c25681d1113cc04
Robson de Jesus <robson.silva@contassconsultoria.com.br>
2021-06-26 15:05:49 -0300

    OC14880 - Correções de rubricas de desconto e rubricas R automáticas

--- libs/db_libpessoal.php
+++ libs/db_libpessoal.php
@@ -981,9 +981,11 @@ function situacao_funcionario($registro = null, $datafim = null)
   $dias_pagamento_sf = 30;
   $afastado = 1;
   $data_afastamento = date("Y-m-d", db_getsession("DB_datausu"));
-  $condicaoaux  = " and (r45_dtafas between '{$dtini}' and '{$dtfim}' or r45_dtreto between '{$dtini}' and '{$dtfim}')";
+  $condicaoaux  = " and ((r45_dtafas between '{$dtini}' and '{$dtfim}' or r45_dtreto between '{$dtini}' and '{$dtfim}') or r45_dtreto > '{$dtfim}')";
   $condicaoaux .= " and r45_situac != 11 and r45_regist =" . db_sqlformat($registro) . " order by r45_regist, r45_dtafas";
   global $afasta;
+  echo "select * from afasta " . bb_condicaosubpes("r45_") . $condicaoaux;
+  db_criatabela(db_query("select * from afasta " . bb_condicaosubpes("r45_") . $condicaoaux));
   if (db_selectmax("afasta", "select * from afasta " . bb_condicaosubpes("r45_") . $condicaoaux)) {
 
     for($iCont = 0; $iCont < count($afasta); $iCont++) {
