845cf0581c653044e3525d049f20eda4b36b8ad1
MarioJunior <mario210755@gmail.com>
2021-07-07 16:02:55 -0300

    OC13589 - ajuste na validação.

--- forms/db_frmEntradaOrdemCompra.php
+++ forms/db_frmEntradaOrdemCompra.php
@@ -193,7 +193,11 @@ if (count($aParametrosEmpenho) > 0) {
           /**
            * Acrescentado por causa do sicom
            */
-          $aNfEletronica = array(0 => 'Selecione',1 => 'Sim, padrão Estadual ou SINIEF 07/05', 2 => 'Sim, chave de acesso municipal ou outra', 3 => 'Não', 4 => 'Sim, padrão Estadual ou SINIEF 07/05 - Avulsa');
+          $aNfEletronica = array(0 => 'Selecione',
+                                 1 => 'Sim, padrão Estadual ou SINIEF 07/05',
+                                 2 => 'Sim, chave de acesso municipal ou outra',
+                                 3 => 'Não',
+                                 4 => 'Sim, padrão Estadual ou SINIEF 07/05 - Avulsa');
           db_select('e69_notafiscaleletronica', $aNfEletronica, true, 1, "onchange='js_tipoChave(this.value);' style='width:315px'"); //
           ?>
         </td>
@@ -1540,9 +1544,6 @@ if (count($aParametrosEmpenho) > 0) {
    */
   function js_confirmaEntrada() {
 
-    /*if(verificaChave()){
-      return false;
-    }*/
     if (confirm('Confirma a entrada dos Itens Selecionados no estoque?')) {
 
       //Número da nota, é obrigatorio
@@ -1552,9 +1553,14 @@ if (count($aParametrosEmpenho) > 0) {
 
             return false;
 
-         }*/     
+         }*/
 
-      if ($F('e69_notafiscaleletronica') == 2) {
+        if ($F('e69_notafiscaleletronica') == 0) {
+            alert('É necessário selecionar o tipo de Nota Fiscal!');
+            return false;
+        }
+
+        if ($F('e69_notafiscaleletronica') == 2) {
         if ($F('e69_nfserie') == '') {
           alert('O número de série deve ser preenchido!');
           $('e69_nfserie').focus();
