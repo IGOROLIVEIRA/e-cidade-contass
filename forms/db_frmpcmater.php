70a8e2b2216c761c6af0e24f9a5ac398384239e9
Your Name <youremail@yourdomain.com>
2021-07-13 09:11:05 -0300

    Ocultar campos na tela de cadastro de material/Serviço

--- forms/db_frmpcmater.php
+++ forms/db_frmpcmater.php
@@ -69,13 +69,13 @@ function js_executaIframe(val) {
     </td>
   </tr>
   <tr>
-    <td nowrap title="<?=@$Tpc01_libaut?>">       <?=@$Lpc01_libaut?>    </td>
-    <td nowrap> <?
+    <!--<td nowrap title="<?//=@$Tpc01_libaut?>">       <?//=@$Lpc01_libaut?>    </td>
+    <td nowrap>--> <?
     $arrlibaut_truefalse = array('t'=>'Sim','f'=>'Não');
-    db_select("pc01_libaut",$arrlibaut_truefalse,true,$db_opcao);
+    db_select("pc01_libaut",$arrlibaut_truefalse,true,$db_opcao,"Hidden");
     ?>
-    <?=$Lpc01_ativo?>
-    <?
+    <td nowrap title="<?//=@$Tpc01_libaut?>"> <?=$Lpc01_ativo?> </td>
+    <td nowrap> <?
     $arr_truefalse = array('f'=>'Não','t'=>'Sim');
     db_select("pc01_ativo",$arr_truefalse,true,$db_opcao);
     ?>
@@ -92,53 +92,46 @@ function js_executaIframe(val) {
             db_select("pc01_servico", $x, true, $db_opcao);
         }
     ?>
-    <?=$Lpc01_veiculo?>
+    <?//=$Lpc01_veiculo?>
     <?
     $aVeic = array("f"=>"Não","t"=>"Sim");
-    db_select("pc01_veiculo",$aVeic,true,$db_opcao);
+    db_select("pc01_veiculo",$aVeic,true,$db_opcao,"hidden");
     ?>
-
-    </td>
-    <tr>
-      <td>
-        <?
+    <?
         echo $Lpc01_fraciona;
-        ?>
-      </td>
-      <td>
-        <?
+    ?>
+    <?
         $aFrac = array("f"=>"Não","t"=>"Sim");
         db_select("pc01_fraciona",$aFrac,true,$db_opcao);
         ?>
-      </td>
-    </tr>
+    </td>
 
     <tr>
       <td nowrap>
         <?
-        echo $Lpc01_validademinima;
+        //echo $Lpc01_validademinima;
         ?>
       </td>
       <td nowrap>
        <?
         $aValMin = array("f"=>"Não","t"=>"Sim");
-        db_select("pc01_validademinima",$aValMin,true,$db_opcao);
+        db_select("pc01_validademinima",$aValMin,true,$db_opcao,"hidden");
         ?>
 
 
         <?
-        echo $Lpc01_obrigatorio;
+        //echo $Lpc01_obrigatorio;
 
         $aPObrigatorio = array("f"=>"Não","t"=>"Sim");
-        db_select("pc01_obrigatorio", $aPObrigatorio ,true,$db_opcao);
+        db_select("pc01_obrigatorio", $aPObrigatorio ,true,$db_opcao,"hidden");
         ?>
 
         <?
-        echo $Lpc01_liberaresumo;
+        //echo $Lpc01_liberaresumo;
 
         $aLiberarResumo = array("t" => "Sim",
                                 "f" => "Não");
-        db_select("pc01_liberaresumo", $aLiberarResumo, true, $db_opcao);
+        db_select("pc01_liberaresumo", $aLiberarResumo, true, $db_opcao,"hidden");
         ?>
       </td>
 
@@ -234,7 +227,7 @@ function js_executaIframe(val) {
    <? endif;    ?>
 
   <!--OC3770-->
-    <tr>
+    <!--<tr>
       <td>
         <strong>
            Tabela
@@ -260,7 +253,7 @@ function js_executaIframe(val) {
         ?>
       </td>
     </tr>
-  <tr>
+  <tr> -->
     <td>
       <strong>Material Utilizado em Obras/serviços?</strong>
     </td>
@@ -269,6 +262,21 @@ function js_executaIframe(val) {
       $aObra = array("0"=>"Selecione","f"=>"Não","t"=>"Sim");
       db_select("pc01_obras",$aObra,true,$db_opcao,"");
       ?>
+      <strong>
+           Tabela
+        <strong>
+        <?
+        $aTabela = array("f"=>"Não","t"=>"Sim");
+        db_select("pc01_tabela",$aTabela,true,$db_opcao,"style=width:90px;");
+        ?>
+
+        <strong>
+           Taxa
+        <strong>
+        <?
+        $aTaxa = array("f"=>"Não","t"=>"Sim");
+        db_select("pc01_taxa",$aTaxa,true,$db_opcao,"style=width:90px;");
+        ?>
     </td>
   </tr>
   <? if($db_opcao == 22 || $db_opcao == 2):?>
