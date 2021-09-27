0d1c6de2c3709e7f2846eea62576ed387153d91f
mario <mario@contassconsultoria.com.br>
2021-09-27 08:41:31 -0300

    OC15700 - migration

---
+++ db/migrations/20210926205913_oc15700.php
@@ -0,0 +1,13 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Oc15700 extends AbstractMigration
+{
+
+    public function up()
+    {
+        $sql = "";
+        $this->execute($sql);
+    }
+}
