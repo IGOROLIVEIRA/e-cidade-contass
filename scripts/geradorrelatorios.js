7d7a38e1358e79d068077e9d07623aad16286cd2
marcelo <marcelo@contassconsultoria.com.br>
2021-09-28 08:08:02 -0300

    correções do Agata

--- scripts/geradorrelatorios.js
+++ scripts/geradorrelatorios.js
@@ -1,24 +1,24 @@
 
 function js_imprimeRelatorio(iCodRelatorio,sCallBackFunction,aParametros){
-
+  console.log(sCallBackFunction);
   js_divCarregando('Aguarde, Gerando Relatório ...','msgBox');
 
   var sQuery  = "iCodRelatorio="+iCodRelatorio;
   sQuery += "&aParametros="+encodeURIComponent(aParametros);
-
+  console.log(sQuery);
   var url   = "sys4_processarelatorioRPC.php";
-  var oAjax = new Ajax.Request(url, 
+  var oAjax = new Ajax.Request(url,
     {
-      method: 'post', 
+      method: 'post',
       parameters: sQuery,
-      onComplete: sCallBackFunction  
+      onComplete: sCallBackFunction
     }
   );
 }
 
 function js_criaObjetoVariavel(sNome,sValor){
 
-  this.sNome  = sNome;   
+  this.sNome  = sNome;
   this.sValor = sValor;
 }
 
@@ -28,7 +28,7 @@ function js_downloadArquivo(oAjax){
   js_removeObj("msgBox");
 
   var sRetorno = eval("("+oAjax.responseText+")");
-
+  console.log(sRetorno);
   if (sRetorno.erro == true){
     alert(sRetorno.sMsg.urlDecode());
   } else {
@@ -37,5 +37,5 @@ function js_downloadArquivo(oAjax){
     jan.moveTo(0,0);
   }
 
-}	
+}
 
--- sys4_processarelatorioRPC.php
+++ sys4_processarelatorioRPC.php
@@ -42,228 +42,218 @@ include("model/dbVariaveisRelatorio.php");
 include("model/dbGeradorRelatorio.model.php");
 include("model/dbOrdemRelatorio.model.php");
 include("model/dbPropriedadeRelatorio.php");
-ini_set("error_reporting","E_ALL & ~NOTICE");
+ini_set("error_reporting", "E_ALL & ~NOTICE");
 
 
-$oPost   		   			           = db_utils::postMemory($_POST);
-$oJson   		   			           = new services_json();
-$cldb_relatorio    			       = new cl_db_relatorio();
+$oPost                                     = db_utils::postMemory($_POST);
+$oJson                                     = new services_json();
+$cldb_relatorio                       = new cl_db_relatorio();
 $cldb_geradorrelatoriotemplate = new cl_db_geradorrelatoriotemplate();
 
 $lSqlErro = false;
 $lErro    = false;
 
 try {
-  $oGeradorRelatorio = new dbGeradorRelatorio($oPost->iCodRelatorio);
-} catch (Exception $eException){
-	$lErro = true;
-  $sRetorno = $eException->getMessage();
+    $oGeradorRelatorio = new dbGeradorRelatorio($oPost->iCodRelatorio);
+} catch (Exception $eException) {
+    $lErro = true;
+    $sRetorno = $eException->getMessage();
 }
 
-if ( !$lErro ) {
+if (!$lErro) {
 
-	// Consulta XML e o Tipo do relatório
-	$rsConsultaRelatorio = $cldb_relatorio->sql_record($cldb_relatorio->sql_query($oPost->iCodRelatorio,"db63_db_tiporelatorio,db63_xmlestruturarel"));
+    // Consulta XML e o Tipo do relatório
+    $rsConsultaRelatorio = $cldb_relatorio->sql_record($cldb_relatorio->sql_query($oPost->iCodRelatorio, "db63_db_tiporelatorio,db63_xmlestruturarel"));
 
 
-	if( $cldb_relatorio->numrows > 0 ){
+    if ($cldb_relatorio->numrows > 0) {
 
-	  $oRelatorio = db_utils::fieldsMemory($rsConsultaRelatorio,0);
+        $oRelatorio = db_utils::fieldsMemory($rsConsultaRelatorio, 0);
 
-	  // Gera arquivo .agt e salva no tmp do dbportal
-	  $sCaminhoRelatorio = $oGeradorRelatorio->geraArquivoAgt($oRelatorio->db63_xmlestruturarel);
+        // Gera arquivo .agt e salva no tmp do dbportal
+        $sCaminhoRelatorio = $oGeradorRelatorio->geraArquivoAgt($oRelatorio->db63_xmlestruturarel);
 
 
-	  $clagata = new cl_dbagata();
-	  $api = $clagata->api;
-	  $api->setReportPath($sCaminhoRelatorio);
+        $clagata = new cl_dbagata();
+        $api = $clagata->api;
+        $api->setReportPath($sCaminhoRelatorio);
 
+        $oPropriedades = $oGeradorRelatorio->getPropriedades();
 
-	  $oPropriedades = $oGeradorRelatorio->getPropriedades();
+        /**
+         * Retorna o formato de saída do relatório
+         * Ex.: PDF, CSV, TXT
+         */
+        $sFormatoSaida = $oPropriedades->getTipoSaida();
+        if (!$sFormatoSaida) {
+            $sFormatoSaida = "pdf";
+        }
 
-	  /**
-	  * Retorna o formato de saída do relatório
-	  * Ex.: PDF, CSV, TXT
-	  */
-	  $sFormatoSaida = $oPropriedades->getTipoSaida();
-	  if (!$sFormatoSaida) {
-	  	$sFormatoSaida = "pdf";
-	  }
+        $api->setParameter('$head1', $oPropriedades->getNome());
 
-	  $api->setParameter('$head1',$oPropriedades->getNome());
+        $aOrdem       = $oGeradorRelatorio->getOrdem();
 
-	  $aOrdem 	  = $oGeradorRelatorio->getOrdem();
+        if (!empty($aOrdem)) {
 
-	  if (!empty($aOrdem)) {
+            $aNomeOrdem = array();
 
-	    $aNomeOrdem = array();
+            foreach ($aOrdem as $iInd1 => $aOrdem2) {
+                foreach ($aOrdem2 as $iInd2 => $oOrdem) {
+                    $aNomeOrdem[] = $oOrdem->getAlias();
+                }
+            }
 
-	    foreach ($aOrdem as $iInd1 => $aOrdem2){
-	      foreach ($aOrdem2 as $iInd2 => $oOrdem ){
-	        $aNomeOrdem[] = $oOrdem->getAlias();
-	      }
-	    }
+            if (!empty($aNomeOrdem)) {
 
-	    if (!empty($aNomeOrdem)) {
+                $sNomeOrdem = implode(", ", $aNomeOrdem);
+                $iLinha     = 2;
 
-	      $sNomeOrdem = implode(", ",$aNomeOrdem);
-	      $iLinha     = 2;
+                for ($iIni = 0; $iIni < strlen($sNomeOrdem); $i++) {
 
-	      for($iIni=0; $iIni < strlen($sNomeOrdem); $i++ ){
+                    $iFim = 52;
 
-	        $iFim = 52;
+                    if ($iLinha == 2) {
+                        $sPrefix = "Ordem: ";
+                        $iFim      -= 8;
+                    } else {
+                        $sPrefix = "";
+                    }
 
-	        if ($iLinha == 2) {
-	  	      $sPrefix = "Ordem: ";
-	  	      $iFim	  -= 8;
-	        } else {
-	          $sPrefix = "";
-	        }
+                    $api->setParameter('$head' . $iLinha, $sPrefix . (substr($sNomeOrdem, $iIni, $iFim)));
+                    $iLinha++;
+                    $iIni += $iFim;
 
-	        $api->setParameter('$head'.$iLinha,$sPrefix.(substr($sNomeOrdem,$iIni,$iFim)));
-	        $iLinha++;
-	        $iIni += $iFim;
+                    if ($iLinha == 7) {
+                        break;
+                    }
+                }
+            }
+        }
 
-	        if ($iLinha == 7) {
-	  	      break;
-	        }
-	      }
-	    }
-	  }
+        if (isset($oPost->aParametros)) {
 
+            $aVariaveisGerador = $oGeradorRelatorio->getVariaveis();
+            $aObjVariaveis     = $oJson->decode(str_replace("\\", "", $oPost->aParametros));
 
-	  if (isset($oPost->aParametros)){
+            foreach ($aVariaveisGerador as $sIndVar => $oVariavelGerador) {
+                foreach ($aObjVariaveis as $iInd => $oVariavel) {
+                    if ($oVariavel->sNome == $oVariavelGerador->getNome()) {
+                        if ($oVariavelGerador->getTipoDado() == 'date') {
+                            $sValor = implode('-', array_reverse(explode('/', $oVariavel->sValor)));
+                        } else {
+                            $sValor = $oVariavel->sValor;
+                        }
+                        $api->setParameter($oVariavel->sNome, utf8_decode($sValor));
+                    }
+                }
+            }
+        }
 
-	  	$aVariaveisGerador = $oGeradorRelatorio->getVariaveis();
-	    $aObjVariaveis     = $oJson->decode(str_replace("\\","",$oPost->aParametros));
 
-	    foreach ( $aVariaveisGerador as $sIndVar => $oVariavelGerador) {
-	      foreach ( $aObjVariaveis as $iInd => $oVariavel) {
-	        if ( $oVariavel->sNome == $oVariavelGerador->getNome()) {
-	          if ( $oVariavelGerador->getTipoDado() == 'date') {
-	            $sValor = implode('-',array_reverse(explode('/',$oVariavel->sValor)));
-	          } else {
-	            $sValor = $oVariavel->sValor;
-	          }
-	          $api->setParameter($oVariavel->sNome,utf8_decode($sValor));
-	        }
-	      }
-	    }
+        // Verifica o tipo de relatório 1-Relatório,  2-Documento Template e utiliza o método da API do Agata referente ao tipo
+        if ($oRelatorio->db63_db_tiporelatorio == 2) {
 
-	  }
+            /**
+             * Sempre será pdf quando entrar aqui
+             */
+            $sFormatoSaida = 'pdf';
 
+            $rsConsultaTemplate = $cldb_geradorrelatoriotemplate->sql_record($cldb_geradorrelatoriotemplate->sql_query(null, "db15_documento", null, " db15_db_relatorio = {$oPost->iCodRelatorio}"));
 
-	  // Verifica o tipo de relatório 1-Relatório,  2-Documento Template e utiliza o método da API do Agata referente ao tipo
-	  if ( $oRelatorio->db63_db_tiporelatorio == 2 ) {
+            if ($cldb_geradorrelatoriotemplate->numrows > 0) {
 
-			/**
-			* Sempre será pdf quando entrar aqui
-			*/
-	  	$sFormatoSaida = 'pdf';
+                $oArquivoSxw = db_utils::fieldsMemory($rsConsultaTemplate, 0);
 
-	  	$rsConsultaTemplate = $cldb_geradorrelatoriotemplate->sql_record($cldb_geradorrelatoriotemplate->sql_query(null,"db15_documento",null, " db15_db_relatorio = {$oPost->iCodRelatorio}"));
+                db_inicio_transacao();
 
-	    if ($cldb_geradorrelatoriotemplate->numrows > 0) {
+                $sArquivoSxw      = "docTamplate" . date("YmdHis") . db_getsession("DB_id_usuario") . ".sxw";
+                $sCaminhoTemplate = "tmp/" . $sArquivoSxw;
 
-	      $oArquivoSxw = db_utils::fieldsMemory($rsConsultaTemplate,0);
+                $lGeraSxw         = pg_lo_export($oArquivoSxw->db15_documento, $sCaminhoTemplate, $conn);
 
-	   	  db_inicio_transacao();
+                if (!$lGeraSxw) {
+                    $lSqlErro = true;
+                    $lErro      = true;
+                    $sRetorno = "Erro ao gerar aquivo Sxw!";
+                }
 
-	   	  $sArquivoSxw      = "docTamplate".date("YmdHis").db_getsession("DB_id_usuario").".sxw";
-	  	  $sCaminhoTemplate = "tmp/".$sArquivoSxw;
+                db_fim_transacao($lSqlErro);
 
-	   	  $lGeraSxw 		= pg_lo_export($oArquivoSxw->db15_documento,$sCaminhoTemplate,$conn);
+                $sCaminhoSalvoSxw = "tmp/docSalvoSxw" . date("YmdHis") . db_getsession("DB_id_usuario") . ".sxw";
 
-	   	  if (!$lGeraSxw) {
-	   	    $lSqlErro = true;
-	   	    $lErro	  = true;
-	   	    $sRetorno = "Erro ao gerar aquivo Sxw!";
-	      }
+                $api->setOutputPath($sCaminhoSalvoSxw);
 
-	      db_fim_transacao($lSqlErro);
+                ob_start();
 
-	      $sCaminhoSalvoSxw = "tmp/docSalvoSxw".date("YmdHis").db_getsession("DB_id_usuario").".sxw";
+                $ok = $api->parseOpenOffice($sCaminhoTemplate);
 
-	      $api->setOutputPath($sCaminhoSalvoSxw);
+                if (!$ok) {
 
-			  ob_start();
+                    $lErro    = true;
+                    $sRetorno = $api->getError();
 
-			  $ok = $api->parseOpenOffice($sCaminhoTemplate);
+                    ob_end_clean();
+                } else if ($sFormatoSaida == 'pdf') {
 
-			  if (!$ok){
+                    ob_end_clean();
 
-			  	$lErro    = true;
-		 	  	$sRetorno = $api->getError();
+                    if ($api->getRowNum() == 0) {
+                        $aRetorno = array("sMsg" => urlencode("Nenhum registro encontrado!"), "erro" => true);
+                        echo $oJson->encode($aRetorno);
+                        exit;
+                    }
 
-		 	  	ob_end_clean();
+                    $sNomeRelatorio   = "tmp/geraRelatorio" . date("YmdHis") . db_getsession("DB_id_usuario") . ".pdf";
+                    $sComandoConverte = `bin/oo2pdf/oo2pdf.sh {$sCaminhoSalvoSxw} {$sNomeRelatorio}`;
 
-		 	  }else if ($sFormatoSaida == 'pdf'){
+                    if (trim($sComandoConverte) != "") {
+                        $aRetorno = array("sMsg" => urlencode("Operação abortada, verifique as configurações do gerador de relatórios!"), "erro" => true);
+                        echo $oJson->encode($aRetorno);
+                        exit;
+                    } else {
+                        $sRetorno = $sNomeRelatorio;
+                    }
+                }
+            } else {
 
-					ob_end_clean();
+                $lErro     = true;
+                $sRetorno = "Nenhum template cadastrado!";
+            }
+        } else {
 
-					if ($api->getRowNum() == 0){
-			 		  $aRetorno = array("sMsg"=>urlencode("Nenhum registro encontrado!"),"erro"=>true);
-					  echo $oJson->encode($aRetorno);
-					  exit;
-					}
+            $api->setFormat($sFormatoSaida);
+            $sNomeRelatorio   = "tmp/geraRelatorio" . date("YmdHis") . db_getsession("DB_id_usuario") . "." . $sFormatoSaida;
+            $api->setOutputPath($sNomeRelatorio);
 
-					$sNomeRelatorio   = "tmp/geraRelatorio".date("YmdHis").db_getsession("DB_id_usuario").".pdf";
-					$sComandoConverte = `bin/oo2pdf/oo2pdf.sh {$sCaminhoSalvoSxw} {$sNomeRelatorio}`;
+            ob_start();
 
-					if (trim($sComandoConverte) != "") {
-			 		  $aRetorno = array("sMsg"=>urlencode("Operação abortada, verifique as configurações do gerador de relatórios!"),"erro"=>true);
-					  echo $oJson->encode($aRetorno);
-					  exit;
-					} else {
-					  $sRetorno = $sNomeRelatorio;
-					}
+            $ok = $api->generateReport();
 
-		  	}
+            if (!$ok) {
 
-		  } else {
-		    $lErro 	= true;
-		  	$sRetorno = "Nenhum template cadastrado!";
-		  }
+                ob_end_clean();
+                
+                $lErro    = true;
+                $sRetorno = $api->getError();
+            } else {
 
-	  } else {
+                ob_end_clean();
 
-	    $api->setFormat($sFormatoSaida);
-	    $sNomeRelatorio   = "tmp/geraRelatorio".date("YmdHis").db_getsession("DB_id_usuario").".".$sFormatoSaida;
-	    $api->setOutputPath($sNomeRelatorio);
+                if ($api->getRowNum() == 0) {
+                    $aRetorno = array("sMsg" => urlencode("Nenhum registro encontrado!"), "erro" => true);
+                    echo $oJson->encode($aRetorno);
+                    exit;
+                }
 
-	  	ob_start();
-
-	  	$ok = $api->generateReport();
-
-	    if(!$ok){
-
-	      ob_end_clean();
-
-	      $lErro    = true;
-	      $sRetorno = $api->getError();
-
-	    }else{
-
-	      ob_end_clean();
-
-	   	  if ($api->getRowNum() == 0){
-		 		  $aRetorno = array("sMsg"=>urlencode("Nenhum registro encontrado!"),"erro"=>true);
-				  echo $oJson->encode($aRetorno);
-				  exit;
-		    }
-
-		    $sRetorno = $sNomeRelatorio;
-	    }
-
-	  }
-
-	} else {
-	  $lErro 	= true;
-	  $sRetorno = "Nenhum relatório emcontrado!";
-	}
-
+                $sRetorno = $sNomeRelatorio;
+            }
+        }
+    } else {
+        $lErro     = true;
+        $sRetorno = "Nenhum relatório emcontrado!";
+    }
 }
 
-$aRetorno = array("sMsg"=>urlencode($sRetorno),"erro"=>$lErro);
+$aRetorno = array("sMsg" => urlencode($sRetorno), "erro" => $lErro);
 
 echo $oJson->encode($aRetorno);
