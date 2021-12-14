04b14ece598154eced303380e129aaaf4d0df74b
Filipi <filipibarbosa.contass@gmail.com>
2021-12-14 09:14:01 -0300

    OC15437 Correcao SQL Migration

--- db/migrations/20211130141617_oc15437.php
+++ db/migrations/20211130141617_oc15437.php
@@ -8,15 +8,15 @@ class Oc15437 extends AbstractMigration
     public function up()
     {
         $sql = " DELETE
-        FROM db_itensmenu
-        WHERE id_item_filho = 'm4_confignumeracao.php';
-
-        DELETE
         FROM db_menu
         WHERE id_item_filho =
                 (SELECT id_item
                  FROM db_itensmenu
                  WHERE funcao = 'm4_confignumeracao.php');
+
+         DELETE
+        FROM db_itensmenu
+        WHERE funcao = 'm4_confignumeracao.php';
 
          INSERT INTO db_itensmenu VALUES((select max(id_item)+1 from db_itensmenu),'Licitacao','Manutencao de licitacao','m4_licitacao.php',1,1,'Manutencao de acordo','t');
         INSERT INTO db_menu VALUES(select id_item from db_itensmenu where descricao like '%(Patrimonial)%'
