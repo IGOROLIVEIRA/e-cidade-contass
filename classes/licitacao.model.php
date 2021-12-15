02cf25f0c62250e2d8305596f339817fedd19e07
marcelo <marcelo@contassconsultoria.com.br>
2021-12-15 01:55:58 -0300

    iniciando zip de arquivos

--- classes/licitacao.model.php
+++ classes/licitacao.model.php
@@ -79,37 +79,7 @@ class licitacao
         l20_edital as processolicitatorio,
         l20_anousu as anoprocessolicitatorio,
         pc01_codmater as codigodoitem,
-        l21_ordem as sequencialdoitemnoprocesso,
-        CASE
-        WHEN pc01_complmater IS NOT NULL
-        AND pc01_complmater != pc01_descrmater THEN pc01_descrmater ||'. '|| pc01_complmater
-        ELSE pc01_descrmater END AS descricaodoitem,
-        m61_descr as unidadedemedida,
-        pc11_quant as quantidadelicitada,
-        case
-            when pc80_criterioadjudicacao = 3 then si02_vlprecoreferencia
-            else si02_vlpercreferencia end as valorunitariomedio,
-        case
-            when l20_tipojulg = 3 then l04_descricao
-            else ' ' end as codigodolote,
-        cgc as cnpj,
-        l20_edital as processolicitatorio,
-        l20_anousu as anoprocessolicitatorio,
-        pc01_codmater as codigodoitem,
-        l21_ordem as sequencialdoitemnoprocesso,
-        m61_descr as unidadedemedida,
-        pc11_quant as quantidadelicitada,
-        case
-            when pc80_criterioadjudicacao = 3 then si02_vlprecoreferencia
-            else si02_vlpercreferencia end as valorunitariomedio,
-        case
-            when l20_tipojulg = 3 then l04_descricao
-            else ' ' end as codigodolote,
-        l20_edital as processolicitatorio,
-        l20_anousu as anoprocessolicitatorio,
-        l04_codigo as codigodolote,
-        l04_liclicitem as codigodoitemvinculadoaolote,
-        l04_descricao as descricaodolote";
+        l21_ordem as sequencialdoitemnoprocesso";
 
         $rsItens  = $oDaoLicitem->sql_record(
             $oDaoLicitem->sql_query_licitacao_exporta(
@@ -172,12 +142,7 @@ class licitacao
             else si02_vlpercreferencia end as valorunitariomedio,
         case
             when l20_tipojulg = 3 then l04_descricao
-            else ' ' end as codigodolote,
-        l20_edital as processolicitatorio,
-        l20_anousu as anoprocessolicitatorio,
-        l04_codigo as codigodolote,
-        l04_liclicitem as codigodoitemvinculadoaolote,
-        l04_descricao as descricaodolote";
+            else ' ' end as codigodolote";
 
         $rsItens  = $oDaoLicitem->sql_record(
             $oDaoLicitem->sql_query_licitacao_exporta(
@@ -211,36 +176,6 @@ class licitacao
         $sCampos = "
         db_config.codigo as tipodoorgao,
 	    db_config.nomeinst as nomeorgao,
-        cgc as cnpj,
-        l20_edital as processolicitatorio,
-        l20_anousu as anoprocessolicitatorio,
-        pc01_codmater as codigodoitem,
-        l21_ordem as sequencialdoitemnoprocesso,
-        CASE
-        WHEN pc01_complmater IS NOT NULL
-        AND pc01_complmater != pc01_descrmater THEN pc01_descrmater ||'. '|| pc01_complmater
-        ELSE pc01_descrmater END AS descricaodoitem,
-        m61_descr as unidadedemedida,
-        pc11_quant as quantidadelicitada,
-        case
-            when pc80_criterioadjudicacao = 3 then si02_vlprecoreferencia
-            else si02_vlpercreferencia end as valorunitariomedio,
-        case
-            when l20_tipojulg = 3 then l04_descricao
-            else ' ' end as codigodolote,
-        cgc as cnpj,
-        l20_edital as processolicitatorio,
-        l20_anousu as anoprocessolicitatorio,
-        pc01_codmater as codigodoitem,
-        l21_ordem as sequencialdoitemnoprocesso,
-        m61_descr as unidadedemedida,
-        pc11_quant as quantidadelicitada,
-        case
-            when pc80_criterioadjudicacao = 3 then si02_vlprecoreferencia
-            else si02_vlpercreferencia end as valorunitariomedio,
-        case
-            when l20_tipojulg = 3 then l04_descricao
-            else ' ' end as codigodolote,
         l20_edital as processolicitatorio,
         l20_anousu as anoprocessolicitatorio,
         l04_codigo as codigodolote,
--- con1_exportaprocessos002.php
+++ con1_exportaprocessos002.php
@@ -1,30 +1,6 @@
 <?
-/*
- *     E-cidade Software Publico para Gestao Municipal
- *  Copyright (C) 2013  DBselller Servicos de Informatica
- *                            www.dbseller.com.br
- *                         e-cidade@dbseller.com.br
- *
- *  Este programa e software livre; voce pode redistribui-lo e/ou
- *  modifica-lo sob os termos da Licenca Publica Geral GNU, conforme
- *  publicada pela Free Software Foundation; tanto a versao 2 da
- *  Licenca como (a seu criterio) qualquer versao mais nova.
- *
- *  Este programa e distribuido na expectativa de ser util, mas SEM
- *  QUALQUER GARANTIA; sem mesmo a garantia implicita de
- *  COMERCIALIZACAO ou de ADEQUACAO A QUALQUER PROPOSITO EM
- *  PARTICULAR. Consulte a Licenca Publica Geral GNU para obter mais
- *  detalhes.
- *
- *  Voce deve ter recebido uma copia da Licenca Publica Geral GNU
- *  junto com este programa; se nao, escreva para a Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
- *  02111-1307, USA.
- *
- *  Copia da licenca no diretorio licenca/licenca_en.txt
- *                                licenca/licenca_pt.txt
- */
-
+// ini_set('display_errors', 'On');
+// error_reporting(E_ALL);
 require_once("libs/db_stdlib.php");
 require_once("libs/db_conecta.php");
 require_once("libs/db_sessoes.php");
@@ -57,14 +33,56 @@ $oLicitacao = new licitacao($iLicitacao);
 try {
     $aEditalLicitacao   = $oLicitacao->getEditalExport();
     $aItensLicitacao    = $oLicitacao->getItensExport();
-    $aEditalLicitacao    = $oLicitacao->getLoteExport();
+    $aLoteLicitacao    = $oLicitacao->getLoteExport();
 } catch (Exception $oErro) {
     db_redireciona('db_erros.php?fechar=true&db_erro=' . $oErro->getMessage());
 }
 
-//========================  escrevemos o arquivos com os itens encontrados para a solicitação
 
-$clabre_arquivo = new cl_abre_arquivo("/tmp/Licitacao_$iLicitacao.$extensao");
+$clabre_arquivo = new cl_abre_arquivo("tmp/Edital_$iLicitacao.$extensao");
+
+if ($clabre_arquivo->arquivo != false) {
+
+    $vir = $separador;
+    $del = $delimitador;
+
+    fputs($clabre_arquivo->arquivo, "\n");
+
+    foreach ($aEditalLicitacao as $iItens => $oItens) {
+
+        $iTipoRegistro                   = '1';
+        $iCodigoOrgao                    = '01';
+        $iTipoOrgao                      = '01';
+        $iCnpj                           = $oItens->cnpj;
+        $sNomeOrgao                      = $oItens->nomeorgao;
+        $iProcessoLicitatorio            = $oItens->processolicitatorio;
+        $iExercicio                      = $oItens->anoprocessolicitatorio;
+        $iNroEdital                      = $oItens->processolicitatorio;
+        $iExercicioEdital                = $oItens->anoprocessolicitatorio;
+        $sProcessoObjeto                 = '';
+        $iNaturezaObjeto                 = '';
+        $iRegistroPreco                  = '';
+
+        fputs($clabre_arquivo->arquivo, formatarCampo($iTipoRegistro, $vir, $del));
+        fputs($clabre_arquivo->arquivo, formatarCampo($iCodigoOrgao, $vir, $del));
+        fputs($clabre_arquivo->arquivo, formatarCampo($iTipoOrgao, $vir, $del));
+        fputs($clabre_arquivo->arquivo, formatarCampo($iNroEdital, $vir, $del));
+        fputs($clabre_arquivo->arquivo, formatarCampo($iCnpj, $vir, $del));
+        fputs($clabre_arquivo->arquivo, formatarCampo($sNomeOrgao, $vir, $del));
+        fputs($clabre_arquivo->arquivo, formatarCampo($iProcessoLicitatorio, $vir, $del));
+        fputs($clabre_arquivo->arquivo, formatarCampo($iExercício, $vir, $del));
+        fputs($clabre_arquivo->arquivo, formatarCampo($iNroEdital, $vir, $del));
+        fputs($clabre_arquivo->arquivo, formatarCampo($iExercicioEdital, $vir, $del));
+        fputs($clabre_arquivo->arquivo, formatarCampo($sProcessoObjeto, $vir, $del));
+        fputs($clabre_arquivo->arquivo, formatarCampo($iNaturezaObjeto, $vir, $del));
+        fputs($clabre_arquivo->arquivo, formatarCampo($iRegistroPreco, $vir, $del));
+        fputs($clabre_arquivo->arquivo, "\n");
+    }
+
+    fclose($clabre_arquivo->arquivo);
+}
+
+$clabre_arquivo = new cl_abre_arquivo("tmp/Itens_$iLicitacao.$extensao");
 
 if ($clabre_arquivo->arquivo != false) {
 
@@ -105,12 +123,51 @@ if ($clabre_arquivo->arquivo != false) {
     }
 
     fclose($clabre_arquivo->arquivo);
-
-    echo "<script>";
-    echo "  jan = window.open('db_download.php?arquivo=" . $clabre_arquivo->nomearq . "','','width='+(screen.availWidth-5)+',height='+(screen.availHeight-40)+',scrollbars=1,location=0 ');";
-    echo "  jan.moveTo(0,0);";
-    echo "</script>";
 }
+
+$clabre_arquivo = new cl_abre_arquivo("tmp/Lote_$iLicitacao.$extensao");
+
+if ($clabre_arquivo->arquivo != false) {
+
+    $vir = $separador;
+    $del = $delimitador;
+
+    fputs($clabre_arquivo->arquivo, "\n");
+
+    foreach ($aEditalLicitacao as $iItens => $oItens) {
+
+        $iTipoRegistro                   = '1';
+        $iCodigoOrgao                    = '01';
+        $iTipoOrgao                      = '01';
+        $iCnpj                           = $oItens->cnpj;
+        $sNomeOrgao                      = $oItens->nomeorgao;
+        $iProcessoLicitatorio            = $oItens->processolicitatorio;
+
+        fputs($clabre_arquivo->arquivo, formatarCampo($iTipoRegistro, $vir, $del));
+        fputs($clabre_arquivo->arquivo, formatarCampo($iCodigoOrgao, $vir, $del));
+        fputs($clabre_arquivo->arquivo, formatarCampo($iTipoOrgao, $vir, $del));
+        fputs($clabre_arquivo->arquivo, formatarCampo($iNroEdital, $vir, $del));
+        fputs($clabre_arquivo->arquivo, formatarCampo($iCnpj, $vir, $del));
+        fputs($clabre_arquivo->arquivo, formatarCampo($sNomeOrgao, $vir, $del));
+        fputs($clabre_arquivo->arquivo, formatarCampo($iProcessoLicitatorio, $vir, $del));
+        fputs($clabre_arquivo->arquivo, "\n");
+    }
+
+    fclose($clabre_arquivo->arquivo);
+}
+$aArquivosGerados = array();
+$aArquivosGerados[] =  strtoupper("tmp/Edital_$iLicitacao.$extensao");
+$aArquivosGerados[] =  strtoupper("tmp/Itens_$iLicitacao.$extensao");
+$aArquivosGerados[] =  strtoupper("tmp/Lote_$iLicitacao.$extensao");
+
+$sNomeArquivo = "export_process";
+
+compactaArquivos($aArquivosGerados, $sNomeArquivo);
+
+echo "<script>";
+echo "  jan = window.open('db_download.php?arquivo=" . "tmp/export_process.zip" . "','','width='+(screen.availWidth-5)+',height='+(screen.availHeight-40)+',scrollbars=1,location=0 ');";
+echo "  jan.moveTo(0,0);";
+echo "</script>";
 
 // Funcao para formatar um campo
 function formatarCampo($valor, $separador, $delimitador)
