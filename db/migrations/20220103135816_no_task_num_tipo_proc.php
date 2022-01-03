9c20cd598e526a43201e56128727ff27330ba2f4
Elvio Amaral <elvioamaral.ea@gmail.com>
2022-01-03 11:44:04 -0300

    NOTASK - add new sequence for table in 2022 (change table).

--- db/migrations/20220103135816_no_task_num_tipo_proc.php
+++ db/migrations/20220103135816_no_task_num_tipo_proc.php
@@ -11,14 +11,13 @@ class NoTaskNumTipoProc extends AbstractMigration
         BEGIN;
         SELECT fc_startsession();
 
-        INSERT INTO acordogruponumeracao
-        SELECT nextval('acordogruponumeracao_ac03_sequencial_seq') AS ac03_sequencial,
-              ac03_acordogrupo,
-              2022 AS ac03_anousu,
-              0 AS ac03_numero,
-              ac03_instit
-        FROM acordogruponumeracao
-        WHERE ac03_anousu = 2021;
+        INSERT INTO numeracaotipoproc
+        SELECT nextval('numeracaotipoproc_p200_codigo_seq') AS p200_codigo,
+               2022 AS p200_ano,
+               0 AS p200_numeracao,
+               p200_tipoproc
+        FROM numeracaotipoproc
+        WHERE p200_ano = 2021;
 
         COMMIT;
 
