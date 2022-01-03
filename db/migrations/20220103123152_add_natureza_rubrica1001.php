7d9442f53f68d8d7169d2ef27dfce5af17ac32f3
Robson de Jesus <robson.silva@contassconsultoria.com.br>
2022-01-03 16:36:36 -0300

    OCFolha - Adicionado naturezas rubricas que nao existiam no sistema

--- db/migrations/20220103123152_add_natureza_rubrica1001.php
+++ db/migrations/20220103123152_add_natureza_rubrica1001.php
@@ -6,13 +6,24 @@ class AddNaturezaRubrica1001 extends AbstractMigration
 {
     public function up()
     {
-        $sql = "INSERT INTO rubricasesocial values ('1001','Subsídio')";
-        $this->execute($sql);
+        if ($this->checkNatureza()) {
+            $sql = "INSERT INTO rubricasesocial values ('1001','Subsídio')";
+            $this->execute($sql);
+        }
     }
 
     public function down()
     {
         $sql = "DELETE FROM rubricasesocial WHERE e990_sequencial = '1001'";
         $this->execute($sql);
+    }
+
+    private function checkNatureza()
+    {
+        $result = $this->fetchRow("SELECT * FROM rubricasesocial WHERE e990_sequencial = '1001'");
+        if (empty($result)) {
+            return true;
+        }
+        return false; 
     }
 }
