1312449c52b35c8ca309edc3c3426b402e52993a
Your Name <youremail@yourdomain.com>
2022-01-14 10:22:31 -0300

    Correção exclusão de con

--- con4_contratos.RPC.php
+++ con4_contratos.RPC.php
@@ -1633,14 +1633,16 @@ switch ($oParam->exec) {
              */
             $result1 = $clempempenhocontrato->sql_record($clempempenhocontrato->sql_query(null,"e100_numemp","","e100_acordo=$oParam->iAcordo"));
             db_fieldsmemory($result1,0);
+            if($e100_numemp!=""){
+
+                $result = $clempelemento->sql_record($clempelemento->sql_query($e100_numemp,null,"*","e64_codele"));
+                db_fieldsmemory($result,0);
             
-            $result = $clempelemento->sql_record($clempelemento->sql_query($e100_numemp,null,"*","e64_codele"));
-            db_fieldsmemory($result,0);
-
-            if($e64_vlremp!=$e64_vlranu){
-                throw new ParameterException(_M($sCaminhoMensagens . 'Acordo não pode ser excluido.'));  
+                if($e64_vlremp!=$e64_vlranu){
+                    throw new ParameterException(('Acordo não pode ser excluido.'));  
+                }
             }
-
+            
             $oAcordo = new Acordo($oParam->iAcordo);
             $oAcordo->remover();
 
