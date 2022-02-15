06790d0f98dfb26154e5997c23d3f50a8bcdcc48
Raimundo Barbosa <raimundo2704@gmail.com>
2022-02-15 18:21:17 -0300

    OC16659 - Correção de leiaute - Campo Procedência-Histórico da CDA

--- div2_certidaodivida002.php
+++ div2_certidaodivida002.php
@@ -920,7 +920,8 @@ function drawDebitos(pdf3 $pdf, cda $oCertidao, $oPardiv, $lTotaliza=false, $lRe
                 $pdf->Cell(10,5,$oDebito->livro."/".$oDebito->folha,1,0,"C",0);
                 $pdf->Cell(15,5,ucfirst($oDebito->origem)."/{$oDebito->codigoorigem}",1,0,"C",0);
                 if($oInstit->getCodigoCliente() == Instituicao::COD_CLI_PMPIRAPORA){
-                    $pdf->Cell(30,5,$oDebito->procedencia . ' - ' . $oDebito->procedenciaHist,1,0,"L",0);
+                    $pdf->Cell(30,5,substr($oDebito->procedencia,0,12) . '-' . substr($oDebito->procedenciaHist,0,12),1,0,"L",0);
+                    //$pdf->Cell(30,5,$oDebito->procedencia . ' - ' . $oDebito->procedenciaHist,1,0,"L",0);
                 }else{
                     $pdf->Cell(30,5,$oDebito->procedencia,1,0,"L",0);
                 };
@@ -1216,7 +1217,8 @@ function drawDebitosHonorarios(pdf3 $pdf, cda $oCertidao, $oPardiv, $lTotaliza=f
                 $pdf->Cell(10,5,$oDebito->exercicio,1,0,"C",0);
                 $pdf->Cell(9,5,$oDebito->numpar,1,0,"C",0);
                 if($oInstit->getCodigoCliente() == Instituicao::COD_CLI_PMPIRAPORA){
-                    $pdf->Cell(30,5,$oDebito->procedencia . ' - ' . $oDebito->procedenciaHist,1,0,"L",0);
+                    $pdf->Cell(30,5,substr($oDebito->procedencia,0,12) . '-' . substr($oDebito->procedenciaHist,0,12),1,0,"L",0);
+                    //$pdf->Cell(30,5,$oDebito->procedencia . ' - ' . $oDebito->procedenciaHist,1,0,"L",0);
                 }else{
                     $pdf->Cell(30,5,$oDebito->procedencia,1,0,"L",0);
                 };
@@ -1239,10 +1241,13 @@ function drawDebitosHonorarios(pdf3 $pdf, cda $oCertidao, $oPardiv, $lTotaliza=f
                     $pdf->Cell(20,5,db_formatar($oDebito->valorcorrigido+getValorHonorarios($oDebito->valorcorrigido),'f'),1,1,"R",0);
 
                 }
+                               
                 if ( $oPardiv->v04_imphistcda == "t" && isset($oDebito->observacao)) {
                     
                     $pdf->SetFont('','I',5);
                     $pdf->setX(10);
+
+                    
 
                     $pdf->SetAligns(array('J'));
                     $pdf->SetWidths(array(194));
@@ -1527,7 +1532,8 @@ function drawDebitosOrigemCorrigido(pdf3 $pdf, cda $oCertidao, $oPardiv, $lTotal
             $pdf->Cell(10,5,$oDebito->livro."/".$oDebito->folha      ,1,0,"C",0);
             $pdf->Cell(12,5,ucfirst($oDebito->origem)."/{$oDebito->codigoorigem}",1,0,"C",0);
             if($oInstit->getCodigoCliente() == Instituicao::COD_CLI_PMPIRAPORA){
-                $pdf->Cell(27,5,$oDebito->procedencia . ' - ' . $oDebito->procedenciaHist,1,0,"L",0);
+                $pdf->Cell(27,5,substr($oDebito->procedencia,0,11) . '-' . substr($oDebito->procedenciaHist,0,11),1,0,"L",0);
+                //$pdf->Cell(27,5,$oDebito->procedencia . ' - ' . $oDebito->procedenciaHist,1,0,"L",0);
             }else{
                 $pdf->Cell(27,5,$oDebito->procedencia,1,0,"L",0);
             };
@@ -1777,7 +1783,8 @@ function drawDebitosOrigemCorrigidoData(pdf3 $pdf, cda $oCertidao, $oPardiv, $lT
             $pdf->Cell(10,5,$oDebito->livro."/".$oDebito->folha       ,1,0,"C",0);
             $pdf->Cell(17,5,$oDebito->origemdebito                    ,1,0,"C",0);
             if($oInstit->getCodigoCliente() == Instituicao::COD_CLI_PMPIRAPORA){
-                $pdf->Cell(27,5,$oDebito->procedencia . ' - ' . $oDebito->procedenciaHist,1,0,"L",0);
+                $pdf->Cell(27,5,substr($oDebito->procedencia,0,11) . '-' . substr($oDebito->procedenciaHist,0,11),1,0,"L",0);
+                //$pdf->Cell(27,5,$oDebito->procedencia . ' - ' . $oDebito->procedenciaHist,1,0,"L",0);
             }else{
                 $pdf->Cell(27,5,$oDebito->procedencia,1,0,"L",0);
             };
@@ -2015,7 +2022,8 @@ function drawDebitosComposicao(pdf3 $pdf, cda $oCertidao, $oPardiv, $lTotaliza=f
             $pdf->Cell(10,5,$oDebito->livro."/".$oDebito->folha      ,1,0,"C",0);
             $pdf->Cell(12,5,ucfirst($oDebito->origem)."/{$oDebito->codigoorigem}",1,0,"C",0);
             if($oInstit->getCodigoCliente() == Instituicao::COD_CLI_PMPIRAPORA){
-                $pdf->Cell(27,5,$oDebito->procedencia . ' - ' . $oDebito->procedenciaHist,1,0,"L",0);
+                $pdf->Cell(27,5,substr($oDebito->procedencia,0,11) . '-' . substr($oDebito->procedenciaHist,0,11),1,0,"L",0);
+                //$pdf->Cell(27,5,$oDebito->procedencia . ' - ' . $oDebito->procedenciaHist,1,0,"L",0);
             }else{
                 $pdf->Cell(27,5,$oDebito->procedencia,1,0,"L",0);
             };           
