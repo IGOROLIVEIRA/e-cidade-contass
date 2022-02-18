a6898cc9a8d99fdbaf1924662c6f5da78187eda9
marcelo <marcelo@contassconsultoria.com.br>
2022-02-18 18:38:48 -0300

    ajuste

--- db/migrations/20220218210555_hotfix2200opcaocodcateg.php
+++ db/migrations/20220218210555_hotfix2200opcaocodcateg.php
@@ -8,7 +8,7 @@ class Hotfix2200opcaocodcateg extends AbstractMigration
     {
         $sql = "
         BEGIN;
-        INSERT INTO habitacao.avaliacaoperguntaopcao (db104_sequencial, db104_avaliacaopergunta, db104_descricao, db104_aceitatexto, db104_identificador, db104_peso, db104_valorresposta, db104_identificadorcampo) VALUES(12, 4000632, '', true, 'codCateg-12', 0, '', 'codCateg');
+        INSERT INTO habitacao.avaliacaoperguntaopcao (db104_sequencial, db104_avaliacaopergunta, db104_descricao, db104_aceitatexto, db104_identificador, db104_peso, db104_valorresposta, db104_identificadorcampo) VALUES((select max(db104_sequencial)+1 from avaliacaoperguntaopcao), 4000632, '', true, 'codCateg-12', 0, '', 'codCateg');
         COMMIT;
         ";
         $this->execute($sql);
