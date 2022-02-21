8fdd244bcebcb6b79e2889bd616b57418cfb8ce2
Mario <Mario@contassconsultoria.com.br>
2022-02-21 08:59:04 -0300

    migration

---
+++ db/migrations/20220221115541_addcamposrhinssoutros.php
@@ -0,0 +1,19 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Addcamposrhinssoutros extends AbstractMigration
+{
+
+    public function up()
+    {
+        $sql = "
+            ALTER TABLE rhinssoutros ADD COLUMN rh51_indicadesconto int4;
+            ALTER TABLE rhinssoutros ADD COLUMN rh51_cgcvinculo varchar(14);
+            ALTER TABLE rhinssoutros ADD COLUMN rh51_categoria int4;
+
+            INSERT INTO db_syscampo VALUES ((select max(codcam)+1 from db_syscampo), 'rh51_cgcvinculo'       ,'text' ,'CNPJ/CPF do Outro Vinculo'     ,'', 'CNPJ/CPF do Outro Vinculo'      ,14 ,false, false, false, 0, 'text', 'CNPJ/CPF do Outro Vinculo');
+        ";
+        $this->execute($sql);
+    }
+}
