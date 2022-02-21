611bc2b40f1b7fa016721f1694c72ca70f439dab
Mario <Mario@contassconsultoria.com.br>
2022-02-21 08:33:00 -0300

    adicionado novos campos desconto

--- forms/db_frmrhinssoutros.php
+++ forms/db_frmrhinssoutros.php
@@ -33,138 +33,176 @@ $clrotulo->label("rh01_regist");
 $clrotulo->label("z01_nome");
 ?>
 <form name="form1" method="post" action="">
-<center>
-<fieldset style="width: 550px; margin-top:10px; margin-bottom:10px"><legend>Desconto Externo de Previdência</legend>
-<table border="0">
-  <tr>
-    <td nowrap title="<?=@$Trh01_regist?>">
-      <?
-      db_ancora(@$Lrh01_regist,"js_pesquisarh01_regist(true);",$db_opcao);
-      ?>
-    </td>
-    <td> 
-      <?
-      db_input('rh01_regist',6,$Irh01_regist,true,'text',$db_opcao," onchange='js_pesquisarh01_regist(false);'");
-      db_input('z01_nome',40,$Iz01_nome,true,'text',3,'');
-      db_input('rh51_seqpes',6,$Irh51_seqpes,true,'hidden',3,"");
-      ?>
-    </td>
-  </tr>
-  <tr>
-    <td nowrap title="<?=@$Trh51_basefo?>">
-      <?=@$Lrh51_basefo?>
-    </td>
-    <td> 
-      <?
-      db_input('rh51_basefo',15,$Irh51_basefo,true,'text',$db_opcao,"")
-      ?>
-    </td>
-  </tr>
-  <tr>
-    <td nowrap title="<?=@$Trh51_descfo?>">
-      <?=@$Lrh51_descfo?>
-    </td>
-    <td> 
-      <?
-      db_input('rh51_descfo',15,$Irh51_descfo,true,'text',$db_opcao,"")
-      ?>
-    </td>
-  </tr>
-  <tr>
-    <td nowrap title="<?=@$Trh51_b13fo?>">
-      <?=@$Lrh51_b13fo?>
-    </td>
-    <td> 
-      <?
-      db_input('rh51_b13fo',15,$Irh51_b13fo,true,'text',$db_opcao,"")
-      ?>
-    </td>
-  </tr>
-  <tr>
-    <td nowrap title="<?=@$Trh51_d13fo?>">
-      <?=@$Lrh51_d13fo?>
-    </td>
-    <td> 
-      <?
-      db_input('rh51_d13fo',15,$Irh51_d13fo,true,'text',$db_opcao,"")
-      ?>
-    </td>
-  </tr>
-  <tr>
-    <td nowrap title="<?=@$Trh51_ocorre?>">
-       <?=@$Lrh51_ocorre?>
-    </td>
-    <td colspan="3"> 
-       <?
-       $arr_ocorre = array(
-                           '05'=>'05 - Não exposto no momento',
-                           '06'=>'06 - Exposta (aposentadoria esp. 15 anos)',
-                           '07'=>'07 - Exposta (aposentadoria esp. 20 anos)',
-                           '08'=>'08 - Exposta (aposentadoria esp. 25 anos)'
-                          );
-       db_select("rh51_ocorre",$arr_ocorre,true,$db_opcao);
-       ?>
-    </td>
-  </tr>
-</table></fieldset>
-<?
-if($db_opcao == 1){
-?>
-<input name="incluir" type="submit" id="db_opcao" value="Incluir" onblur='js_tabulacaoforms("form1","rh01_regist",true,1,"rh01_regist",true);'>
-<?
-}else{
-?>
-<input name="alterar" type="submit" id="db_opcao" value="Alterar">
-<input name="excluir" type="submit" id="db_opcao" value="Excluir" onblur='js_tabulacaoforms("form1","rh01_regist",true,1,"rh01_regist",true);'>
-<?
-}
-?>
-</center>
+  <center>
+    <fieldset style="width: 580px; margin-top:10px; margin-bottom:10px">
+      <legend>Desconto Externo de Previdência</legend>
+      <table border="0">
+        <tr>
+          <td nowrap title="<?= @$Trh01_regist ?>">
+            <?
+            db_ancora(@$Lrh01_regist, "js_pesquisarh01_regist(true);", $db_opcao);
+            ?>
+          </td>
+          <td>
+            <?
+            db_input('rh01_regist', 6, $Irh01_regist, true, 'text', $db_opcao, " onchange='js_pesquisarh01_regist(false);'");
+            db_input('z01_nome', 40, $Iz01_nome, true, 'text', 3, '');
+            db_input('rh51_seqpes', 6, $Irh51_seqpes, true, 'hidden', 3, "");
+            ?>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <strong>Indicativo de Desconto:</strong>
+          </td>
+          <td>
+            <?
+            $arr_indica = array(
+              1 => 'Desconto do segurado sobre a remuneração informada',
+              2 => 'Desconto do segurado sobre a diferença',
+              3 => 'Não realiza desconto do segurado'
+            );
+            db_select("rh51_indicadesconto", $arr_indica, true, $db_opcao);
+            ?>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <strong>CNPJ/CPF do Outro Vínculo:</strong>
+          </td>
+          <td>
+            <?
+            db_input('rh51_cgcvinculo', 14, $Irh51_cgcvinculo, true, 'text', $db_opcao, "")
+            ?>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <strong>Categoria do Trabalhador no outro Vínculo:</strong>
+          </td>
+          <td>
+            <?
+            db_input('rh51_categoria', 3, $Irh51_categoria, true, 'text', $db_opcao, "")
+            ?>
+          </td>
+        </tr>
+        <tr>
+          <td nowrap title="<?= @$Trh51_basefo ?>">
+            <?= @$Lrh51_basefo ?>
+          </td>
+          <td>
+            <?
+            db_input('rh51_basefo', 15, $Irh51_basefo, true, 'text', $db_opcao, "")
+            ?>
+          </td>
+        </tr>
+        <tr>
+          <td nowrap title="<?= @$Trh51_descfo ?>">
+            <?= @$Lrh51_descfo ?>
+          </td>
+          <td>
+            <?
+            db_input('rh51_descfo', 15, $Irh51_descfo, true, 'text', $db_opcao, "")
+            ?>
+          </td>
+        </tr>
+        <tr>
+          <td nowrap title="<?= @$Trh51_b13fo ?>">
+            <?= @$Lrh51_b13fo ?>
+          </td>
+          <td>
+            <?
+            db_input('rh51_b13fo', 15, $Irh51_b13fo, true, 'text', $db_opcao, "")
+            ?>
+          </td>
+        </tr>
+        <tr>
+          <td nowrap title="<?= @$Trh51_d13fo ?>">
+            <?= @$Lrh51_d13fo ?>
+          </td>
+          <td>
+            <?
+            db_input('rh51_d13fo', 15, $Irh51_d13fo, true, 'text', $db_opcao, "")
+            ?>
+          </td>
+        </tr>
+        <tr>
+          <td nowrap title="<?= @$Trh51_ocorre ?>">
+            <?= @$Lrh51_ocorre ?>
+          </td>
+          <td colspan="3">
+            <?
+            $arr_ocorre = array(
+              '05' => '05 - Não exposto no momento',
+              '06' => '06 - Exposta (aposentadoria esp. 15 anos)',
+              '07' => '07 - Exposta (aposentadoria esp. 20 anos)',
+              '08' => '08 - Exposta (aposentadoria esp. 25 anos)'
+            );
+            db_select("rh51_ocorre", $arr_ocorre, true, $db_opcao);
+            ?>
+          </td>
+        </tr>
+      </table>
+    </fieldset>
+    <?
+    if ($db_opcao == 1) {
+    ?>
+      <input name="incluir" type="submit" id="db_opcao" value="Incluir" onblur='js_tabulacaoforms("form1","rh01_regist",true,1,"rh01_regist",true);'>
+    <?
+    } else {
+    ?>
+      <input name="alterar" type="submit" id="db_opcao" value="Alterar">
+      <input name="excluir" type="submit" id="db_opcao" value="Excluir" onblur='js_tabulacaoforms("form1","rh01_regist",true,1,"rh01_regist",true);'>
+    <?
+    }
+    ?>
+  </center>
 </form>
 <script>
-function js_pesquisarh01_regist(mostra){
-  if(mostra==true){
-    js_OpenJanelaIframe('top.corpo','db_iframe_rhpessoal','func_rhpessoal.php?testarescisao=ra&funcao_js=parent.js_preenchepesquisa|rh01_regist&instit=<?=(db_getsession("DB_instit"))?>','Pesquisa',true);
-  }else{
-    if(document.form1.rh01_regist.value != ''){ 
-      js_OpenJanelaIframe('top.corpo','db_iframe_rhpessoal','func_rhpessoal.php?testarescisao=ra&pesquisa_chave='+document.form1.rh01_regist.value+'&funcao_js=parent.js_mostrapessoal&instit=<?=(db_getsession("DB_instit"))?>','Pesquisa',false);
-    }else{
-      document.form1.z01_nome.value = '';
-      document.form1.rh51_basefo.value = ''; 
-      document.form1.rh51_descfo.value = ''; 
-      document.form1.rh51_b13fo.value = ''; 
-      document.form1.rh51_d13fo.value = ''; 
-      document.form1.rh51_seqpes.value = ''; 
+  function js_pesquisarh01_regist(mostra) {
+    if (mostra == true) {
+      js_OpenJanelaIframe('top.corpo', 'db_iframe_rhpessoal', 'func_rhpessoal.php?testarescisao=ra&funcao_js=parent.js_preenchepesquisa|rh01_regist&instit=<?= (db_getsession("DB_instit")) ?>', 'Pesquisa', true);
+    } else {
+      if (document.form1.rh01_regist.value != '') {
+        js_OpenJanelaIframe('top.corpo', 'db_iframe_rhpessoal', 'func_rhpessoal.php?testarescisao=ra&pesquisa_chave=' + document.form1.rh01_regist.value + '&funcao_js=parent.js_mostrapessoal&instit=<?= (db_getsession("DB_instit")) ?>', 'Pesquisa', false);
+      } else {
+        document.form1.z01_nome.value = '';
+        document.form1.rh51_basefo.value = '';
+        document.form1.rh51_descfo.value = '';
+        document.form1.rh51_b13fo.value = '';
+        document.form1.rh51_d13fo.value = '';
+        document.form1.rh51_seqpes.value = '';
+      }
     }
   }
-}
-function js_mostrapessoal(chave,erro){
-  document.form1.z01_nome.value = chave; 
-  if(erro==true){ 
-    document.form1.rh01_regist.focus(); 
-    document.form1.rh01_regist.value = ''; 
-    document.form1.rh51_basefo.value = ''; 
-    document.form1.rh51_descfo.value = ''; 
-    document.form1.rh51_b13fo.value = ''; 
-    document.form1.rh51_d13fo.value = ''; 
-    document.form1.rh51_seqpes.value = ''; 
-  }else{
-    js_preenchepesquisa(document.form1.rh01_regist.value);
+
+  function js_mostrapessoal(chave, erro) {
+    document.form1.z01_nome.value = chave;
+    if (erro == true) {
+      document.form1.rh01_regist.focus();
+      document.form1.rh01_regist.value = '';
+      document.form1.rh51_basefo.value = '';
+      document.form1.rh51_descfo.value = '';
+      document.form1.rh51_b13fo.value = '';
+      document.form1.rh51_d13fo.value = '';
+      document.form1.rh51_seqpes.value = '';
+    } else {
+      js_preenchepesquisa(document.form1.rh01_regist.value);
+    }
   }
-}
-/*
-function js_mostrapessoal1(chave1,chave2){
-  document.form1.rh01_regist.value = chave1;
-  document.form1.z01_nome.value   = chave2;
-  db_iframe_rhpessoal.hide();
-  js_submita();
-}
-function js_pesquisa(){
-  js_OpenJanelaIframe('top.corpo','db_iframe_rhpessoal','func_rhpessoal.php?funcao_js=parent.js_preenchepesquisa|rh01_regist&instit=<?=(db_getsession("DB_instit"))?>','Pesquisa',true);
-}
-*/
-function js_preenchepesquisa(chave){
-  db_iframe_rhpessoal.hide();
-  location.href = 'pes1_rhinssoutros001.php?chavepesquisa='+chave;
-}
+  /*
+  function js_mostrapessoal1(chave1,chave2){
+    document.form1.rh01_regist.value = chave1;
+    document.form1.z01_nome.value   = chave2;
+    db_iframe_rhpessoal.hide();
+    js_submita();
+  }
+  function js_pesquisa(){
+    js_OpenJanelaIframe('top.corpo','db_iframe_rhpessoal','func_rhpessoal.php?funcao_js=parent.js_preenchepesquisa|rh01_regist&instit=<?= (db_getsession("DB_instit")) ?>','Pesquisa',true);
+  }
+  */
+  function js_preenchepesquisa(chave) {
+    db_iframe_rhpessoal.hide();
+    location.href = 'pes1_rhinssoutros001.php?chavepesquisa=' + chave;
+  }
 </script>
