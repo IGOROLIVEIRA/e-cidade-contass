f6294347ae6517924d7bb427a4239cddb3a24259
Igor Afonso Oliveira Ruas <igorruas@Igors-MacBook-Pro.local>
2022-03-04 08:50:33 -0300

    OC16910 - criando layout educacenso

--- edu4_importaralunoscenso001.php
+++ edu4_importaralunoscenso001.php
@@ -1,7 +1,7 @@
 <?php
 /*
  *     E-cidade Software Publico para Gestao Municipal
- *  Copyright (C) 2014  DBselller Servicos de Informatica
+ *  Copyright (C) 2014  DBSeller Servicos de Informatica
  *                            www.dbseller.com.br
  *                         e-cidade@dbseller.com.br
  *
@@ -24,14 +24,6 @@
  *  Copia da licenca no diretorio licenca/licenca_en.txt
  *                                licenca/licenca_pt.txt
  */
-
-
-/**
- * Rotina Importação Censo para Clientes Novos
- *
- * Secretaria > Procedimentos > Censo Escolar > Importação > Alunos (Para Clientes Novos)
- */
-
 require_once("libs/db_stdlib.php");
 require_once("libs/db_stdlibwebseller.php");
 require_once("libs/db_utils.php");
@@ -43,207 +35,86 @@ require_once("dbforms/db_funcoes.php");
 require_once("model/dbLayoutReader.model.php");
 require_once("model/dbLayoutLinha.model.php");
 
-/**
- * Função que verifica se o arquivo de LOG possui mais de uma linha.
- *
- * @param  String $sLog  => Arquivo do LOG
- * @return boolean true  => Se existir mais de uma linha
- *                 false => Se existir uma linha
- *
- * @author Thiago A. de Lima - thiago.lima@dbseller.com.br
- */
-function webChecaLog($sLog) {
+$iAnoAtual = date("Y", db_getsession("DB_datausu"));
 
-  $iContador   = 0;
-  $pArquivoLog = fopen($sLog, "r");
-
-  $lVazio      = true;
-
-  while (!feof($pArquivoLog)) {
-
-    $sLinhaArquivo = fgets($pArquivoLog, 2000);
-    if (trim($sLinhaArquivo) != "") {
-      $iContador++;
-    }
-
-    if ($iContador > 1) {
-      $lVazio = false;
-      break;
-    }
-  }
-
-  if (!$lVazio) {
-
-    return true;
-  } else {
-    return false;
-  }
-
-}
-
-db_postmemory($HTTP_POST_VARS);
-
-$oDaoEscola = db_utils::getdao('escola');
 $iEscola    = db_getsession("DB_coddepto");
-$iAnoAtual  = date("Y",db_getsession("DB_datausu"));
-$oPost      = db_utils::postMemory($_POST);
-$oFile      = db_utils::postMemory($_FILES);
-
-if (isset($oPost->importar)) {
-
-  $lErro = false;
-
-  try {
-
-    db_inicio_transacao();
-    if (isset($oPost->codigoinep_banco) && trim($oPost->codigoinep_banco) != "") {
-      $iCodigoInepEscola = $oPost->codigoinep_banco;
-    } else {
-      $iCodigoInepEscola = null;
-    }
-
-    if (isset($oPost->ano_opcao)) {
-      $iAnoEscolhido = $oPost->ano_opcao;
-    }
-
-    if ($oPost->ano_opcao == 2011) {
-
-      require_once("model/educacao/importacaoAtualizacaoAluno2011.model.php");
-      $oCenso = new importacaoAtualizacaoAluno2011($oFile->arquivo['tmp_name'], $iAnoEscolhido, $iCodigoInepEscola, 96);
-    } elseif ($oPost->ano_opcao == 2010) {
-
-      require_once("model/educacao/importacaoAtualizacaoAluno2010.model.php");
-      $oCenso = new importacaoAtualizacaoAluno2010($oFile->arquivo['tmp_name'], $iAnoEscolhido, $iCodigoInepEscola, 98);
-      $oCenso->lLayoutComPipe = false;
-    } elseif ( in_array($oPost->ano_opcao, array(2012, 2013, 2014)) ) {
-
-      db_app::import("educacao.censo.DadosCenso");
-      db_app::import("educacao.censo.*");
-
-      $iCodigoLayout = 184;
-
-      switch ($oPost->ano_opcao) {
-
-        case 2013:
-          $iCodigoLayout = 199;
-          break;
-        case 2014:
-          $iCodigoLayout = 219;
-          break;
-        default:
-          $iCodigoLayout = 184;
-          break;
-      }
-
-      $oCenso = new ImportacaoCenso2012($oFile->arquivo['tmp_name'], $iAnoEscolhido, $iCodigoInepEscola = null, $iCodigoLayout);
-      $oCenso->lImportarAluno             = true;
-      $oCenso->lIncluirAlunoNaoEncontrado = true;
-      $oCenso->lImportarAlunoAtivo        = false;
-    } else {
-
-      db_msgbox("Arquivo do ano escolhido não existe!");
-      db_fim_transacao(true);
-      exit;
-    }
-
-    $oCenso->lIncluirAlunoNaoEncontrado = true;
-    $oCenso->importarArquivo();
-  } catch ( Exception $eException ) {
-
-    $lErro    = true;
-    $sMsgErro = $eException->getMessage();
-  }
-
-  db_fim_transacao($lErro);
-}
 ?>
 <html>
-  <head>
-    <title>DBSeller Inform&aacute;tica Ltda - P&aacute;gina Inicial</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-    <meta http-equiv="Expires" CONTENT="0">
-    <script language="JavaScript" type="text/javascript" src="scripts/scripts.js"></script>
-    <script language="JavaScript" type="text/javascript" src="scripts/strings.js"></script>
-    <link href="estilos.css" rel="stylesheet" type="text/css">
-  </head>
-  <body bgcolor=#CCCCCC leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" onLoad="a=1">
-	<table width="100%" border="0" cellpadding="0" cellspacing="0" bgcolor="#5786B2">
-		<tr>
-			<td width="360" height="18">&nbsp;</td>
-			<td width="263">&nbsp;</td>
-			<td width="25">&nbsp;</td>
-			<td width="140">&nbsp;</td>
-		</tr>
-	</table>
-	<form name="form1" method="post" action="" enctype="multipart/form-data">
-		<table width="100%" border="0" cellspacing="0" cellpadding="0">
-			<tr>
-				<td height="100%" align="left" valign="top" bgcolor="#CCCCCC"><br>
-					<center>
-						<fieldset style="width: 95%">
-							<legend>
-								<b>Importação de informações do CENSO ESCOLAR -> ALUNO</b>
-							</legend>
-							<table border="0" align="left">
-								<tr>
-									<td><br /> <b>Ano das informações do arquivo:</b> <select
-										name="ano_opcao">
+<head>
+  <title>DBSeller Inform&aacute;tica Ltda - P&aacute;gina Inicial</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+  <meta http-equiv="Expires" CONTENT="0">
+    <? db_app::load("scripts.js, prototype.js, strings.js, datagrid.widget.js"); ?>
+    <? db_app::load("estilos.css, grid.style.css"); ?>
+    <style type="text/css">
+        #arquivo {
+            height: 25px;
+        }
+    </style>
+</head>
+<body class="body-default" onLoad="a=1">
+<div class="container">
+  <form name="form1" method="post" action="edu4_importaralunoscensoprocessamento.php" enctype="multipart/form-data">
+    <fieldset>
+      <legend>Importação de Informações do CENSO ESCOLAR -> ALUNO</legend>
+      <table class="form-container">
+        <tr>
+          <td>
+            <label for="ano_opcao">Ano das Informações do Arquivo:</label>
+          </td>
+          <td>
+            <select id="ano_opcao" name="ano_opcao">
+              <option value="<?= $iAnoAtual ?>" <?= !empty($ano_opcao) == $iAnoAtual ? "selected" : "" ?>>
+                  <?= $iAnoAtual ?>
+              </option>
+              <option value="<?= $iAnoAtual - 1 ?>" <?= !empty($ano_opcao) == $iAnoAtual - 1 ? "selected" : "" ?>>
+                  <?= $iAnoAtual - 1 ?>
+              </option>
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label>Arquivo de Importação do Censo:</label>
+          </td>
+          <td>
+              <?php
+              db_input('arquivo', 50, '', true, 'file', 3, "");
+              ?>
+          </td>
+        </tr>
+      </table>
+    </fieldset>
+    <input name="importar" type="submit" id="importar" value="Importar"/>
+  </form>
+</div>
+<?php
+db_menu();
+?>
 
-											<option value="<?=$iAnoAtual?>"
-											<?=@$ano_opcao == $iAnoAtual ? "selected" : ""?>>
-												<?=$iAnoAtual?>
-											</option>
+<script>
+    var oUrl = js_urlToObject(null);
 
-											<option value="<?=$iAnoAtual-1?>"
-											<?=@$ano_opcao == $iAnoAtual-1 ? "selected" : ""?>>
-												<?=$iAnoAtual-1?>
-											</option>
+    if (oUrl.sArquivoLog){
+        emiteLog();
+    }
 
-									</select>
-									</td>
-								</tr>
-								<tr>
-									<td><b>Arquivo de importação do Censo:</b> <?db_input('arquivo', 50, @$Iarquivo, true, 'file', 3, "");?>
-										<br /> <br />
-									</td>
-								</tr>
-							</table>
-						</fieldset>
-					</center>
-				</td>
-			</tr>
-			<tr>
-				<td align="center">
-				  <br>
-				  <input name="importar" type="submit" id="importar" value="Importar" />
-				</td>
-			</tr>
-		</table>
-	</form>
-	<?php
-	    db_menu(db_getsession("DB_id_usuario"), db_getsession("DB_modulo"), db_getsession("DB_anousu"),db_getsession("DB_instit"));
-	?>
+    function emiteLog() {
+
+        var sMensagem = "Importação concluída com sucesso. Deseja imprimir os logs?";
+
+        if(oUrl.lTemInconsistencia) {
+          sMensagem = "Foram encontradas inconsistências no arquivo de importação. Deseja imprimir os logs?";
+        }
+
+        if(confirm(sMensagem)){
+            var sEndereco = 'edu4_importacaoatualizacaoaluno002.php?sArquivoErro='+oUrl.sArquivoLog;
+            var oJanela   = window.open(sEndereco,'', 'width='+(screen.availWidth-5)+',height='+(screen.availHeight-40)+',scrollbars=1,location=0');
+            oJanela.moveTo(0,0);
+        }
+        location.href = "edu4_importaralunoscenso001.php";
+    }
+</script>
+
 </body>
 </html>
-<?php
-if (isset($oPost->importar)) {
-
-  if ($lErro) {
-    db_msgbox(str_replace("\n","\\n",$sMsgErro));
-  } else {
-
-    if (webChecaLog($oCenso->sNomeArquivoLog)) {
-    ?>
-      <script>
-        var sEndereco = 'edu4_importacaoatualizacaoaluno002.php?sArquivoErro=<?=$oCenso->sNomeArquivoLog?>';
-        var oJanela   = window.open(sEndereco,'', 'width='+(screen.availWidth-5)+',height='+(screen.availHeight-40)+',scrollbars=1,location=0');
-        oJanela.moveTo(0,0);
-      </script>
-    <?php
-    } else {
-      db_msgbox("Importação dos Dados Realizada com Sucesso.");
-    }
-  }
-  db_redireciona("edu4_importaralunoscenso001.php");
-}
-?>
