288249ae478a348eb2e86ed7492c9073602e4569
Raimundo Barbosa <raimundo2704@gmail.com>
2022-03-14 10:12:29 -0300

    OC17027 - Customização do Modelo de NF Avulsa - Montalvânia

--- fpdf151/impmodelos/mod_imprime492.php
+++ fpdf151/impmodelos/mod_imprime492.php
@@ -42,7 +42,11 @@ if ($this->notaCancelada) {
 $this->objpdf->sety($xlin + 3);
 $this->objpdf->setx($xlin + 19);
 $this->objpdf->Setfont('Arial', 'B', 10);
-$this->objpdf->cell(121, 5, "NOTA FISCAL DE SERVIÇOS ELETRÔNICA - AVULSA", 0, 1, "C");
+if ($oInstit->getCodigoCliente() == Instituicao::COD_CLI_PMMONTALVANIA){
+    $this->objpdf->cell(121, 5, "NOTA FISCAL DE SERVIÇOS - AVULSA", 0, 1, "C");
+}else{
+    $this->objpdf->cell(121, 5, "NOTA FISCAL DE SERVIÇOS ELETRÔNICA - AVULSA", 0, 1, "C");
+}
 
 //Numero da nota
 $this->objpdf->sety(2);
@@ -329,5 +333,13 @@ $this->objpdf->sety(250);
 $this->objpdf->Setfont('Arial', 'B', 10);
 $this->objpdf->cell(30, 20,"Avisos", "LTB", 0, "C");
 $this->objpdf->Setfont('Arial', '', 8);
-$this->objpdf->rect(40, $this->yOld+40, 160, 20);
-$this->objpdf->MultiCell(160, 5,$this->texto_aviso, 0, "L");
+if ($oInstit->getCodigoCliente() == Instituicao::COD_CLI_PMMONTALVANIA){
+    $this->objpdf->rect(40, $this->yOld + 30, 100, 20);
+    $this->objpdf->MultiCell(100, 5,$this->texto_aviso, 0, "L");
+    $this->objpdf->rect(140, $this->yOld + 30, 60, 20);
+    $this->objpdf->setX(160);
+    $this->objpdf->cell(30, 5,"Servidor(a) Emitente", 0, "C");
+}else{
+    $this->objpdf->rect(40, $this->yOld + 30, 160, 20);
+    $this->objpdf->MultiCell(160, 5,$this->texto_aviso, 0, "L");
+}
