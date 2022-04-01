8377a869163fd6e8f8467fbc67777bf74746f256
Mario <Mario@contassconsultoria.com.br>
2022-04-01 10:35:46 -0300

    evento 2230

--- con4_cargaformularioseventosnaoperiodicos.php
+++ con4_cargaformularioseventosnaoperiodicos.php
@@ -78,6 +78,14 @@ require_once("dbforms/db_funcoes.php");
                         <label>Desligamento s2299</label>
                     </td>
                 </tr>
+                <tr>
+                    <td>
+                        <input type="checkbox" value="4000108">
+                    </td>
+                    <td>
+                        <label>S-2230 - Afastamento Temporario</label>
+                    </td>
+                </tr>
             </table>
         </fieldset>
         <input type="button" value="Processar" id="btnProcessar">
