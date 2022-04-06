481e856c2915d6ed4e1618b93509aa689acf398a
Elvio Amaral <elvio@contassconsultoria.com.br>
2022-04-06 18:10:13 -0300

    OC 17014 - Solicitação de Cadastro de estruturais no PCASP (change query database name and if position).

--- db/migrations/20220329181525_oc17014.php
+++ db/migrations/20220329181525_oc17014.php
@@ -8,7 +8,7 @@ class Oc17014 extends PostgresMigration
     {
         $jsonData = file_get_contents('db/migrations/20220329182150_oc17014.json');
         $jsonDados = json_decode($jsonData);
-        $dbName = $this->fetchAll("SELECT datname FROM pg_database WHERE datname = 'e-cidade-zerada-2019'");
+        $dbName = $this->fetchAll("SELECT datname FROM pg_database WHERE current_database() = 'e-cidade-zerada-2019'");
         $clientInstit = $this->fetchAll("SELECT codigo FROM db_config WHERE cgc IN ('18650945000114','17097791000112', '01612501000191')");
 
         if (!empty($dbName) || !empty($clientInstit)) {
@@ -36,28 +36,29 @@ class Oc17014 extends PostgresMigration
 
                         $aConPlano = array($c60_codcon, $c60_anousu, $c60_estrut, $c60_descr, $c60_finali, $c60_codsis, $c60_codcla, $c60_consistemaconta, $c60_naturezasaldo, $c60_nregobrig, $c60_infcompmsc);
                         $this->insertConplano($aConPlano);
-                    }
-
-                    if ($c60_infcompmsc == 1) {
-
-                        $aInstit = $this->fetchAll("SELECT DISTINCT codigo FROM db_config JOIN conplanoreduz ON (c61_instit, c61_anousu) = (codigo, {$ano['c60_anousu']})");
-
-                        foreach ($aInstit as $instituicao) {
-
-                            $c61_codcon        = $c60_codcon;
-                            $c61_anousu        = $ano['c60_anousu'];
-                            $c61_reduz         = current($this->fetchRow("select nextval('conplanoreduz_c61_reduz_seq')"));
-                            $c61_instit        = $instituicao['codigo'];
-                            $c61_codigo        = 1;
-                            $c61_contrapartida = 0;
-
-                            $aConPlanoReduz    = array($c61_codcon, $c61_anousu, $c61_reduz, $c61_instit, $c61_codigo, $c61_contrapartida);
-                            $this->insertConplanoReduz($aConPlanoReduz);
-
-                            $aConplanoExe      = array($c61_anousu, $c61_reduz, $c61_codigo, 0, 0);
-                            $this->insertConplanoExe($aConplanoExe);
+                        
+                        if ($c60_infcompmsc == 1) {
+    
+                            $aInstit = $this->fetchAll("SELECT DISTINCT codigo FROM db_config JOIN conplanoreduz ON (c61_instit, c61_anousu) = (codigo, {$ano['c60_anousu']})");
+    
+                            foreach ($aInstit as $instituicao) {
+    
+                                $c61_codcon        = $c60_codcon;
+                                $c61_anousu        = $ano['c60_anousu'];
+                                $c61_reduz         = current($this->fetchRow("select nextval('conplanoreduz_c61_reduz_seq')"));
+                                $c61_instit        = $instituicao['codigo'];
+                                $c61_codigo        = 1;
+                                $c61_contrapartida = 0;
+    
+                                $aConPlanoReduz    = array($c61_codcon, $c61_anousu, $c61_reduz, $c61_instit, $c61_codigo, $c61_contrapartida);
+                                $this->insertConplanoReduz($aConPlanoReduz);
+    
+                                $aConplanoExe      = array($c61_anousu, $c61_reduz, $c61_codigo, 0, 0);
+                                $this->insertConplanoExe($aConplanoExe);
+                            }
                         }
                     }
+
                 }
             }
         }
