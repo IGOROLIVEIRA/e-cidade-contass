07995813a075257898e7e1d658aadbad653dda66
Mario <Mario@contassconsultoria.com.br>
2022-05-06 07:57:37 -0300

    dados envio

--- src/RecursosHumanos/ESocial/Agendamento/Eventos/EventoS2230.php
+++ src/RecursosHumanos/ESocial/Agendamento/Eventos/EventoS2230.php
@@ -30,38 +30,22 @@ class EventoS2230 extends EventoBase
     public function montarDados()
     {
         $aDadosAPI = array();
-         echo "evento";
-         echo '<pre>';
-         print_r($this->dados);
-         exit;
-
         $iSequencial = 1;
         foreach ($this->dados as $oDados) {
-           
+
             $oDadosAPI                                = new \stdClass;
             $oDadosAPI->evtAfastTemp                      = new \stdClass;
             $oDadosAPI->evtAfastTemp->sequencial          = $iSequencial;
             $oDadosAPI->evtAfastTemp->modo                = $this->modo;
             $oDadosAPI->evtAfastTemp->indRetif            = 1;
             $oDadosAPI->evtAfastTemp->nrRecibo            = null;
-            $oDadosAPI->evtAfastTemp->cpfTrab             = $oDados->trabalhador->cpfTrab;
-            $oDadosAPI->evtAfastTemp->nisTrab             = $oDados->trabalhador->nisTrab;
-            $oDadosAPI->evtAfastTemp->nmTrab              = $oDados->trabalhador->nmTrab;
-            $oDadosAPI->evtAfastTemp->sexo                = $oDados->trabalhador->sexo;
-            $oDadosAPI->evtAfastTemp->racaCor             = $oDados->trabalhador->racaCor;
-            $oDadosAPI->evtAfastTemp->estCiv              = empty($oDados->trabalhador->estCiv) ? null : $oDados->trabalhador->estCiv;
-            $oDadosAPI->evtAfastTemp->grauInstr           = $oDados->trabalhador->grauInstr;
-            $oDadosAPI->evtAfastTemp->indPriEmpr          = $oDados->trabalhador->indPriEmpr;
-            $oDadosAPI->evtAfastTemp->nmSoc               = empty($oDados->trabalhador->nmSoc) ? null : $oDados->trabalhador->nmSoc;
-
-            $oDadosAPI->evtAfastTemp->dtNascto            = $oDados->nascimento->dtNascto;
-            $oDadosAPI->evtAfastTemp->codMunic            = empty($oDados->nascimento->codMunic) ? null : $oDados->nascimento->codMunic;
-            $oDadosAPI->evtAfastTemp->uf                  = empty($oDados->nascimento->uf) ? null : $oDados->nascimento->uf;
-            $oDadosAPI->evtAfastTemp->paisNascto          = $oDados->nascimento->paisNascto;
-            $oDadosAPI->evtAfastTemp->paisNac             = $oDados->nascimento->paisNac;
-            $oDadosAPI->evtAfastTemp->nmMae               = empty($oDados->nascimento->nmMae) ? null : $oDados->nascimento->nmMae;
-            $oDadosAPI->evtAfastTemp->nmPai               = empty($oDados->nascimento->nmPai) ? null : $oDados->nascimento->nmPai;
-
+            $oDadosAPI->evtAfastTemp->idevinculo->cpftrab                     = $oDados->ideVinculo->cpfTrab;
+            $oDadosAPI->evtAfastTemp->idevinculo->matricula                   = $oDados->ideVinculo->matricula;
+            $oDadosAPI->evtAfastTemp->idevinculo->codcateg                    = $oDados->ideVinculo->codCateg;
+            $oDadosAPI->evtAfastTemp->iniafastamento->dtiniafast              = $oDados->perAquis->dtInicio;
+            $oDadosAPI->evtAfastTemp->iniafastamento->codmotafast             = $oDados->iniAfastamento->codMotAfast;
+            $oDadosAPI->evtAfastTemp->iniafastamento->peraquis->dtinicio      = $oDados->perAquis->dtInicio;
+            $oDadosAPI->evtAfastTemp->iniafastamento->peraquis->dtfim         = $oDados->perAquis->dtFim;
             $aDadosAPI[] = $oDadosAPI;
             $iSequencial++;
         }
