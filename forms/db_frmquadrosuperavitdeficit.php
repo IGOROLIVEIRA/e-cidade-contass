8660ec3aeef12cafda650d99013de01a47f22ad9
widouglas <widouglasmail@gmail.com>
2022-05-24 14:27:08 -0300

    Oc17280 - Correcao de Fontes Inexistentes

--- forms/db_frmquadrosuperavitdeficit.php
+++ forms/db_frmquadrosuperavitdeficit.php
@@ -1,226 +1,234 @@
 
 <div class="registro_empinscritosemrp">
-    <?php
+<?php
 
-    $clorctiporec = new cl_orctiporec();
-    $sql = "select distinct concat('1', substring(o15_codtri, 2, 2)) as o15_codtri, o15_descr from orctiporec o1 where o15_codtri != '' and o15_codtri::int >= 100 AND o15_codtri::int <= 299 AND (o15_datalimite IS NULL OR o15_datalimite <= '" . db_getsession("DB_anousu") . "-12-31') order by concat('1', substring(o15_codtri, 2, 2)), o15_descr";
+$clorctiporec = new cl_orctiporec();
+$sql = "select distinct concat('1', substring(o15_codtri, 2, 2)) as o15_codtri from orctiporec o1 where o15_codtri != '' and o15_codtri::int >= 100 AND o15_codtri::int <= 299 AND (o15_datalimite IS NULL OR o15_datalimite <= '" . db_getsession("DB_anousu") . "-12-31') order by concat('1', substring(o15_codtri, 2, 2))";
 
-    $recursos = $clorctiporec->sql_record($sql);
+$recursos = $clorctiporec->sql_record($sql);
 
-    ?>
-    <form name="form1" method="post" action="" style="" onsubmit="return validaForm(this);">
-        <fieldset style="width:0px">
-            <legend align="center">
-                <strong>Quadro do Superávit / Déficit Financeiro</strong>
-            </legend>
-            <table class="DBGrid" style="width: 800px">
-                <tr>
-                    <th class="table_header">Fonte</th>
-                    <th class="table_header">Descrição</th>
-                    <th class="table_header">Valor</th>
-                    <th class="table_header">Suplementado</th>
-                    <th class="table_header">Saldo</th>
-                </tr>
-            <?php
-            $aRecurso = db_utils::getCollectionByRecord($recursos);
-            $iTotalFontes = 0;
-            $arrayEmpenhos = 0;
+?>
+<form name="form1" method="post" action="" style="" onsubmit="return validaForm(this);">
+    <fieldset style="width:0px">
+        <legend align="center">
+            <strong>Quadro do Superávit / Déficit Financeiro</strong>
+        </legend>
+        <table class="DBGrid" style="width: 600px">
+            <tr>
+                <th class="table_header">Fonte</th>
+                <th class="table_header">Valor</th>
+                <th class="table_header">Suplementado</th>
+                <th class="table_header">Saldo</th>
+            </tr>
+        <?php
+        $aRecurso = db_utils::getCollectionByRecord($recursos);
+        $iTotalFontes = 0;
+        $arrayEmpenhos = 0;
 
-            foreach ($aRecurso as $oFot) :
-                $iFonte = ltrim($oFot->o15_codtri, "0");
+        foreach ($aRecurso as $oFot) :
+            $iFonte = ltrim($oFot->o15_codtri, "0");
+        ?>
+
+            <tr>
+                <td class="linhagrid">
+                    <?= $iFonte ?>
+                    <input type="hidden" name="aFonte[<?= $iFonte ?>][fonte]" value="<?= $oFot->o15_codtri ?>" id="">
+                </td>
+
+                <td class="linhagrid">
+                    <input type="text" style="width: 100px; text-align: right" name="aFonte[<?= $iFonte ?>][valor]" value="0.00" size="16" maxlength="" onkeyup="js_ValidaCampos(this, 4, 'valor', false, null, event)" onblur="js_ValidaMaiusculo(this,'',event);" oninput="js_ValidaCampos(this,0,'','','',event);" onkeydown="return js_controla_tecla_enter(this,event);" id="">
+                </td>
+
+                <td class="linhagrid">
+                    <input type="text" readonly="true" style="width: 100px; background: #DEB887; text-align: right" name="aFonte[<?= $iFonte ?>][suplementado]"  value="0.00" size="16" maxlength="" onkeyup="js_ValidaCampos(this, 4, 'valor', false, null, event)" onblur="js_ValidaMaiusculo(this,'',event);" oninput="js_ValidaCampos(this,0,'','','',event);" onkeydown="return js_controla_tecla_enter(this,event);" id="">
+                </td>
+                
+                <td class="linhagrid">
+                    <input type="text" readonly="true" style="width: 100px; background: #DEB887; text-align: right" name="aFonte[<?= $iFonte ?>][saldo]"  value="0.00" size="16" maxlength="" onkeyup="js_ValidaCampos(this, 4, 'valor', false, null, event)" onblur="js_ValidaMaiusculo(this,'',event);" oninput="js_ValidaCampos(this,0,'','','',event);" onkeydown="return js_controla_tecla_enter(this,event);" id="">
+                </td>
+            </tr>
+            <?
+            $iTotalFontes++;
+
+            $arrayFontes[$iFonte] = $iFonte;
+
             ?>
 
-                <tr>
-                    <td class="linhagrid">
-                        <?= $iFonte ?>
-                        <input type="hidden" name="aFonte[<?= $iFonte ?>][fonte]" value="<?= $oFot->o15_codtri ?>" id="">
-                    </td>
-
-                    <td class="linhagrid" style="text-align: left;">
-                        <?= $oFot->o15_descr ?>
-                    </td>
-
-                    <td class="linhagrid">
-                        <input type="text" style="width: 100px; text-align: right" name="aFonte[<?= $iFonte ?>][valor]" value="0.00" size="16" maxlength="" onkeyup="js_ValidaCampos(this, 4, 'valor', false, null, event)" onblur="js_ValidaMaiusculo(this,'',event);" oninput="js_ValidaCampos(this,0,'','','',event);" onkeydown="return js_controla_tecla_enter(this,event);" id="">
-                    </td>
-
-                    <td class="linhagrid">
-                        <input type="text" readonly="true" style="width: 100px; background: #DEB887; text-align: right" name="aFonte[<?= $iFonte ?>][suplementado]"  value="0.00" size="16" maxlength="" onkeyup="js_ValidaCampos(this, 4, 'valor', false, null, event)" onblur="js_ValidaMaiusculo(this,'',event);" oninput="js_ValidaCampos(this,0,'','','',event);" onkeydown="return js_controla_tecla_enter(this,event);" id="">
-                    </td>
-                    
-                    <td class="linhagrid">
-                        <input type="text" readonly="true" style="width: 100px; background: #DEB887; text-align: right" name="aFonte[<?= $iFonte ?>][saldo]"  value="0.00" size="16" maxlength="" onkeyup="js_ValidaCampos(this, 4, 'valor', false, null, event)" onblur="js_ValidaMaiusculo(this,'',event);" oninput="js_ValidaCampos(this,0,'','','',event);" onkeydown="return js_controla_tecla_enter(this,event);" id="">
-                    </td>
-                </tr>
-                <?
-                $iTotalFontes++;
-
-                $arrayFontes[$iFonte] = $iFonte;
-
-                ?>
-
-            <?php endforeach; ?>
-            </table>
-            <center>
-                <input type="submit" value="Salvar" id="btmSalvar" name="incluir">
-                <input type="button" onclick="importar()" value="Importar" id="btmImportar" name="">
-            </center>
-        </fieldset>
-    </form>
+        <?php endforeach; ?>
+        </table>
+        <center>
+            <input type="submit" value="Salvar" id="btmSalvar" name="incluir">
+            <input type="button" onclick="importar()" value="Importar" id="btmImportar" name="">
+        </center>
+    </fieldset>
+</form>
 </div>
 <script>
-    carregarvalores();
+carregarvalores();
 
-    /**
-     * função para carregar valores na tela
-     */
+/**
+ * função para carregar valores na tela
+ */
 
-    function carregarvalores() {
-        console.log("Estou aqui");
-        buscarvalores({
-            exec: 'getValores',
-            // fonte: fonte
-        }, js_carregarValores);
-        console.log("Aqui tbem");
-        
-        buscarvalores({
-            exec: 'getSuplementado',
-            // fonte: fonte
-        }, js_carregarValoresSuplementados);
-        
-    }
+function carregarvalores() {
+    console.log("Estou aqui");
+    buscarvalores({
+        exec: 'getValores',
+        // fonte: fonte
+    }, js_carregarValores);
+    console.log("Aqui tbem");
+    
+    buscarvalores({
+        exec: 'getSuplementado',
+        // fonte: fonte
+    }, js_carregarValoresSuplementados);
+    
+}
 
-    function importar()
-    {
-        buscarvalores({
-            exec: 'getImportar',
-            // fonte: fonte
-        }, js_carregarValores); 
-    }
+function importar()
+{
+    buscarvalores({
+        exec: 'getImportar',
+        // fonte: fonte
+    }, js_carregarValores); 
+}
 
-    function js_carregarValores(oRetorno) {
-
-        var valores = JSON.parse(oRetorno.responseText.urlDecode());
-        // console.log(valores);
-        for (var [key, fonte] of Object.entries(valores.fonte)) {
-            var valor = parseFloat(fonte.c241_valor);
+function js_carregarValores(oRetorno) {
+    var valores = JSON.parse(oRetorno.responseText.urlDecode());
+    // console.log(valores);
+    
+    for (var [key, fonte] of Object.entries(valores.fonte)) {
+        // console.log(fonte);
+        // console.log("Estou no primeiro chaveamento");
+        var valor = parseFloat(fonte.c241_valor);
+        if (document.form1['aFonte[' + fonte.c241_fonte + '][valor]']) {
+            // console.log("Fonte: " + fonte.c241_fonte);
+            // console.log("Valor: " + valor);
+            // console.log("Valor do campo antes: " + document.form1['aFonte[' + fonte.c241_fonte + '][valor]'].value);
             document.form1['aFonte[' + fonte.c241_fonte + '][valor]'].value = valor.toFixed(2);
+            // console.log("Valor do campo depois: " + document.form1['aFonte[' + fonte.c241_fonte + '][valor]'].value);
             var valor = fonte.c241_valor - document.form1['aFonte[' + fonte.c241_fonte + '][suplementado]'].value;
-            document.form1['aFonte[' + fonte.c241_fonte + '][saldo]'].value = valor.toFixed(2);
+            // document.form1['aFonte[' + fonte.c241_fonte + '][saldo]'].value = valor.toFixed(2);
         }
+    }
 
-        valores.fonte.forEach(function(fonte, b) {
+    valores.fonte.forEach(function(fonte, b) {
+        var valor = parseFloat(fonte.c241_valor);
+        if (document.form1['aFonte[' + fonte.c241_fonte + '][valor]']) {
             document.form1['aFonte[' + fonte.c241_fonte + '][valor]'].value = fonte.c241_valor;
             var valor = fonte.c241_valor - document.form1['aFonte[' + fonte.c241_fonte + '][suplementado]'].value;
             document.form1['aFonte[' + fonte.c241_fonte + '][saldo]'].value = valor.toFixed(2);
-        });
-    }
+        }
+    });
+}
 
-    function js_carregarValoresSuplementados(oRetorno) {
-        var valores = JSON.parse(oRetorno.responseText.urlDecode());
-        console.log(valores);
-        valores.fonte.forEach(function(fonte, b) {
+function js_carregarValoresSuplementados(oRetorno) {
+    var valores = JSON.parse(oRetorno.responseText.urlDecode());
+
+    valores.fonte.forEach(function(fonte, b) {
+        if (document.form1['aFonte[' + fonte.fonte + '][suplementado]']) {
             document.form1['aFonte[' + fonte.fonte + '][suplementado]'].value = fonte.valor;
             var valor = document.form1['aFonte[' + fonte.fonte + '][valor]'].value - fonte.valor;
             document.form1['aFonte[' + fonte.fonte + '][saldo]'].value = valor.toFixed(2);
-        });
-    }
-
-    function buscarvalores(params, onComplete) {
-        js_divCarregando('Carregando Valores', 'div_aguarde');
-        var request = new Ajax.Request('quadrosuperavitdeficit.RPC.php', {
-            method: 'post',
-            parameters: 'json=' + JSON.stringify(params),
-            onComplete: function(res) {
-                js_removeObj('div_aguarde');
-                onComplete(res);
-            }
-        });
-    }
-
-    /**
-     * função para carregar valores do SICOM
-     */
-
-    function carregarSicom() {
-        document.form1.reset();
-        buscarSicom({
-            exec: 'getSicom',
-            // fonte: fonte
-        }, Atualizar);
-    }
-
-    function Atualizar(oRetorno) {
-        var valoresSicom = JSON.parse(oRetorno.responseText.urlDecode());
-        if (valoresSicom == 0) {
-            alert('Gere os os arquivos CTB, CAIXA e CUTE do mês de dezembro para atualizar a disponibilidade');
-            return false;
         }
+    });
+}
 
-        valoresSicom.oDados.forEach(function(dadosfonte, b) {
-            Object.keys(dadosfonte).forEach(function(fonte, key) {
-                if (dadosfonte[fonte].vlrcaixabruta == undefined) {
-                    document.form1['aFonte[' + fonte + '][valor]'].value = 0;
-                } else {
-                    document.form1['aFonte[' + fonte + '][valor]'].value = dadosfonte[fonte].vlrcaixabruta;
-                }
+function buscarvalores(params, onComplete) {
+    js_divCarregando('Carregando Valores', 'div_aguarde');
+    var request = new Ajax.Request('quadrosuperavitdeficit.RPC.php', {
+        method: 'post',
+        parameters: 'json=' + JSON.stringify(params),
+        onComplete: function(res) {
+            js_removeObj('div_aguarde');
+            onComplete(res);
+        }
+    });
+}
 
-                if (dadosfonte[fonte].VlrroexercicioAnteriores == undefined) {
-                    document.form1['aFonte[' + fonte + '][vlr_rpExerAnteriores]'].value = 0;
-                } else {
-                    document.form1['aFonte[' + fonte + '][vlr_rpExerAnteriores]'].value = dadosfonte[fonte].VlrroexercicioAnteriores;
-                }
+/**
+ * função para carregar valores do SICOM
+ */
 
-                // if(dadosfonte[fonte].vlrrestorecolher == undefined){
-                //   document.form1['aFonte[' + fonte + '][vlr_restArecolher]'].value = 0;
-                // }else{
-                //   document.form1['aFonte[' + fonte + '][vlr_restArecolher]'].value = dadosfonte[fonte].vlrrestorecolher;
-                // }
-                //
-                // if(dadosfonte[fonte].vlrAtivoFian == undefined){
-                //   document.form1['aFonte[' + fonte + '][vlr_restRegAtivoFinan]'].value = 0;
-                // }else{
-                //   document.form1['aFonte[' + fonte + '][vlr_restRegAtivoFinan]'].value = dadosfonte[fonte].vlrAtivoFian;
-                // }
+function carregarSicom() {
+    document.form1.reset();
+    buscarSicom({
+        exec: 'getSicom',
+        // fonte: fonte
+    }, Atualizar);
+}
 
-                document.form1['aFonte[' + fonte + '][vlr_DispCaixa]'].value = dadosfonte[fonte].vlrDisponibilidade;
-
-            });
-        });
+function Atualizar(oRetorno) {
+    var valoresSicom = JSON.parse(oRetorno.responseText.urlDecode());
+    if (valoresSicom == 0) {
+        alert('Gere os os arquivos CTB, CAIXA e CUTE do mês de dezembro para atualizar a disponibilidade');
+        return false;
     }
 
-    function buscarSicom(params, onComplete) {
-        js_divCarregando('Carregando Valores', 'div_aguarde');
-        var request = new Ajax.Request('cadastrodespesainscritarp.RPC.php', {
-            method: 'post',
-            parameters: 'json=' + JSON.stringify(params),
-            onComplete: function(res) {
-                js_removeObj('div_aguarde');
-                onComplete(res);
+    valoresSicom.oDados.forEach(function(dadosfonte, b) {
+        Object.keys(dadosfonte).forEach(function(fonte, key) {
+            if (dadosfonte[fonte].vlrcaixabruta == undefined) {
+                document.form1['aFonte[' + fonte + '][valor]'].value = 0;
+            } else {
+                document.form1['aFonte[' + fonte + '][valor]'].value = dadosfonte[fonte].vlrcaixabruta;
             }
+
+            if (dadosfonte[fonte].VlrroexercicioAnteriores == undefined) {
+                document.form1['aFonte[' + fonte + '][vlr_rpExerAnteriores]'].value = 0;
+            } else {
+                document.form1['aFonte[' + fonte + '][vlr_rpExerAnteriores]'].value = dadosfonte[fonte].VlrroexercicioAnteriores;
+            }
+
+            // if(dadosfonte[fonte].vlrrestorecolher == undefined){
+            //   document.form1['aFonte[' + fonte + '][vlr_restArecolher]'].value = 0;
+            // }else{
+            //   document.form1['aFonte[' + fonte + '][vlr_restArecolher]'].value = dadosfonte[fonte].vlrrestorecolher;
+            // }
+            //
+            // if(dadosfonte[fonte].vlrAtivoFian == undefined){
+            //   document.form1['aFonte[' + fonte + '][vlr_restRegAtivoFinan]'].value = 0;
+            // }else{
+            //   document.form1['aFonte[' + fonte + '][vlr_restRegAtivoFinan]'].value = dadosfonte[fonte].vlrAtivoFian;
+            // }
+
+            document.form1['aFonte[' + fonte + '][vlr_DispCaixa]'].value = dadosfonte[fonte].vlrDisponibilidade;
+
         });
-    }
+    });
+}
 
-    /**
-     * Calculo disponibilidade de caixa quando digitar nos inputs da tela
-     *
-     */
+function buscarSicom(params, onComplete) {
+    js_divCarregando('Carregando Valores', 'div_aguarde');
+    var request = new Ajax.Request('cadastrodespesainscritarp.RPC.php', {
+        method: 'post',
+        parameters: 'json=' + JSON.stringify(params),
+        onComplete: function(res) {
+            js_removeObj('div_aguarde');
+            onComplete(res);
+        }
+    });
+}
 
-    function js_CalculaDisponibilidade(fonte) {
+/**
+ * Calculo disponibilidade de caixa quando digitar nos inputs da tela
+ *
+ */
 
-        let vlrDisCaixaBruta = document.form1['aFonte[' + fonte + '][valor]'].value;
-        let vlrRpExercicioanterior = document.form1['aFonte[' + fonte + '][vlr_rpExerAnteriores]'].value;
-        // let vlrRestoRecolher       = document.form1['aFonte[' + fonte + '][vlr_restArecolher]'].value;
-        // let vlrRegAtivoFian        = document.form1['aFonte[' + fonte + '][vlr_restRegAtivoFinan]'].value;
-        let ResultVlrDisponibilidade = Number(vlrDisCaixaBruta) - Number(vlrRpExercicioanterior);
+function js_CalculaDisponibilidade(fonte) {
 
-        //document.form1['aFonte[' + fonte + '][vlr_DispCaixa]'].value = ResultVlrDisponibilidade.toFixed(2)
-        document.form1['aFonte[' + fonte + '][vlr_DispCaixa]'].value = ResultVlrDisponibilidade.toFixed(2)
+    let vlrDisCaixaBruta = document.form1['aFonte[' + fonte + '][valor]'].value;
+    let vlrRpExercicioanterior = document.form1['aFonte[' + fonte + '][vlr_rpExerAnteriores]'].value;
+    // let vlrRestoRecolher       = document.form1['aFonte[' + fonte + '][vlr_restArecolher]'].value;
+    // let vlrRegAtivoFian        = document.form1['aFonte[' + fonte + '][vlr_restRegAtivoFinan]'].value;
+    let ResultVlrDisponibilidade = Number(vlrDisCaixaBruta) - Number(vlrRpExercicioanterior);
 
-    }
+    //document.form1['aFonte[' + fonte + '][vlr_DispCaixa]'].value = ResultVlrDisponibilidade.toFixed(2)
+    document.form1['aFonte[' + fonte + '][vlr_DispCaixa]'].value = ResultVlrDisponibilidade.toFixed(2)
 
-    function validaForm(form) {
-        console.log(form);
-        return true;
-    }
+}
+
+function validaForm(form) {
+    console.log(form);
+    return true;
+}
 </script>
