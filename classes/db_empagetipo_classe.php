04ec9387f76c40098e905b64e77bdb1c7cdfbd8b
dayvison <dayvison.marley@contassconsultoria.com.br>
2022-06-08 15:29:17 -0300

    OC17802 - Solicitação de validação na rotina Manutenção de Pagamentos

--- classes/db_empagetipo_classe.php
+++ classes/db_empagetipo_classe.php
@@ -883,8 +883,10 @@ class cl_empagetipo {
       $sSql .= " and c61_anousu =".db_getsession("DB_anousu");
     }
     /* OC11508 Verificação adicionada para permitir utilização do recurso 160/260 na fonte 100 */
-    if(substr($iFonteEmpenho, 1, 2) == '60') {
-      $sSql .= " and e83_descr like '%FEP' ";
+    if($iAnoUsu < 2022){
+        if(substr($iFonteEmpenho, 1, 2) == '60') {
+          $sSql .= " and e83_descr like '%FEP' ";
+        }
     }
     /* OC12503 Verificação adicionada para permitir utilização do recurso 161/261 na fonte 100 */
     if(substr($iFonteEmpenho, 1, 2) == '61') {
