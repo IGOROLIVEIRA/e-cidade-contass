a3052a8e74ca8ca567232c4c58436705aaa8e99b
widouglas <widouglasmail@gmail.com>
2022-07-01 16:28:14 -0300

    Oc17942 - Correcao do index

--- db/migrations/20220701164732_oc17942.php
+++ db/migrations/20220701164732_oc17942.php
@@ -6,7 +6,7 @@ class Oc17942 extends AbstractMigration
 {
     public function up()
     {
-        $sql = 'DROP INDEX "contabilidade"."idx_fonte_ano";';
+        $sql = 'DROP INDEX "contabilidade"."idx_fonte_anousu";';
         $this->execute($sql);
         $sql = 'CREATE UNIQUE INDEX idx_fonte_ano ON "contabilidade"."quadrosuperavitdeficit"(c241_fonte, c241_ano, c241_instit);';
         $this->execute($sql);
