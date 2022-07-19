e7aa2f3ebc4993b960b800ae8e4ac3322191bd79
rodrigocabral <marcos.rodrigo.cabral@hp.com>
2022-07-19 07:43:17 -0300

    Refactory remove if else sem sentido

--- cai3_gerfinanc555.php
+++ cai3_gerfinanc555.php
@@ -1,37 +1,13 @@
-<?
-/*
- *     E-cidade Software Publico para Gestao Municipal                
- *  Copyright (C) 2009  DBselller Servicos de Informatica             
- *                            www.dbseller.com.br                     
- *                         e-cidade@dbseller.com.br                   
- *                                                                    
- *  Este programa e software livre; voce pode redistribui-lo e/ou     
- *  modifica-lo sob os termos da Licenca Publica Geral GNU, conforme  
- *  publicada pela Free Software Foundation; tanto a versao 2 da      
- *  Licenca como (a seu criterio) qualquer versao mais nova.          
- *                                                                    
- *  Este programa e distribuido na expectativa de ser util, mas SEM   
- *  QUALQUER GARANTIA; sem mesmo a garantia implicita de              
- *  COMERCIALIZACAO ou de ADEQUACAO A QUALQUER PROPOSITO EM           
- *  PARTICULAR. Consulte a Licenca Publica Geral GNU para obter mais  
- *  detalhes.                                                         
- *                                                                    
- *  Voce deve ter recebido uma copia da Licenca Publica Geral GNU     
- *  junto com este programa; se nao, escreva para a Free Software     
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA          
- *  02111-1307, USA.                                                  
- *  
- *  Copia da licenca no diretorio licenca/licenca_en.txt 
- *                                licenca/licenca_pt.txt 
- */
+<?php
 
 require("libs/db_stdlib.php");
 require("libs/db_conecta.php");
 include("libs/db_sessoes.php");
 include("libs/db_sql.php");
+require_once "model/configuracao/Instituicao.model.php";
 parse_str($HTTP_SERVER_VARS['QUERY_STRING']);
 ?>
-<html>
+<html lang="">
 <head>
 <title>Descritivo do Parcelamento</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
@@ -57,56 +33,48 @@ th {
 </head>
 
 <body bgcolor=#CCCCCC leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<?php
+$oInstituicao = new Instituicao(db_getsession('DB_instit'));
 
+if ($oInstituicao->getUsaDebitosItbi() === true) {
+    $it01_guia = ", itbinumpre.it15_guia";
+    $left_join_itbi = " inner join itbinumpre on itbinumpre.it15_numpre = a.k00_numpre and it15_ultimaguia is true";
 
+}
 
-    <?
-   $config = db_query("select * from db_config where codigo = ".db_getsession("DB_instit"));
-   db_fieldsmemory($config, 0);
-
-   if ($db21_usadebitoitbi == 't') {
-    $it01_guia = ", arrecad_itbi.it01_guia";
-    $left_join_itbi = " left join arrecad_itbi on arrecad_itbi.k00_numpre = a.k00_numpre ";
-
-   }
-   if(1==1){
-
-    $sql = "select a.k00_numpre,
-                    k00_numpar,
-                    k00_numtot,
-                    k00_dtoper,
-                    k00_dtvenc,
-                    k00_receit,
-                    k02_drecei,
-                    k00_valor,
-                    k00_hist,
-                    k01_descr
-                    {$it01_guia}
-            from arrecad a
-                     inner join arreinstit on arreinstit.k00_numpre = a.k00_numpre
-                                          and arreinstit.k00_instit = ".db_getsession('DB_instit')."
-                 left outer join arrematric on arrematric.k00_numpre = a.k00_numpre
-                 left outer join arreinscr on arreinscr.k00_numpre = a.k00_numpre
-                 {$left_join_itbi}
-                 ,tabrec inner join tabrecjm on tabrecjm.k02_codjm = tabrec.k02_codjm
-             ,histcalc
-            where a.k00_numpre = ".$numpre." and
-                  k02_codigo   = k00_receit and
-                  k01_codigo   = k00_hist
-            ";
-    if($numpar != 0){
-       $sql .= " and k00_numpar = $numpar";
-    }
-    $js_func = "";
-    db_lovrot($sql,5,"()","",$js_func);
- }else{
- ?>
-
-
+$sql = "select a.k00_numpre,
+                k00_numpar,
+                k00_numtot,
+                k00_dtoper,
+                k00_dtvenc,
+                k00_receit,
+                k02_drecei,
+                k00_valor,
+                k00_hist,
+                k01_descr
+                {$it01_guia}
+        from arrecad a
+                 inner join arreinstit on arreinstit.k00_numpre = a.k00_numpre
+                                      and arreinstit.k00_instit = " . db_getsession('DB_instit') . "
+             left outer join arrematric on arrematric.k00_numpre = a.k00_numpre
+             left outer join arreinscr on arreinscr.k00_numpre = a.k00_numpre
+             {$left_join_itbi}
+             ,tabrec inner join tabrecjm on tabrecjm.k02_codjm = tabrec.k02_codjm
+         ,histcalc
+        where a.k00_numpre = " . $numpre . " and
+              k02_codigo   = k00_receit and
+              k01_codigo   = k00_hist
+        ";
+if ($numpar != 0) {
+    $sql .= " and k00_numpar = $numpar";
+}
+$js_func = "";
+db_lovrot($sql, 5, "()", "", $js_func);
+?>
 
   <table width="100%" border="0" cellspacing="0" cellpadding="3">
    <tr bgcolor="#FFCC66">
-      <?php if ($db21_usadebitoitbi == 't') : ?>
+      <?php if ($oInstituicao->getUsaDebitosItbi() === true) : ?>
       <th width="5%" nowrap>ITBI</th>
       <th width="5%" nowrap>Numpre</th>
       <?php else : ?>
@@ -122,7 +90,7 @@ th {
       <th width="17%" nowrap>Descri&ccedil;&atilde;o</th>
       <th width="15%" nowrap>Valor</th>
     </tr>
-    <?
+    <?php
     $sql = "select a.k00_numpre,
                     k00_numpar,
                     k00_numtot,
@@ -171,7 +139,7 @@ th {
       }
       ?>
        <tr bgcolor="<?=$qcor?>">
-        <?php if ($db21_usadebitoitbi == 't') : ?>
+        <?php if ($oInstituicao->getUsaDebitosItbi() === true) : ?>
          <td width="5%" nowrap align="right" > <?=$it01_guia?></td>
          <td width="5%" nowrap align="right" > <?=$k00_numpre?></td>
         <?php else : ?>
@@ -187,13 +155,10 @@ th {
          <td width="17%" nowrap><?=$k02_drecei?></td>
          <td width="15%" nowrap align="right"> <?=db_formatar(db_formatar($k00_valor,"v")*-1,"f")?> </td>
       </tr>
-    <?
+    <?php
       }
     }
     ?>
   </table>
- <?
- }
- ?>
 </body>
 </html>
