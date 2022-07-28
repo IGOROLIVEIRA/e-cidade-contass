60d518963d0623e51319a98d50b3a5e65ab6f5ec
Igor Afonso Oliveira Ruas <igorruas@Igors-MacBook-Pro.local>
2022-07-28 13:57:51 -0300

    corrido impartacao da receita e despesas do orcamento para o ano de 2023

--- orc4_duplicaorcamentorec001.php
+++ orc4_duplicaorcamentorec001.php
@@ -1,28 +1,28 @@
 <?
 /*
- *     E-cidade Software Publico para Gestao Municipal                
- *  Copyright (C) 2009  DBselller Servicos de Informatica             
- *                            www.dbseller.com.br                     
- *                         e-cidade@dbseller.com.br                   
- *                                                                    
- *  Este programa e software livre; voce pode redistribui-lo e/ou     
- *  modifica-lo sob os termos da Licenca Publica Geral GNU, conforme  
- *  publicada pela Free Software Foundation; tanto a versao 2 da      
- *  Licenca como (a seu criterio) qualquer versao mais nova.          
- *                                                                    
- *  Este programa e distribuido na expectativa de ser util, mas SEM   
- *  QUALQUER GARANTIA; sem mesmo a garantia implicita de              
- *  COMERCIALIZACAO ou de ADEQUACAO A QUALQUER PROPOSITO EM           
- *  PARTICULAR. Consulte a Licenca Publica Geral GNU para obter mais  
- *  detalhes.                                                         
- *                                                                    
- *  Voce deve ter recebido uma copia da Licenca Publica Geral GNU     
- *  junto com este programa; se nao, escreva para a Free Software     
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA          
- *  02111-1307, USA.                                                  
- *  
- *  Copia da licenca no diretorio licenca/licenca_en.txt 
- *                                licenca/licenca_pt.txt 
+ *     E-cidade Software Publico para Gestao Municipal
+ *  Copyright (C) 2009  DBselller Servicos de Informatica
+ *                            www.dbseller.com.br
+ *                         e-cidade@dbseller.com.br
+ *
+ *  Este programa e software livre; voce pode redistribui-lo e/ou
+ *  modifica-lo sob os termos da Licenca Publica Geral GNU, conforme
+ *  publicada pela Free Software Foundation; tanto a versao 2 da
+ *  Licenca como (a seu criterio) qualquer versao mais nova.
+ *
+ *  Este programa e distribuido na expectativa de ser util, mas SEM
+ *  QUALQUER GARANTIA; sem mesmo a garantia implicita de
+ *  COMERCIALIZACAO ou de ADEQUACAO A QUALQUER PROPOSITO EM
+ *  PARTICULAR. Consulte a Licenca Publica Geral GNU para obter mais
+ *  detalhes.
+ *
+ *  Voce deve ter recebido uma copia da Licenca Publica Geral GNU
+ *  junto com este programa; se nao, escreva para a Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307, USA.
+ *
+ *  Copia da licenca no diretorio licenca/licenca_en.txt
+ *                                licenca/licenca_pt.txt
  */
 
 
@@ -37,6 +37,7 @@ require_once ("dbforms/db_funcoes.php");
 include ("dbforms/db_classesgenericas.php");
 
 include ("classes/db_orcfontes_classe.php");
+include ("classes/db_conplanoorcamentoanalitica_classe.php");
 include ("classes/db_orcfontesdes_classe.php");
 include ("classes/db_orcreceita_classe.php");
 
@@ -45,6 +46,7 @@ $cliframe_seleciona = new cl_iframe_seleciona;
 $clorcfontes = new cl_orcfontes;
 $clorcfontesdes = new cl_orcfontesdes;
 $clorcreceita = new cl_orcreceita;
+$clconplanoorcamentoanalitica = new cl_conplanoorcamentoanalitica;
 
 db_postmemory($HTTP_POST_VARS);
 $debug = false;
@@ -52,6 +54,7 @@ $debug = false;
 $anousu = db_getsession("DB_anousu");
 $dt_ini = $anousu.'-01-01';
 $dt_fin = $anousu.'-12-31';
+$aFontesNovas = array('100' => 15000000,'101' => 15000001,'102' => 15000002,'103' => 18000000,'104' => 18010000,'105' => 18020000,'106' => 15760010,'107' => 15440000,'108' => 17080000,'112' => 16590020,'113' => 15990030,'116' => 17500000,'117' => 17510000,'118' => 15400007,'119' => 15400003,'120' => 15760000,'121' => 16220000,'122' => 15700000,'123' => 16310000,'124' => 17000000,'129' => 16600000,'130' => 18990040,'131' => 17590050,'132' => 16040000,'142' => 16650000,'143' => 15510000,'144' => 15520000,'145' => 15530000,'146' => 15690000,'147' => 15500000,'153' => 16010000,'154' => 16590000,'155' => 16210000,'156' => 16610000,'157' => 17520000,'158' => 18990060,'159' => 16000000,'160' => 17040000,'161' => 17070000,'162' => 17490120,'163' => 17130070,'164' => 17060000,'165' => 18990000,'166' => 15420007,'167' => 15420003,'168' => 17100100,'169' => 17100000,'170' => 15010000,'171' => 15710000,'172' => 15720000,'173' => 15750000,'174' => 15740000,'175' => 15730000,'176' => 16320000,'177' => 16330000,'178' => 16360000,'179' => 16340000,'180' => 16350000,'181' => 17010000,'182' => 17020000,'183' => 17030000,'184' => 17090000,'185' => 17530000,'186' => 17040000,'187' => 17050000,'188' => 15000080,'189' => 15000090,'190' => 17540000,'191' => 17540000,'192' => 17550000,'193' => 18990130);
 
 $anousu_ant = (db_getsession("DB_anousu") - 1);
 $dt_ini_ant = $anousu_ant.'-01-01';
@@ -72,7 +75,7 @@ if (isset ($processa_fontes) && ($processa_fontes == 'Processar')) {
 		for ($i = 0; $i < count($chaves); $i ++) {
 			if ($chaves[$i] == "")
 				continue;
-			// seleciona orgaos e insere no exercicio alvo	
+			// seleciona orgaos e insere no exercicio alvo
 			$res = $clorcfontes->sql_record($clorcfontes->sql_query_file($chaves[$i],$anousu_ant ));
 			if (($clorcfontes->numrows) > 0) {
 				db_fieldsmemory($res, 0);
@@ -80,7 +83,7 @@ if (isset ($processa_fontes) && ($processa_fontes == 'Processar')) {
 				$clorcfontes->o57_codfon  = $o57_codfon;
 				$clorcfontes->o57_fonte    = $o57_fonte;
 				$clorcfontes->o57_descr  = $o57_descr;
-				$clorcfontes->o57_finali    = $o57_finali;				
+				$clorcfontes->o57_finali    = $o57_finali;
 				$clorcfontes->incluir($o57_codfon,$anousu);
 				if ($clorcfontes->erro_status == '0') {
 					db_msgbox($clorcfontes->erro_msg);
@@ -105,7 +108,7 @@ if (isset ($processa_fontesdes) && ($processa_fontesdes == 'Processar')) {
 				db_fieldsmemory($res, 0);
 				$clorcfontesdes->o60_anousu = $anousu;
 				$clorcfontesdes->o60_codfon  = $o60_codfon;
-				$clorcfontesdes->o60_perc      = $o60_perc;				
+				$clorcfontesdes->o60_perc      = $o60_perc;
 				$clorcfontesdes->incluir($anousu,$o60_codfon);
 				if ($clorcfontesdes->erro_status == '0') {
 					db_msgbox($clorcfontesdes->erro_msg);
@@ -113,7 +116,7 @@ if (isset ($processa_fontesdes) && ($processa_fontesdes == 'Processar')) {
 					break;
 				}
 			}
-		} // 
+		} //
 		db_fim_transacao($erro);
 	} //
 } //
@@ -125,15 +128,20 @@ if (isset ($processa_receitas) && ($processa_receitas == 'Processar')) {
 		for ($i = 0; $i < count($chaves); $i ++) {
 			if ($chaves[$i] == "")
 				continue;
-			$res = $clorcreceita->sql_record($clorcreceita->sql_query_file($anousu_ant, $chaves[$i], "*",null,""));
-
+            $res = $clorcreceita->sql_record($clorcreceita->sql_query_plano($anousu_ant, $chaves[$i], "*",null,""));
 			if (($clorcreceita->numrows) > 0) {
 
 				db_fieldsmemory($res, 0);
+
 				$clorcreceita->o70_anousu         = $anousu;
 				$clorcreceita->o70_codrec         = $o70_codrec;
 				$clorcreceita->o70_codfon         = $o70_codfon;
 				$clorcreceita->o70_codigo         = $o70_codigo;
+                if($anousu == 2023){
+                    $resEstrutNovo = $clconplanoorcamentoanalitica->sql_record($clconplanoorcamentoanalitica->sql_query(null, null, "*", null, " c60_anousu = ".$anousu." and c60_estrut= '".$o57_fonte."'"));
+                    $clorcreceita->o70_codfon         = $c61_codcon;
+				    $clorcreceita->o70_codigo         = $c61_codigo;
+                }
 				$clorcreceita->o70_valor          = $o70_valor;
 				$clorcreceita->o70_reclan         = "$o70_reclan";
 
@@ -147,7 +155,7 @@ if (isset ($processa_receitas) && ($processa_receitas == 'Processar')) {
 					break;
 				}
 			}
-		} // 
+		} //
 		db_fim_transacao($erro);
 	} //
     if ($clorcreceita->erro_status != '0' && $erro != true) {
@@ -191,7 +199,7 @@ if (isset ($processa_receitas) && ($processa_receitas == 'Excluir')) {
 </head>
 <body bgcolor=#CCCCCC leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" onLoad="a=1" >
 <table width="790" border="0" cellpadding="0" cellspacing="0" bgcolor="#5786B2">
-  <tr> 
+  <tr>
     <td width="360" height="18">&nbsp;</td>
     <td width="263">&nbsp;</td>
     <td width="25">&nbsp;</td>
@@ -210,7 +218,7 @@ if (isset ($processa_receitas) && ($processa_receitas == 'Excluir')) {
      <table border=0 align=center>
      <tr>
        <td colspan=3><h3>Duplicação de Receitas ( do exerício <?=(db_getsession("DB_anousu")-1)?> para <?=db_getsession("DB_anousu")?> ) </h3></td>
-     </tr>    
+     </tr>
 <!--     <tr>-->
 <!--       <td colspan=3><b>Cadastros </b> </td>-->
 <!--     </tr>-->
@@ -224,7 +232,7 @@ if (isset ($processa_receitas) && ($processa_receitas == 'Excluir')) {
 <!--       <td>Desdobramentos </td>-->
 <!--       <td><input type='submit' name='processa_fontesdes' value='Selecionar'   ></td>-->
 <!--     </tr>-->
-    
+
      <tr>
        <td colspan=3><b>Receitas</b> </td>
      </tr>
@@ -232,13 +240,13 @@ if (isset ($processa_receitas) && ($processa_receitas == 'Excluir')) {
        <td width=40px> &nbsp; </td>
        <td>Receitas </td>
        <td><input type='submit' name='processa_receitas' value='Selecionar' ></td>
-     </tr>   
+     </tr>
     </table>
    </td>
-  </tr>  
-  </table> 
-  
-   
+  </tr>
+  </table>
+
+
 </td>
 <td height=100% width=50% valign=top align=center >
 <?
@@ -248,11 +256,11 @@ $size_iframe = 400;
 if (isset ($processa_fontes) && $processa_fontes == "Selecionar") {
 	$sql = "select o57_codfon,o57_fonte,o57_descr
 	        	   from orcfontes
-		           where o57_anousu=". (db_getsession("DB_anousu") - 1)."                       
+		           where o57_anousu=". (db_getsession("DB_anousu") - 1)."
 		           EXCEPT
 		           select o57_codfon,o57_fonte,o57_descr
 		           from orcfontes
-		           where o57_anousu=".db_getsession("DB_anousu")."                       
+		           where o57_anousu=".db_getsession("DB_anousu")."
 		           order by o57_fonte
 	          ";
 	$sql_marca = "";
@@ -267,20 +275,20 @@ if (isset ($processa_fontes) && $processa_fontes == "Selecionar") {
 	$cliframe_seleciona->iframe_seleciona(1);
 ?><table border=0 width=100%>
       <tr><td width=100% align=center><input type=button value=Processar onClick="js_processa('fontes');"></td></tr>
-      </table>               
-    <? 
+      </table>
+    <?
 
 }
 if (isset ($processa_fontesdes) && $processa_fontesdes == "Selecionar") {
 	$sql = "select o60_codfon,o57_fonte,o57_descr
 	               from orcfontesdes
 	           			 inner join orcfontes on o57_codfon=o60_codfon and o57_anousu=o60_anousu
-		           where o60_anousu=". (db_getsession("DB_anousu") - 1)."                       
+		           where o60_anousu=". (db_getsession("DB_anousu") - 1)."
 		           EXCEPT
 		           select o60_codfon,o57_fonte,o57_descr
 		           from orcfontesdes
 						 inner join orcfontes on o57_codfon=o60_codfon and o57_anousu=o60_anousu
-		           where o60_anousu=".db_getsession("DB_anousu")."                       
+		           where o60_anousu=".db_getsession("DB_anousu")."
 		           order by o57_fonte
 	          ";
 	$sql_marca = "";
@@ -295,20 +303,20 @@ if (isset ($processa_fontesdes) && $processa_fontesdes == "Selecionar") {
 	$cliframe_seleciona->iframe_seleciona(1);
 ?><table border=0 width=100%>
           <tr><td width=100% align=center><input type=button value=Processar onClick="js_processa('fontesdes');"></td></tr>
-          </table>               
-       <? 
+          </table>
+       <?
 
 }
 if (isset ($processa_receitas) && $processa_receitas == "Selecionar") {
 	$sql = "select o70_codrec,o57_fonte,o70_valor,o70_instit
 	            from orcreceita
 					 inner join orcfontes on o57_anousu=o70_anousu and o70_codfon=o57_codfon
-		        where o70_anousu=". (db_getsession("DB_anousu") - 1)."                       
+		        where o70_anousu=". (db_getsession("DB_anousu") - 1)."
 		        EXCEPT
 		        select  o70_codrec,o57_fonte,o70_valor,o70_instit
 		        from orcreceita
 					inner join orcfontes on o57_anousu=o70_anousu and o70_codfon=o57_codfon
-		        where o70_anousu=".db_getsession("DB_anousu")."                       
+		        where o70_anousu=".db_getsession("DB_anousu")."
 		        order by o57_fonte
 	          ";
 	$sql_marca = "";
@@ -326,8 +334,8 @@ if (isset ($processa_receitas) && $processa_receitas == "Selecionar") {
               <td width=50% align=right><input type=button value=Processar onClick="js_processa('Processar');"></td>
               <td width=50% align=left><input type=button value=Excluir onClick="js_processa('Excluir');">
               </td></tr>
-          </table>               
-       <? 
+          </table>
+       <?
 
 }
 ?>
