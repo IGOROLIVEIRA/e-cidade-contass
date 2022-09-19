bd49f95de6fd0ebd1c37fafa2cc8f2fca0788b66
Juliana <jccctbsi@gmail.com>
2022-09-19 11:21:12 -0300

    carne Erro na emissao do Carne na CGF

--- cai3_gerfinanc033.php
+++ cai3_gerfinanc033.php
@@ -25,14 +25,14 @@
  *                                licenca/licenca_pt.txt
  */
 
-require_once("fpdf151/scpdf.php");
-require_once("fpdf151/impcarne.php");
-require_once("libs/db_sql.php");
-require_once("libs/db_utils.php");
-require_once("libs/db_stdlib.php");
-require_once("libs/db_app.utils.php");
-require_once("libs/db_libtributario.php");
-require_once("dbforms/db_funcoes.php");
+require_once(modification("fpdf151/scpdf.php"));
+require_once(modification("fpdf151/impcarne.php"));
+require_once(modification("libs/db_sql.php"));
+require_once(modification("libs/db_utils.php"));
+require_once(modification("libs/db_stdlib.php"));
+require_once(modification("libs/db_app.utils.php"));
+require_once(modification("libs/db_libtributario.php"));
+require_once(modification("dbforms/db_funcoes.php"));
 
 
 db_app::import("exceptions.*");
