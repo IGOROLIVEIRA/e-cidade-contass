a188ae2f190957ae3accbe7255415a832c475f37
Filipi <filipibarbosa.contass@gmail.com>
2022-09-20 09:07:35 -0300

    OC18302 correcao migration

--- db/migrations/20220920113427_oc18302.php
+++ db/migrations/20220920113427_oc18302.php
@@ -7,7 +7,16 @@ class Oc18302 extends AbstractMigration
 
     public function up()
     {
-        $sql = "alter table atolegal add ed05_i_aparecerelatorio bool default false; ";
+        $sql = "
+        select fc_startsession();
+
+        begin;
+        
+        alter table atolegal add ed05_i_aparecerelatorio bool default false; 
+        
+        commit;
+        
+        ";
         $this->execute($sql);
     }
 }
