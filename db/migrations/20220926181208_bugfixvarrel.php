fed209298174518fb9236ea90a040126ff3cf206
Guilherme Souza <guilhermesouza.contass@gmail.com>
2022-09-26 18:23:22 +0000

    Update 20220926181208_bugfixvarrel.php

--- db/migrations/20220926181208_bugfixvarrel.php
+++ db/migrations/20220926181208_bugfixvarrel.php
@@ -11,7 +11,7 @@ class Bugfixvarrel extends AbstractMigration
         $sql="
         BEGIN;
 
-        INSERT INTO db_syscampo VALUES ((select max(codcam)+1 from db_syscampo), 'dCriacao','date' ,'Data de criação mês textual','', 'Data de criação mês textual',16	,false, false, false, 0, 'date', 'Data de criação mês textual');
+        INSERT INTO db_syscampo VALUES ((select max(codcam)+1 from db_syscampo), 'dCriacao','date' ,'Data de criaÃ§Ã£o mÃªs textual','', 'Data de criaÃ§Ã£o mÃªs textual',16	,false, false, false, 0, 'date', 'Data de criaÃ§Ã£o mÃªs textual');
         INSERT INTO db_sysarqcamp (codarq, codcam, seqarq, codsequencia) VALUES (1260, (select codcam from db_syscampo where nomecam = 'dCriacao'), (select max(seqarq)+1 from db_sysarqcamp where codarq = 1260), 0);
         
         COMMIT;
