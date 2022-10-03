e373d2cf1307a0fd4fbba82f55c200c247d3c151
Filipi <filipibarbosa.contass@gmail.com>
2022-10-03 08:45:00 -0300

    OC18552 inclusao dos campos na tabela db_syscampo

--- db/migrations/20220929023613_oc18552.php
+++ db/migrations/20220929023613_oc18552.php
@@ -4,11 +4,17 @@ use Phinx\Migration\AbstractMigration;
 
 class Oc18552 extends AbstractMigration
 {
-   
+
     public function up()
     {
-        $sql="
+        $sql = "
         BEGIN;
+
+        INSERT INTO db_syscampo 
+        VALUES ((select max(codcam)+1 from db_syscampo),'l20_dataaberproposta', 'date', 'Data Abertura Proposta','','Data Abertura Proposta',10,false,false,false,0,'date','Data Abertura Proposta');
+
+        INSERT INTO db_syscampo 
+        VALUES ((select max(codcam)+1 from db_syscampo),'l20_dataencproposta', 'date', 'Data de Encerramento Proposta','','Data de Encerramento Proposta',10,false,false,false,0,'date','Data de Encerramento Proposta');
 
         ALTER TABLE liclicita ADD l20_dataaberproposta date null;
 
