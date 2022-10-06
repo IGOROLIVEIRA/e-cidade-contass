77dadf3831d41760fdaee2affd43ed923a36ff36
Filipi <filipibarbosa.contass@gmail.com>
2022-10-06 14:18:02 -0300

    OC18553  criacao migration e criacao da tabela amparo legal

--- db/migrations/20221006125609_oc18553.php
+++ db/migrations/20221006125609_oc18553.php
@@ -12,17 +12,17 @@ class Oc18553 extends AbstractMigration
         BEGIN;
 
         CREATE SEQUENCE amparolegal_l212_codigo_seq
-INCREMENT 1
-MINVALUE 1
-MAXVALUE 9223372036854775807
-START 1
-CACHE 1;
+        INCREMENT 1
+        MINVALUE 1
+        MAXVALUE 9223372036854775807
+        START 1
+        CACHE 1;
 
-create table amparolegal(
+        create table amparolegal(
 
- l212_codigo int not null default 0,
- l212_lei varchar (100) not null ,
- CONSTRAINT amparolegal_sequ_pk PRIMARY KEY (l212_codigo));
+        l212_codigo int not null default 0,
+        l212_lei varchar (100) not null ,
+        CONSTRAINT amparolegal_sequ_pk PRIMARY KEY (l212_codigo));
 
         COMMIT;
 
