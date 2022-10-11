06daff502168852e1fd7c542152a4d609cff161f
marcelo <marcelo@contassconsultoria.com.br>
2022-10-11 06:57:43 -0300

    #NOTASK | ajustando consulta do evento 1200

--- src/RecursosHumanos/ESocial/Model/Formulario/Preenchimentos.php
+++ src/RecursosHumanos/ESocial/Model/Formulario/Preenchimentos.php
@@ -771,9 +771,13 @@ WHERE rh30_vinculo IN ('I',
                                             and r33_mesusu = date_part('month',fc_getsession('DB_datausu')::date)
                                             and r33_instit = fc_getsession('DB_instit')::int
                                     ) as x on r33_codtab = rhpessoalmov.rh02_tbprev+2
-                where h13_categoria in ('101', '106', '111', '301', '302', '303', '305', '306', '309', '312', '313', '902','701','712','771','901','711','410')
-                and rh30_vinculo = 'A'
-                and r33_tiporegime = '1'
+                where (
+                    (h13_categoria = '901' and rh30_vinculo = 'A')
+                    or
+                    (h13_categoria in ('101', '106', '111', '301', '302', '303', '305', '306', '309', '312', '313', '902','701','712','771','711')
+                    and rh30_vinculo = 'A'
+                    and r33_tiporegime = '1')
+                )
                 and ((rh05_recis is not null
                 and date_part('month', rh05_recis) = date_part('month', fc_getsession('DB_datausu')::date)
                 and date_part('year', rh05_recis) = date_part('year', fc_getsession('DB_datausu')::date)
