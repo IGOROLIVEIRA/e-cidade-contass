e863200b989130877938ab9614b7c2a38c4be40f
Filipi <filipibarbosa.contass@gmail.com>
2022-11-11 17:27:09 -0300

    OC18991 inclusao do campo código anterior na listagem das pesquisas dos veiculos

--- func_veiculosconsulta.php
+++ func_veiculosconsulta.php
@@ -48,139 +48,142 @@ $iInstituicao = db_getsession("DB_instit");
 
 ?>
 <html>
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<link href="estilos.css" rel="stylesheet" type="text/css">
-<script language="JavaScript" type="text/javascript" src="scripts/scripts.js"></script>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+  <link href="estilos.css" rel="stylesheet" type="text/css">
+  <script language="JavaScript" type="text/javascript" src="scripts/scripts.js"></script>
 </head>
+
 <body bgcolor=#CCCCCC leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
-<table height="100%" border="0"  align="center" cellspacing="0" bgcolor="#CCCCCC">
-  <tr>
-    <td height="63" align="center" valign="top">
+  <table height="100%" border="0" align="center" cellspacing="0" bgcolor="#CCCCCC">
+    <tr>
+      <td height="63" align="center" valign="top">
         <table width="35%" border="0" align="center" cellspacing="0">
-	     <form name="form1" method="post" action="" >
-          <tr>
-            <td width="4%" align="right" nowrap title="<?=$Tve01_codigo?>">
-              <?=$Lve01_codigo?>
-            </td>
-            <td width="96%" align="left" nowrap>
-              <?
-		       db_input("ve01_codigo",10,$Ive01_codigo,true,"text",4,"","chave_ve01_codigo");
-		       ?>
-            </td>
-          </tr>
-          <tr>
-            <td width="4%" align="right" nowrap title="<?=$Tve01_placa?>">
-              <?=$Lve01_placa?>
-            </td>
-            <td width="96%" align="left" nowrap>
-              <?
-           db_input("ve01_placa",10,$Ive01_placa,true,"text",4,"","chave_ve01_placa");
-           ?>
-            </td>
-          </tr>
-          <tr>
-            <td nowrap align="right" title="<?=$Tve37_veiccadcentral?>"><?=$Lve37_veiccadcentral?></td>
-            <td nowrap align="left">
-            <?
-               $res_veiccadcentral = $clveiccadcentral->sql_record($clveiccadcentral->sql_query(null,"ve36_sequencial,descrdepto",null,""));
-               db_selectrecord("veiccadcentral",$res_veiccadcentral,true,4,"","chave_veiccadcentral","","0");
-            ?>
-            </td>
-          </tr>
-         <tr>
-           <td align="right">
-             <strong>Seleção por:</strong>
-           </td>
-           <td>
-             <?
-             $x = array("0"=>"Selecione","1"=>"Ativos","2"=>"Baixados");
-             db_select('chave_baixa',$x,true,2,"","chave_baixa","");
-             ?>
-           </td>
-         </tr>
-          <tr>
-            <td colspan="2" align="center">
-              <input name="pesquisar" type="submit" id="pesquisar2" value="Pesquisar">
-              <input name="limpar" type="reset" id="limpar" value="Limpar" >
-              <input name="Fechar" type="button" id="fechar" value="Fechar" onClick="parent.db_iframe_veiculos.hide();">
-             </td>
-          </tr>
-        </form>
+          <form name="form1" method="post" action="">
+            <tr>
+              <td width="4%" align="right" nowrap title="<?= $Tve01_codigo ?>">
+                <?= $Lve01_codigo ?>
+              </td>
+              <td width="96%" align="left" nowrap>
+                <?
+                db_input("ve01_codigo", 10, $Ive01_codigo, true, "text", 4, "", "chave_ve01_codigo");
+                ?>
+              </td>
+            </tr>
+            <tr>
+              <td width="4%" align="right" nowrap title="<?= $Tve01_placa ?>">
+                <?= $Lve01_placa ?>
+              </td>
+              <td width="96%" align="left" nowrap>
+                <?
+                db_input("ve01_placa", 10, $Ive01_placa, true, "text", 4, "", "chave_ve01_placa");
+                ?>
+              </td>
+            </tr>
+            <tr>
+              <td nowrap align="right" title="<?= $Tve37_veiccadcentral ?>"><?= $Lve37_veiccadcentral ?></td>
+              <td nowrap align="left">
+                <?
+                $res_veiccadcentral = $clveiccadcentral->sql_record($clveiccadcentral->sql_query(null, "ve36_sequencial,descrdepto", null, ""));
+                db_selectrecord("veiccadcentral", $res_veiccadcentral, true, 4, "", "chave_veiccadcentral", "", "0");
+                ?>
+              </td>
+            </tr>
+            <tr>
+              <td align="right">
+                <strong>Seleção por:</strong>
+              </td>
+              <td>
+                <?
+                $x = array("0" => "Selecione", "1" => "Ativos", "2" => "Baixados");
+                db_select('chave_baixa', $x, true, 2, "", "chave_baixa", "");
+                ?>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2" align="center">
+                <input name="pesquisar" type="submit" id="pesquisar2" value="Pesquisar">
+                <input name="limpar" type="reset" id="limpar" value="Limpar">
+                <input name="Fechar" type="button" id="fechar" value="Fechar" onClick="parent.db_iframe_veiculos.hide();">
+              </td>
+            </tr>
+          </form>
         </table>
       </td>
-  </tr>
-  <tr>
-    <td align="center" valign="top">
-      <?
-      $where = "";
+    </tr>
+    <tr>
+      <td align="center" valign="top">
+        <?
+        $where = "";
 
-      if (isset($chave_veiccadcentral) && trim($chave_veiccadcentral) != "" && $chave_veiccadcentral != "0"){
-        $where .= "and  ve37_veiccadcentral = $chave_veiccadcentral ";
-      } else {
-        $where .="";
-      }
-
-      if(isset($chave_baixa) && trim($chave_baixa) != "" && $chave_baixa != "0"){
-        if ($chave_baixa == "1"){
-          $where = "and ve01_ativo = '1' and not exists (select ve04_codigo from veicbaixa where ve04_veiculo = ve01_codigo and ve04_veiccadtipobaixa not in (1,2,3,4,5,7,99))";
-        }else{
-          $where = "and exists (select ve04_codigo from veicbaixa where ve04_veiculo = ve01_codigo )";
-        }
-      }
-
-      if(!isset($pesquisa_chave)){
-
-        if(isset($campos)==false){
-
-           $campos  = "distinct ve01_codigo,ve01_placa,upper(si05_descricao) as si05_descricao,ve21_descr,";
-           $campos .= "ve22_descr,ve23_descr,ve01_chassi,ve01_certif,ve01_anofab,ve01_anomod";
-        }
-
-        if (isset($chave_ve01_codigo) && (trim($chave_ve01_codigo)!="") ) {
-	         $sql = $clveiculos->sql_query($chave_ve01_codigo,$campos,"ve01_codigo","ve01_codigo=$chave_ve01_codigo and instit = {$iInstituicao} ");
-        } else if( isset($chave_ve01_placa) && (trim($chave_ve01_placa)!="") ) {
-           $sql = $clveiculos->sql_query("",$campos,"ve01_placa"," trim(ve01_placa) like '$chave_ve01_placa%' and instit = {$iInstituicao} ");
+        if (isset($chave_veiccadcentral) && trim($chave_veiccadcentral) != "" && $chave_veiccadcentral != "0") {
+          $where .= "and  ve37_veiccadcentral = $chave_veiccadcentral ";
         } else {
-           $sql = $clveiculos->sql_query("",$campos,"ve01_codigo","instit = {$iInstituicao} $where");
+          $where .= "";
         }
 
-        $repassa = array();
-        if(isset($chave_ve01_codigo)){
-          $repassa = array("chave_ve01_codigo" => $chave_ve01_codigo, "chave_ve01_codigo"=>$chave_ve01_codigo);
+        if (isset($chave_baixa) && trim($chave_baixa) != "" && $chave_baixa != "0") {
+          if ($chave_baixa == "1") {
+            $where = "and ve01_ativo = '1' and not exists (select ve04_codigo from veicbaixa where ve04_veiculo = ve01_codigo and ve04_veiccadtipobaixa not in (1,2,3,4,5,7,99))";
+          } else {
+            $where = "and exists (select ve04_codigo from veicbaixa where ve04_veiculo = ve01_codigo )";
+          }
         }
 
-        db_lovrot($sql,15,"()","",$funcao_js,"","NoMe",$repassa,false);
-      }else{
-        if($pesquisa_chave!=null && $pesquisa_chave!=""){
-          if ($where!=""){
-           	$where = " and ".$where;
+        if (!isset($pesquisa_chave)) {
+
+          if (isset($campos) == false) {
+
+            $campos  = "distinct ve01_codigo,ve01_placa,upper(si05_descricao) as si05_descricao,ve21_descr,";
+            $campos .= "ve22_descr,ve23_descr,ve01_chassi,ve01_certif,ve01_anofab,ve01_anomod,ve01_codigoant";
           }
-          $result = $clveiculos->sql_record($clveiculos->sql_query($pesquisa_chave,"*",null,"ve01_codigo=$pesquisa_chave $where and instit = {$iInstituicao} "));
-          if($clveiculos->numrows!=0){
-            db_fieldsmemory($result,0);
-            echo "<script>".$funcao_js."('$ve01_placa',false);</script>";
-          }else{
-	         echo "<script>".$funcao_js."('Chave(".$pesquisa_chave.") não Encontrado',true);</script>";
+
+          if (isset($chave_ve01_codigo) && (trim($chave_ve01_codigo) != "")) {
+            $sql = $clveiculos->sql_query($chave_ve01_codigo, $campos, "ve01_codigo", "ve01_codigo=$chave_ve01_codigo and instit = {$iInstituicao} ");
+          } else if (isset($chave_ve01_placa) && (trim($chave_ve01_placa) != "")) {
+            $sql = $clveiculos->sql_query("", $campos, "ve01_placa", " trim(ve01_placa) like '$chave_ve01_placa%' and instit = {$iInstituicao} ");
+          } else {
+            $sql = $clveiculos->sql_query("", $campos, "ve01_codigo", "instit = {$iInstituicao} $where");
           }
-        }else{
-	       echo "<script>".$funcao_js."('',false);</script>";
+
+          $repassa = array();
+          if (isset($chave_ve01_codigo)) {
+            $repassa = array("chave_ve01_codigo" => $chave_ve01_codigo, "chave_ve01_codigo" => $chave_ve01_codigo);
+          }
+
+          db_lovrot($sql, 15, "()", "", $funcao_js, "", "NoMe", $repassa, false);
+        } else {
+          if ($pesquisa_chave != null && $pesquisa_chave != "") {
+            if ($where != "") {
+              $where = " and " . $where;
+            }
+            $result = $clveiculos->sql_record($clveiculos->sql_query($pesquisa_chave, "*", null, "ve01_codigo=$pesquisa_chave $where and instit = {$iInstituicao} "));
+            if ($clveiculos->numrows != 0) {
+              db_fieldsmemory($result, 0);
+              echo "<script>" . $funcao_js . "('$ve01_placa',false);</script>";
+            } else {
+              echo "<script>" . $funcao_js . "('Chave(" . $pesquisa_chave . ") não Encontrado',true);</script>";
+            }
+          } else {
+            echo "<script>" . $funcao_js . "('',false);</script>";
+          }
         }
-      }
-      ?>
-     </td>
-   </tr>
-</table>
+        ?>
+      </td>
+    </tr>
+  </table>
 </body>
+
 </html>
 <?
-if(!isset($pesquisa_chave)){
-  ?>
+if (!isset($pesquisa_chave)) {
+?>
   <script>
   </script>
-  <?
+<?
 }
 ?>
 <script>
-js_tabulacaoforms("form2","chave_ve01_codigo",true,1,"chave_ve01_codigo",true);
+  js_tabulacaoforms("form2", "chave_ve01_codigo", true, 1, "chave_ve01_codigo", true);
 </script>
