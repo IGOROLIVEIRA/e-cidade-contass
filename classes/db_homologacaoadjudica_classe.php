5d99ae4cc4c57e3a3cd43a5f9fb5d1a86ba83a0a
mario rodrigues ruas junior <mario@contassconsultoria.com.br>
2023-01-11 11:58:06 +0000

    Merge branch 'OC19344' into 'master'
    Oc19344
    
    See merge request contass/ecidade!3255
    
    (cherry picked from commit 78bc51793441785f7eadddd82043db2d6e0dc8b0)
    
    caa7b34e OC19344 VERIFICACAO RELATORIO DE HOMOLOGACAO TABELA E TAXA
    fefb79a5 Merge branch 'master' of 10.250.30.15:contass/ecidade into OC19344

--- classes/db_homologacaoadjudica_classe.php
+++ classes/db_homologacaoadjudica_classe.php
@@ -667,6 +667,7 @@ class cl_homologacaoadjudica {
         $sql .= "      left  join empempaut              on empempaut.e61_autori                = empautitem.e55_autori ";
         $sql .= "      left  join empempenho             on empempenho.e60_numemp               = empempaut.e61_numemp ";
         $sql .= "      left join liclicitem              on liclicitem.l21_codpcprocitem        = pcprocitem.pc81_codprocitem";
+        $sql .= "      left join liclicita              on liclicita.l20_codigo                 = liclicitem.l21_codliclicita";
         $sql .= "      left join liclicitemlote         on liclicitemlote.l04_liclicitem        = liclicitem.l21_codigo";
         $sql .= "      left join pcorcamitemlic         on liclicitem.l21_codigo               = pcorcamitemlic.pc26_liclicitem";
         $sql .= "      left join pcorcamitem            on pcorcamitemlic.pc26_orcamitem       = pcorcamitem.pc22_orcamitem";
