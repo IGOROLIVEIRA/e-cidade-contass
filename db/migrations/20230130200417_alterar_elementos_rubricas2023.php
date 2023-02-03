773c1051448a18f831d3ddd7df53df0a85f705ec
Robson de Jesus <robson.dejesus@arctouch.com>
2023-02-03 16:01:08 -0300

    OCFolha - alteradndo elementos de lotações para o padrão de 2023,  corrigido elementos das rubricas

--- db/migrations/20230130200417_alterar_elementos_rubricas2023.php
+++ db/migrations/20230130200417_alterar_elementos_rubricas2023.php
@@ -41,32 +41,32 @@ class AlterarElementosRubricas2023 extends AbstractMigration
         /**
          * Pessoal do FUNDEB (Recursos: Até 30%)  -> Vencimentos e Salários - RPPS 
          */
-        $sql .= " UPDATE rhrubelemento SET rh23_codele = (SELECT O56_codele FROM orcelemento WHERE o56_elemento LIKE '3319011010100' and o56_anousu = 2023 LIMIT 1)
-        WHERE rh23_codele IN (COALESCE((SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011020100' LIMIT 1),(SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011010000' LIMIT 1))); ";
+        $sql .= " UPDATE rhrubelemento SET rh23_codele = COALESCE((SELECT O56_codele FROM orcelemento WHERE o56_elemento LIKE '3319011010100' and o56_anousu = 2023 LIMIT 1),(SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011010000' LIMIT 1))
+        WHERE rh23_codele IN (SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011020100' LIMIT 1); ";
 
         /**
          * Pessoal do FUNDEB (Recursos: Até 30%)  -> Vencimentos e Salários - RPPS 
          */
-        $sql .= " UPDATE rhrubelemento SET rh23_codele = (SELECT O56_codele FROM orcelemento WHERE o56_elemento LIKE '3319011010200' and o56_anousu = 2023 LIMIT 1)
-        WHERE rh23_codele IN (COALESCE((SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011020200' LIMIT 1),(SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011010000' LIMIT 1))); ";
+        $sql .= " UPDATE rhrubelemento SET rh23_codele = COALESCE((SELECT O56_codele FROM orcelemento WHERE o56_elemento LIKE '3319011010200' and o56_anousu = 2023 LIMIT 1),(SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011010000' LIMIT 1))
+        WHERE rh23_codele IN (SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011020200' LIMIT 1); ";
 
         /**
          * Pessoal de Cargo Efetivo (Vinculado ao RPPS), exceto FUNDEB  -> Vencimentos e Salários - RPPS 
          */
-        $sql .= " UPDATE rhrubelemento SET rh23_codele = (SELECT O56_codele FROM orcelemento WHERE o56_elemento LIKE '3319011010100' and o56_anousu = 2023 LIMIT 1)
-        WHERE rh23_codele IN (COALESCE((SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011030000' LIMIT 1),(SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011010000' LIMIT 1))); ";
+        $sql .= " UPDATE rhrubelemento SET rh23_codele = COALESCE((SELECT O56_codele FROM orcelemento WHERE o56_elemento LIKE '3319011010100' and o56_anousu = 2023 LIMIT 1),(SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011010000' LIMIT 1))
+        WHERE rh23_codele IN (SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011030000' LIMIT 1); ";
 
         /**
          * Pessoal de Cargo Efetivo (Vinculado ao INSS), exceto FUNDEB, exceto FUNDEB  -> Vencimentos e Salários - RPPS 
          */
-        $sql .= " UPDATE rhrubelemento SET rh23_codele = (SELECT O56_codele FROM orcelemento WHERE o56_elemento LIKE '3319011010200' and o56_anousu = 2023 LIMIT 1)
-        WHERE rh23_codele IN (COALESCE((SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011040000' LIMIT 1),(SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011010000' LIMIT 1))); ";
+        $sql .= " UPDATE rhrubelemento SET rh23_codele = COALESCE((SELECT O56_codele FROM orcelemento WHERE o56_elemento LIKE '3319011010200' and o56_anousu = 2023 LIMIT 1),(SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011010000' LIMIT 1))
+        WHERE rh23_codele IN (SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011040000' LIMIT 1); ";
 
         /**
          * Pessoal de Cargo Comissionado, exceto FUNDEB  -> Vencimentos e Salários - RGPS 
          */
-        $sql .= " UPDATE rhrubelemento SET rh23_codele = (SELECT O56_codele FROM orcelemento WHERE o56_elemento LIKE '3319011010200' and o56_anousu = 2023 LIMIT 1)
-        WHERE rh23_codele IN (COALESCE((SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011050000' LIMIT 1),(SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011010000' LIMIT 1))); ";
+        $sql .= " UPDATE rhrubelemento SET rh23_codele = COALESCE((SELECT O56_codele FROM orcelemento WHERE o56_elemento LIKE '3319011010200' and o56_anousu = 2023 LIMIT 1),(SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011010000' LIMIT 1))
+        WHERE rh23_codele IN (SELECT o56_codele FROM orcelemento WHERE o56_anousu = 2023 AND o56_elemento = '3319011050000' LIMIT 1); ";
 
 
         $this->execute($sql);
