9880c634af5dd9ce9be8db3d86fb34cae36edefb
@guilhermesouza.contass <guilhermesouza.contass@gmail.com>
2023-02-07 08:19:01 -0300

    OCSicomAM2023 MIGRATION

--- db/migrations/20230206201833_ocsicom2023migration.php
+++ db/migrations/20230206201833_ocsicom2023migration.php
@@ -33,6 +33,25 @@ class Ocsicom2023migration extends AbstractMigration
         ALTER TABLE ralic102023 ADD si180_tipoorcamento INT; 
         
         ALTER TABLE contratos102023 ALTER COLUMN si83_objetocontrato TYPE varchar(1000);
+
+        CREATE TABLE partlic102023(
+        si203_sequencial		 int8 NOT NULL default 0,
+        si203_tiporegistro			 int8 NOT NULL default 0,
+        si203_codorgao		 varchar(2) NOT NULL,
+        si203_codunidadesub		 varchar(8) NOT NULL,
+        si203_exerciciolicitacao			 int8 NOT NULL default 0,
+        si203_nroprocessolicitatorio	 int8 NOT NULL default 0,
+        si203_tipodocumento		 int8 NOT NULL default 0,
+        si203_nrodocumento		 varchar(14),
+        si203_mes		 int8 NOT NULL default 0,
+        si203_instit		 int8 default 0);
+            
+        CREATE SEQUENCE partlic102023_si203_sequencial_seq
+        INCREMENT 1
+        MINVALUE 1
+        MAXVALUE 9223372036854775807
+        START 1
+        CACHE 1;
         
         COMMIT;";
 
