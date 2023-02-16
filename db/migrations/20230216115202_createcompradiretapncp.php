6cf0dfbd3543a45b73ded11f8bbb1b8595dcdf7a
Mario <Mario@contassconsultoria.com.br>
2023-02-16 09:57:01 -0300

    adicionado migrations

---
+++ db/migrations/20230216115202_createcompradiretapncp.php
@@ -0,0 +1,23 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Createcompradiretapncp extends AbstractMigration
+{
+    public function up()
+    {
+        $sql = "
+            alter table pcproc add column pc80_numdispensa int8;
+            alter table pcproc add column pc80_dispvalor bool;
+            alter table pcproc add column pc80_orcsigiloso bool;
+            alter table pcproc add column pc80_subcontratacao bool;
+            alter table pcproc add column pc80_dadoscomplementares text;        
+        
+            INSERT INTO db_menu VALUES(32,(select id_item from db_itensmenu where descricao='PNCP'),51,28);
+
+            INSERT INTO db_itensmenu VALUES ((select max(id_item)+1 from db_itensmenu), 'Dispensa por Valor', 'Dispensa por Valor', 'com1_pncpdispensaporvalor001.php', 1, 1, 'Dispensa por Valor', 't');
+            INSERT INTO db_menu VALUES((select id_item from db_itensmenu where desctec like'%PNCP' and funcao = ' '),(select max(id_item) from db_itensmenu),1,28);
+            
+        ";
+    }
+}
