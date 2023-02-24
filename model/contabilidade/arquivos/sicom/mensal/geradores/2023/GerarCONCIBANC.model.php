0f37a0ae61e2e8e7239b76ee84938e1a6d07a4fe
Igor Afonso Oliveira Ruas <igor@contassconsultoria.com.br>
2023-02-24 17:07:31 +0000

    Merge branch 'oc19799' into 'master'
    Criando arquivo SICOM Concibanc
    
    See merge request contass/ecidade!3553
    
    (cherry picked from commit bf2546b3695fbf2a54badfad3c8d6a409fd74925)
    
    14d6abdd Criando arquivo SICOM Concibanc

--- model/contabilidade/arquivos/sicom/mensal/geradores/2023/GerarCONCIBANC.model.php
+++ model/contabilidade/arquivos/sicom/mensal/geradores/2023/GerarCONCIBANC.model.php
@@ -17,18 +17,68 @@ class GerarCONCIBANC extends GerarAM
      */
     public $iMes;
 
-    
+    /**
+     *
+     * Mes de referência
+     * @var Date
+     */
+    public $sDataInicial;
+
+    /**
+     *
+     * Mes de referência
+     * @var Date
+     */
+    public $sDataFinal;
+
+
     public function gerarDados()
     {
+
+
 
         // Arquivo será construido.
         $this->sArquivo = "CONCIBANC";
         $this->abreArquivo();
 
-        $aCSV['tiporegistro'] = '99';
-        $this->sLinha = $aCSV;
-        $this->adicionaLinha();
 
+
+        $sqlConcibanc102023  = "	    select k13_reduz, c61_codtce, si09_codorgaotce as si202_codorgao,";
+	    $sqlConcibanc102023 .= "               substr(fc_saltessaldo,41,13)::float8 as si202_vlsaldofinalcontabil ";
+        $sqlConcibanc102023 .= "    	from (";
+ 	    $sqlConcibanc102023 .= "              select k13_reduz, c61_codtce, si09_codorgaotce, ";
+	    $sqlConcibanc102023 .= "                     fc_saltessaldo(k13_reduz,'".$this->sDataInicial."','".$this->sDataFinal."',null," . db_getsession("DB_instit") . ") ";
+	    $sqlConcibanc102023 .= "              from   saltes ";
+	    $sqlConcibanc102023 .= "                     inner join conplanoexe   on k13_reduz = c62_reduz ";
+		$sqlConcibanc102023 .= "                                              and c62_anousu = ".db_getsession('DB_anousu')." ";
+		$sqlConcibanc102023 .= "                     inner join conplanoreduz on c61_anousu=c62_anousu and c61_reduz = c62_reduz and c61_instit = " . db_getsession("DB_instit") . " ";
+	    $sqlConcibanc102023 .= "                     inner join conplano      on c60_codcon = c61_codcon and c60_anousu=c61_anousu ";
+	    $sqlConcibanc102023 .= "                     left  join conplanoconta on c60_codcon = c63_codcon and c63_anousu=c60_anousu ";
+	    $sqlConcibanc102023 .= "                     left  join infocomplementaresinstit on si09_instit = c61_instit";
+        $sqlConcibanc102023 .= " and c60_codsis = 6 ";
+        $sqlConcibanc102023 .= "  ) as x ";
+
+        $rsConcibanc102023 = db_query($sqlConcibanc102023);
+       // db_criatabela($rsConcibanc102023);
+
+        if(pg_num_rows($rsConcibanc102023) < 0){
+            $aCSV['si202_tiporegistro'] = '99';
+            $this->sLinha = $aCSV;
+            $this->adicionaLinha();
+        }
+
+        if(pg_num_rows($rsConcibanc102023) > 0){
+            $aConcibanc102023 = db_utils::getCollectionByRecord($rsConcibanc102023, false, false, true);
+            foreach($aConcibanc102023 as $oConcibanc102023){
+                $aCSV['si202_tiporegistro']         = str_pad(10, 2, "0", STR_PAD_LEFT);
+                $aCSV['si202_codorgao']             = $this->padLeftZero($oConcibanc102023->si202_codorgao, 2);
+                $aCSV['si202_codctb']               = $oConcibanc102023->c61_codtce == null || $oConcibanc102023->c61_codtce == 0 ? $oConcibanc102023->k13_reduz : $oConcibanc102023->c61_codtce;
+                $aCSV['si202_vlsaldofinalextrato']  = $this->sicomNumberReal($oConcibanc102023->si202_vlsaldofinalcontabil, 2);
+                $aCSV['si202_vlsaldofinalcontabil'] = $this->sicomNumberReal($oConcibanc102023->si202_vlsaldofinalcontabil, 2);
+                $this->sLinha = $aCSV;
+                $this->adicionaLinha();
+            }
+        }
 
     }
 }
