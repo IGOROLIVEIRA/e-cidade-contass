1ad6f9cba37421124292b98cf41d9df974ae5712
Mario <Mario@contassconsultoria.com.br>
2023-03-28 14:40:55 -0300

    hotfix

--- forms/db_frmliclicita.php
+++ forms/db_frmliclicita.php
@@ -989,8 +989,8 @@ $lBloqueadoRegistroPreco = (empty($itens_lancados) ? $db_opcao : 3);
                 db_input('l20_liccomissao', 10, $Il20_liccomissao, true, 'hidden', $db_opcao, " onchange='js_pesquisal20_liccomissao(false);'")
 
                 ?>
-                <tr style="padding-bottom:70px important!;">
-                    <td nowrap title="<?= @$Tl20_equipepregao ?>" id="equipepregao">
+                <tr style="padding-bottom:70px important!;" id="equipepregao">
+                    <td nowrap title="<?= @$Tl20_equipepregao ?>">
                         <?
                         db_ancora(@$Ll20_equipepregao, "js_pesquisal20_equipepregao(true);", $db_opcao);
                         ?>
@@ -1408,6 +1408,8 @@ $lBloqueadoRegistroPreco = (empty($itens_lancados) ? $db_opcao : 3);
             document.getElementById("respAberProcesso").style.display = "none";
             document.getElementById("respEmissaoEdi").style.display = "none";
             document.getElementById("dataaber").style.display = "none";
+            document.getElementById("dataaberturapncp").style.display = "none";
+            document.getElementById("equipepregao").style.display = 'none';
 
 
             let listaNatureza = document.getElementById('l20_naturezaobjeto').options;
@@ -1439,7 +1441,6 @@ $lBloqueadoRegistroPreco = (empty($itens_lancados) ? $db_opcao : 3);
             document.form1.l20_recdocumentacao.style.display = 'none';
             document.form1.dtjs_l20_recdocumentacao.style.display = 'none';
             document.form1.l20_usaregistropreco.style.display = 'none';
-            document.form1.l20_equipepregao.style.display = 'none';
             document.form1.l20_local.style.display = 'none';
             document.form1.l20_aceitabilidade.style.display = 'none';
             document.getElementById("tipolicitacao").style.display = 'none';
@@ -1448,7 +1449,6 @@ $lBloqueadoRegistroPreco = (empty($itens_lancados) ? $db_opcao : 3);
             document.getElementById("numeroconvidado").style.display = 'none';
             document.getElementById("recdocumentacao").style.display = 'none';
             document.getElementById("usaregistropreco").style.display = 'none';
-            document.getElementById("equipepregao").style.display = 'none';
             document.getElementById("local").style.display = 'none';
             document.getElementById("aceitabilidade").style.display = 'none';
             document.getElementById("respAvaliaBens").style.display = "none";
@@ -1465,6 +1465,8 @@ $lBloqueadoRegistroPreco = (empty($itens_lancados) ? $db_opcao : 3);
             document.getElementById("respAberProcesso").style.display = '';
             document.getElementById("respEmissaoEdi").style.display = '';
             document.getElementById("dataaber").style.display = '';
+            document.getElementById("dataaberturapncp").style.display = '';
+            document.getElementById("equipepregao").style.display = '';
 
             let listaNatureza = document.getElementById('l20_naturezaobjeto').options;
 
