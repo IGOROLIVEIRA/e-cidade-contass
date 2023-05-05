b41e3bc475acdbff4ff2e78ccafe12f7103d0266
Mario <Mario@contassconsultoria.com.br>
2023-05-05 08:32:38 -0300

    hotfix

--- lic1_liclicita002.php
+++ lic1_liclicita002.php
@@ -208,9 +208,6 @@ if (isset($alterar)) {
 
     if ($sqlerro == false) {
 
-
-
-
         /*
             Validações dos membros da licitação
             48 - Convite
@@ -570,7 +567,8 @@ if (isset($alterar)) {
             $clliclicita->l20_categoriaprocesso     = $oPost->l20_categoriaprocesso;
             $clliclicita->l20_receita               = $oPost->l20_receita;
             $clliclicita->l20_dataaber              = $oPost->l20_dataaber;
-            $clliclicita->l20_recdocumentacao       = $oPost->l20_recdocumentacao;
+            $clliclicita->l20_datacria              = $oPost->l20_datacria;
+            $clliclicita->l20_recdocumentacao       = $oPost->l20_dataaberproposta;
 
             $clliclicita->alterar($l20_codigo, $descricao);
 
