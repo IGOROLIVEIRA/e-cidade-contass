fe7ec579b0a50b1f7185723fb6c33356697147a5
tarique vieira ramos <tariquevieira@gmail.com>
2023-05-10 09:15:23 -0300

    OC20204 adicionando pregão presencial

--- model/licitacao/PortalCompras/Provedor/LigadorClasses.model.php
+++ model/licitacao/PortalCompras/Provedor/LigadorClasses.model.php
@@ -5,7 +5,8 @@ class LigadorClasses
     public static function listaBindModalidades(): array
     {
         return [
-            '53' => 'PregaoFabrica'
+            '53' => 'PregaoFabrica',
+            '52' => 'PregaoFabrica'
        ];
     }
 }
