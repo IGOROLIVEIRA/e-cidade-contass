dea8a104beec8fe968e6ecdad3bf2aeaedf3dc2f
Rodrigo Cabral <rodrigo.cabral@quizlet.com>
2023-05-19 07:37:14 -0300

    Forca a formatacao para evitar problemas com o json_encode

--- app/Repositories/Tributario/Arrecadacao/ApiArrecadacaoPix/DTO/PixArrecadacaoPayloadDTO.php
+++ app/Repositories/Tributario/Arrecadacao/ApiArrecadacaoPix/DTO/PixArrecadacaoPayloadDTO.php
@@ -100,7 +100,7 @@ class PixArrecadacaoPayloadDTO implements IPixPayload
                 continue;
             }
 
-            if ($attribute === 'valorOriginalSolicitacao' && !is_float($value)) {
+            if ($attribute === 'valorOriginalSolicitacao') {
                 $value = number_format($value, 2, '.', '');
             }
 
