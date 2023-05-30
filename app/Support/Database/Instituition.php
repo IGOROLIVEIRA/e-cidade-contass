0618a9ef3ce577a8e3c5cffdf849d9682ae1fe05
Rodrigo Cabral <rodrigo.cabral@quizlet.com>
2023-05-29 21:49:36 -0300

    Corrige metodo

--- app/Support/Database/Instituition.php
+++ app/Support/Database/Instituition.php
@@ -4,11 +4,8 @@ namespace App\Support\Database;
 
 trait Instituition
 {
-    public function getInstituicaoByCnpj(string $cnpj = NULL): array
+    public function checkInstituicaoExists(string $cnpj): bool
     {
-        if (empty($cnpj)) {
-            return [];
-        }
-        return $this->fetchRow("select codigo from db_config where cgc = '{$cnpj}'");
+        return !!$this->fetchRow("select codigo from db_config where cgc = '{$cnpj}'");
     }
 }
