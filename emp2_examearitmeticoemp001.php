8e69a8e6c865eebd727acd221ddacaa0d2c9eaa9
dayvison <dayvison.marley@contassconsultoria.com.br>
2023-06-05 08:13:06 -0300

    20399- v3 - Solicitação de ajuste no exame aritmético

--- emp2_examearitmeticoemp001.php
+++ emp2_examearitmeticoemp001.php
@@ -83,10 +83,15 @@ include("dbforms/db_classesgenericas.php");
                 <td>
                     <select name="iTipo" id="iTipo">
                         <option value="">Todos</option>
-                        <option value="1">Saúde</option>
-                        <option value="2">Educação</option>
-                        <option value="3">Fundeb</option>
+                        <option value="1">Saúde 15%</option>
+                        <option value="9">Vinculados à Saúde</option>
+                        <option value="2">Educação 25%</option>                        
+                        <option value="5">Fundeb 70%</option>
+                        <option value="6">Fundeb 30%</option>
+                        <option value="8">Cide</option>
+                        <option value="7">Multas de Transito</option>
                         <option value="4">Geral</option>
+                        
                     </select>
                 </td>
             </tr>
