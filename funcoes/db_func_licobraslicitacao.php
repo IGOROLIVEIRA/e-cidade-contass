e21c9299fa5f65ec5f35d2f900cd9549c2a40b0a
Filipi <filipibarbosa.contass@gmail.com>
2023-06-22 14:12:05 -0300

    OC20540 ajustes na listagem das licitacoes que nao sao do sistema

--- funcoes/db_func_licobraslicitacao.php
+++ funcoes/db_func_licobraslicitacao.php
@@ -1,3 +1,2 @@
 <?
-$campos = "licobraslicitacao.obr07_sequencial,licobraslicitacao.obr07_processo,licobraslicitacao.obr07_exercicio,licobraslicitacao.obr07_objeto,licobraslicitacao.obr07_tipoprocesso,licobraslicitacao.obr07_instit,l44_descricao";
-?>
+$campos = "licobraslicitacao.obr07_sequencial,licobraslicitacao.obr07_processo,licobraslicitacao.obr07_exercicio,licobraslicitacao.obr07_objeto,licobraslicitacao.obr07_tipoprocesso,l44_descricao";
