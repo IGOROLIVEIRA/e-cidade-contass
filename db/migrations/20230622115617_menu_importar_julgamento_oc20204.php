2d938b5bb6e831a6c169e809b09305e410cd6158
tarique vieira ramos <tariquevieira@gmail.com>
2023-06-22 10:31:46 -0300

    OC20204 fix: corrigindo migration

--- db/migrations/20230622115617_menu_importar_julgamento_oc20204.php
+++ db/migrations/20230622115617_menu_importar_julgamento_oc20204.php
@@ -7,8 +7,11 @@ class MenuImportarJulgamentoOc20204 extends AbstractMigration
     public function up()
     {
         $sql = "
-        INSERT INTO db_itensmenu values ((select max(id_item)+1 from db_itensmenu),'Importar Julgamento','Importar Julgamento','lic1_importajulgamento001.php',1,1,'Importar Julgamento','t');
-        INSERT INTO db_menu VALUES(1818,(select max(id_item) from db_itensmenu),5,381);
+        BEGIN;
+            INSERT INTO db_itensmenu values ((select max(id_item)+1 from db_itensmenu),'Importar Julgamento','Importar Julgamento','lic1_importajulgamento001.php',1,1,'Importar Julgamento','t');
+            INSERT INTO db_menu VALUES(1818,(select max(id_item) from db_itensmenu),5,381);
+
+        COMMIT;
         ";
 
         $this->execute($sql);
