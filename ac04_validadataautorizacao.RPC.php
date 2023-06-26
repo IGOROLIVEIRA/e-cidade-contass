ec0e9c4342e8c199df16b969261d1ed4bb1812b2
Filipi <filipibarbosa.contass@gmail.com>
2023-06-26 14:50:24 -0300

    OC20521 correcao validacao data da autorizacao

--- ac04_validadataautorizacao.RPC.php
+++ ac04_validadataautorizacao.RPC.php
@@ -23,10 +23,11 @@ $oRetorno->status  = 1;
 $oRetorno->erro  = '';
 
 
-
 try {
-  $rsAcordo = db_query("select ac16_sequencial from acordo inner join cgm on cgm.z01_numcgm = acordo.ac16_contratado inner join db_depart on db_depart.coddepto = acordo.ac16_coddepto inner join db_depart as responsavel on responsavel.coddepto = acordo.ac16_deptoresponsavel inner join acordosituacao on acordosituacao.ac17_sequencial = acordo.ac16_acordosituacao where 1 = 1 and (select ac18_datafim from acordovigencia where ac18_acordoposicao = (select ac26_sequencial from acordoposicao where ac26_acordo = ac16_sequencial order by ac26_sequencial desc limit 1)) >= '$e54_emiss' and '$e54_emiss' >= (select ac18_datainicio from acordovigencia where ac18_acordoposicao = (select ac26_sequencial from acordoposicao where ac26_acordo = ac16_sequencial order by ac26_sequencial desc limit 1)) and ac16_sequencial = $oParam->ac16_sequencial  and ac16_instit = $iInstituicao and ac16_acordosituacao in (4) and ac16_origem in(1, 2, 3, 6)");
-  if (pg_num_rows($rsAcordo) == 0) {
+  $rsSituacaoAcordo = db_query("select ac16_acordosituacao from acordo where ac16_sequencial = $oParam->ac16_sequencial");
+  $ac16_acordosituacao = db_utils::fieldsMemory($rsSituacaoAcordo, 0)->ac16_acordosituacao;
+  $rsAcordo = db_query("select ac16_sequencial from acordo where (select ac18_datafim from acordovigencia where ac18_acordoposicao = (select ac26_sequencial from acordoposicao where ac26_acordo = ac16_sequencial order by ac26_sequencial desc limit 1)) >= '$e54_emiss' and '$e54_emiss' >= (select ac18_datainicio from acordovigencia where ac18_acordoposicao = (select ac26_sequencial from acordoposicao where ac26_acordo = ac16_sequencial order by ac26_sequencial desc limit 1)) and ac16_sequencial = $oParam->ac16_sequencial  and ac16_instit = $iInstituicao");
+  if (pg_num_rows($rsAcordo) == 0 && $ac16_acordosituacao != 1) {
     throw new Exception("Usuario: A data da autorização de empenho nao esta dentro do periodo de vigencia do contrato");
   }
 } catch (Exception $e) {
