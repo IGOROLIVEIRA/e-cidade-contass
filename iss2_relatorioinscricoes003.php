186e979c2730453ccf1904780299ddeac95d6a6f
Raimundo Barbosa <raimundo2704@gmail.com>
2023-08-25 12:11:52 -0300

    OC20067 - Inclusão do ini_set, sem ele o relatório parou de emitir

--- iss2_relatorioinscricoes003.php
+++ iss2_relatorioinscricoes003.php
@@ -145,7 +145,8 @@
 
 	# Include AgataAPI class
 	include_once('dbagata/classes/core/AgataAPI.class');
-	
+	ini_set("error_reporting","E_ALL & ~NOTICE");
+
 	# Instantiate AgataAPI
 	$clagata = new cl_dbagata("issqn/iss1_inscr003.agt");
 
