f2740acc5a5bbb79730e460645d25d1748c60b45
tarique vieira ramos <tariquevieira@gmail.com>
2023-09-20 14:15:30 -0300

    OC21032 refactor: extraindo medo mexcluir dotacoes

--- model/contrato/apostilamento/Command/SalvaDotacoesCommand.php
+++ model/contrato/apostilamento/Command/SalvaDotacoesCommand.php
@@ -6,6 +6,27 @@ class SalvaDotacoesCommand
     public function execute($dotacoes, $idAcordoItem)
     {
 
+        $this->excluiDotações($idAcordoItem);
+
+        foreach ($dotacoes as $dotacao) {
+
+            $oDaoAcordoItemDotacao = db_utils::getDao("acordoitemdotacao");
+             $oDaoAcordoItemDotacao->ac22_acordoitem = $idAcordoItem;
+             $oDaoAcordoItemDotacao->ac22_anousu     = db_getsession('DB_anousu');
+             $oDaoAcordoItemDotacao->ac22_valor      = (float)$dotacao->valor;
+             $oDaoAcordoItemDotacao->ac22_coddot     = $dotacao->dotacao;
+             $oDaoAcordoItemDotacao->ac22_quantidade = "" . $dotacao->quantidade . "";
+             $oDaoAcordoItemDotacao->incluir(null);
+             if ($oDaoAcordoItemDotacao->erro_status == 0) {
+                 $sErroMsg  = "Erro ao salvar item";
+                 $sErroMsg .= "Não foi possivel incluir dotacao ({$dotacao->dotacao})\n.{$oDaoAcordoItemDotacao->erro_msg}";
+                 throw new Exception($sErroMsg);
+             }
+        }
+    }
+
+    private function excluiDotações($idAcordoItem)
+    {
         $oDaoAcordoItemDotacaoReserva = db_utils::getDao("orcreservaacordoitemdotacao");
         $oDaoReserva                  = db_utils::getDao("orcreserva");
         $oDaoAcordoItemDotacao = db_utils::getDao("acordoitemdotacao");
@@ -36,22 +57,6 @@ class SalvaDotacoesCommand
                 $oDaoReserva->excluir($oDadosReserva->o84_orcreserva);
             }
             $oDaoAcordoItemDotacao->excluir($oDotacaoCadastrada->ac22_sequencial);
-        }
-
-        foreach ($dotacoes as $dotacao) {
-
-            $oDaoAcordoItemDotacao = db_utils::getDao("acordoitemdotacao");
-             $oDaoAcordoItemDotacao->ac22_acordoitem = $idAcordoItem;
-             $oDaoAcordoItemDotacao->ac22_anousu     = db_getsession('DB_anousu');
-             $oDaoAcordoItemDotacao->ac22_valor      = (float)$dotacao->valor;
-             $oDaoAcordoItemDotacao->ac22_coddot     = $dotacao->dotacao;
-             $oDaoAcordoItemDotacao->ac22_quantidade = "" . $dotacao->quantidade . "";
-             $oDaoAcordoItemDotacao->incluir(null);
-             if ($oDaoAcordoItemDotacao->erro_status == 0) {
-                 $sErroMsg  = "Erro ao salvar item";
-                 $sErroMsg .= "Não foi possivel incluir dotacao ({$dotacao->dotacao})\n.{$oDaoAcordoItemDotacao->erro_msg}";
-                 throw new Exception($sErroMsg);
-             }
         }
     }
 }
