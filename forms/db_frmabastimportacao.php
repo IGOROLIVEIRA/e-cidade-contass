248b12a032344a2f96262d96a7142659c7c59898
Filipi <filipibarbosa.contass@gmail.com>
2023-09-29 10:24:36 -0300

    OC17663 correcao na geracao e importacao do campo data do abastecimento

--- forms/db_frmabastimportacao.php
+++ forms/db_frmabastimportacao.php
@@ -120,14 +120,14 @@ if (isset($_POST["processar"])) {
 
             $cell = $objWorksheet->getCellByColumnAndRow(1, $row);
             $data = $cell->getValue();
+            
+            $data = explode("/", $data);
+            $data = $data[2] . "-" . $data[1] . "-" . $data[0];
 
             if ($data == "") {
                 break;
             }
         		
-            $data = DateTime::createFromFormat("m/d/Y" , $aDatas[$i][0]);
-            $data = $data->format('Y-m-d');
-
             $cell = $objWorksheet->getCellByColumnAndRow(2, $row);
             $val = $cell->getValue();
             $tamVal = strlen($val);
