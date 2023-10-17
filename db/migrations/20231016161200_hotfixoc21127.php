b9db87f1cb444c475542794ec04b45226366cbc0
antonio-i-sampaio <igorsantateresa@gmail.com>
2023-10-17 13:26:46 -0300

    Hotfix - OC21127

--- db/migrations/20231016161200_hotfixoc21127.php
+++ db/migrations/20231016161200_hotfixoc21127.php
@@ -6,8 +6,8 @@ class Hotfixoc21127 extends AbstractMigration
 {
     public function up(){
         $sql = "
-        update empenho.naturezabemservico set e101_codnaturezarendimento = 17006 where e101_resumo = 'Transporte de cargas, exceto os relacionados no código 8767';
-        update empenho.naturezabemservico set e101_codnaturezarendimento = 17007 where e101_resumo = 'Serviços de auxílio diagnóstico e terapia, patologia clínica, imagenologia, anatomia patológica e citopatológia, medi...';
+        update empenho.naturezabemservico set e101_resumo = 'Transporte de cargas, exceto os relacionados no código 8767', e101_descr = 'Transporte de cargas, exceto os relacionados no código 8767' where e101_codnaturezarendimento = 17006;
+        update empenho.naturezabemservico set e101_resumo = 'Serviços de auxílio diagnóstico e terapia, patologia clínica, imagenologia, anatomia patológica e citopatológia, medi...', e101_descr = 'Serviços de auxílio diagnóstico e terapia, patologia clínica, imagenologia, anatomia patológica e citopatológia, medicina nuclear e análises e patologias clínicas de que trata o art. 31.' where e101_codnaturezarendimento = 17007;
         ";
         $this->execute($sql);
     }
