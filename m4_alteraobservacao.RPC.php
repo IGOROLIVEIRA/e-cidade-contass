59203c117f590efbc37ed05bd70a6385843a092e
Filipi <filipibarbosa.contass@gmail.com>
2023-10-20 09:55:55 -0300

    OC20648 alteracao da observacao

--- m4_alteraobservacao.RPC.php
+++ m4_alteraobservacao.RPC.php
@@ -10,35 +10,51 @@ db_postmemory($_POST);
 $oJson             = new services_json();
 $oParam            =  json_decode(str_replace('\\', '', $_POST["json"]));
 
-$e54_emiss = implode('-', array_reverse(explode('/', "$oParam->e54_emiss")));
-$ac16_sequencial = $oParam->ac16_sequencial;
+$sequencial = $oParam->sequencial;
+$tabela = $oParam->tabela;
+$observacao = utf8_decode(db_stdClass::db_stripTagsJson($oParam->observacao));
+//$observacao = rawurldecode(mb_convert_encoding($oParam->observacao, "ISO-8859-1", "UTF-8"));//addslashes(stripslashes(chop($oParam->observacao)));
+//$observacao = nl2br(mb_convert_encoding($oParam->observacao, "ISO-8859-1", "UTF-8"));
+
 $iInstituicao = db_getsession("DB_instit");
 
 $oRetorno          = new stdClass();
 $oRetorno->status  = 1;
 $oRetorno->erro  = '';
 
-
 try {
 
-  $aTipos = 
+  switch ($tabela) {
 
-  $clsolicita = new cl_solicita;
+    case "solicita":
 
-  $sequencial = 118;
-  $rsSolicitacao = $clsolicita->sql_record($clsolicita->sql_query_file($sequencial,"pc10_resumo"));
-  $resumo = db_utils::fieldsMemory($rsSolicitacao, 0)->pc10_resumo;
-  $oRetorno->observacao = $resumo;
+        db_query("update solicita set pc10_resumo= '$observacao' where pc10_numero = $sequencial;");
+        break;
 
-  /*
-  $rsAcordo = db_query("select ac16_sequencial from acordo where (select ac18_datafim from acordovigencia where ac18_acordoposicao = (select ac26_sequencial from acordoposicao where ac26_acordo = ac16_sequencial order by ac26_sequencial desc limit 1)) >= '$e54_emiss' and '$e54_emiss' >= (select ac18_datainicio from acordovigencia where ac18_acordoposicao = (select ac26_sequencial from acordoposicao where ac26_acordo = ac16_sequencial order by ac26_sequencial limit 1)) and ac16_sequencial = $oParam->ac16_sequencial  and ac16_instit = $iInstituicao");
-  if (pg_num_rows($rsAcordo) == 0 && $ac16_acordosituacao != 1) {
-    throw new Exception("Usuario: A data da autorização de empenho nao esta dentro do periodo de vigencia do contrato");
-  }*/
+    case "pcproc":
+
+      db_query("update pcproc set pc80_resumo = '$observacao' where pc80_codproc = $sequencial;");
+      break;
+
+    case "empautoriza":
+
+      db_query("update empautoriza set e54_resumo = '$observacao' where e54_autori = $sequencial;");
+      break;
+
+    case "empempenho":
+
+      db_query("update empempenho set e60_resumo = '$observacao' where e60_numemp = $sequencial;");
+      break;
+
+    case "matordem":
+
+      db_query("update matordem set m51_obs = '$observacao' where m51_codordem = $sequencial;");
+      break;
+  }
 
 } catch (Exception $e) {
   $oRetorno->erro   = urlencode($e->getMessage());
   $oRetorno->status = 2;
 }
 
-echo json_encode($oRetorno);
+echo $oJson->encode($oRetorno);
--- m4_pesquisaobservacao.RPC.php
+++ m4_pesquisaobservacao.RPC.php
@@ -33,21 +33,29 @@ try {
     case "pcproc":
 
         $clpcproc = new cl_pcproc;
-        $rsSolicitacao = $clpcproc->sql_record($clpcproc->sql_query_file($sequencial,"pc10_resumo"));
-        $oRetorno->observacao = urlencode(db_utils::fieldsMemory($rsSolicitacao, 0)->pc10_resumo);
+        $rsProcesso = $clpcproc->sql_record($clpcproc->sql_query_file($sequencial,"pc80_resumo"));
+        $oRetorno->observacao = urlencode(db_utils::fieldsMemory($rsProcesso, 0)->pc80_resumo);
         break;
 
     case "empautoriza":
-        echo "i equals 2";
+
+        $clempautoriza = new cl_empautoriza;
+        $rsAutorizacao = $clempautoriza->sql_record($clempautoriza->sql_query_file($sequencial,"e54_resumo"));
+        $oRetorno->observacao = urlencode(db_utils::fieldsMemory($rsAutorizacao, 0)->e54_resumo);
         break;
-    case "empautoriza":
-        echo "i equals 2";
-        break;
+
     case "empempenho":
-        echo "i equals 2";
+
+        $clempempenho = new cl_empempenho;
+        $rsEmpempenho = $clempempenho->sql_record($clempempenho->sql_query_file($sequencial,"e60_resumo"));
+        $oRetorno->observacao = urlencode(db_utils::fieldsMemory($rsEmpempenho, 0)->e60_resumo);
         break;
+
     case "matordem":
-        echo "i equals 2";
+
+        $clmatordem = new cl_matordem;
+        $rsOrdem = $clmatordem->sql_record($clmatordem->sql_query_file($sequencial,"m51_obs"));
+        $oRetorno->observacao = urlencode(db_utils::fieldsMemory($rsOrdem, 0)->m51_obs);
         break;
   }
 
