b16ac7ce7dcd1efe82ef1a1db3844af9eff67212
Rodrigo Cabral <rodrigo.cabral@quizlet.com>
2023-11-08 09:24:23 -0300

    Cria método helper para remover acentuacao das palavras

---
+++ app/Support/String/StringHelper.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace App\Support\String;
+
+class StringHelper
+{
+    public static function removeAccent(string $string): string
+    {
+        $accents = [
+            'á' => 'a', 'à' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a',
+            'é' => 'e', 'è' => 'e', 'ê' => 'e', 'ë' => 'e',
+            'í' => 'i', 'ì' => 'i', 'î' => 'i', 'ï' => 'i',
+            'ó' => 'o', 'ò' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ø' => 'o',
+            'ú' => 'u', 'ù' => 'u', 'û' => 'u', 'ü' => 'u',
+            'ç' => 'c',
+            'ñ' => 'n',
+            'ı' => 'y', 'ÿ' => 'y',
+        ];
+
+        return strtr($string, $accents);
+    }
+}
