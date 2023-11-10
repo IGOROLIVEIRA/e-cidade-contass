ef2ebcbc82c25ef820950d1ecf18de70ef69ab58
Joalisson Barros <joalissonricardo@gmail.com>
2023-11-09 21:55:22 -0300

    refactor(config): Modifica o FrontController

--- FrontController.php
+++ FrontController.php
@@ -1,7 +1,17 @@
 <?php
 
-if (preg_match('/[^?]*api\/v\d\//', $_SERVER['REQUEST_URI'])) {
-  return require_once 'api/api.php';
+try {
+
+    if (preg_match('/[^?]*api/', $_SERVER['REQUEST_URI'])) {
+        return require_once 'public/index.php';
+    }
+
+    return require('app.php');
+} catch (Exception $e) {
+    if (config('app.debug')) {
+        throw $e;
+    }
+    //@todo - verificar a possíbilidade de enviar o erro para o monitoramento
+    logger()->emergency($e->getMessage(), $e->getTrace());
 }
 
-return require('app.php');
