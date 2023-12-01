64fdd1b8de2f582765e45c01c3ff5bac7dd723a1
Raimundo Barbosa <raimundo2704@gmail.com>
2023-12-01 15:04:02 -0300

    OC21467 - Alteração do schema e o uso de trait Instituition

--- db/migrations/20231127180808_oc21467.php
+++ db/migrations/20231127180808_oc21467.php
@@ -1,33 +1,35 @@
 <?php
 
-use Phinx\Migration\AbstractMigration;
+use App\Support\Database\AsFunction;
+use App\Support\Database\Instituition;
+use Classes\PostgresMigration;
 
-class Oc21467 extends AbstractMigration
+class Oc21467 extends PostgresMigration
 {
+    use AsFunction;
+    use Instituition;
+
+	public const PMPIRAPORA 	= '23539463000121';
+	public const PMBURITIZEIRO	= '18279067000172';
+
     public function up()
     {		
-		//Cria a tabela autoriza_cgm_excluipgtoparcial, para inclusão dos numcgm que será autorizado na rotina arr4_excluiPagamentoParcial001.php
-		$this->execute("CREATE TABLE IF NOT EXISTS issqn.autorizacgmexcluirpgtoparcial (id_usuario int8 NOT NULL DEFAULT 0, 
-						CONSTRAINT autorizacgmexcluirpgtoparcial_idusuario_pk PRIMARY KEY (id_usuario));");
+		//Cria a tabela autorizausuarioexcluirpgtoparcial, para inclusão dos id_usuario que será autorizado na rotina arr4_excluiPagamentoParcial001.php
+		$this->execute("CREATE TABLE IF NOT EXISTS arrecadacao.autorizausuarioexcluirpgtoparcial (id_usuario int8 NOT NULL DEFAULT 0, 
+						CONSTRAINT autorizausuarioexcluirpgtoparcial_idusuario_pk PRIMARY KEY (id_usuario));");
 
-		$sqlPmPirapora 		= $this->query("select 1 from db_config where prefeitura = true and db21_codcli = (58)");
-		$resPmPirapora		= $sqlPmPirapora->fetchAll(\PDO::FETCH_ASSOC);
-
-		if (!empty($resPmPirapora)){
-			$this->execute("INSERT INTO autorizacgmexcluirpgtoparcial VALUES (2050);");
-			$this->execute("INSERT INTO autorizacgmexcluirpgtoparcial VALUES (2463);");
-			$this->execute("INSERT INTO autorizacgmexcluirpgtoparcial VALUES (2086);");
-			$this->execute("INSERT INTO autorizacgmexcluirpgtoparcial VALUES (2046);");
-			$this->execute("INSERT INTO autorizacgmexcluirpgtoparcial VALUES (2766);");
+		if (!empty($this->checkInstituicaoExists(self::PMPIRAPORA))){
+			$this->execute("INSERT INTO autorizausuarioexcluirpgtoparcial VALUES (2050);");
+			$this->execute("INSERT INTO autorizausuarioexcluirpgtoparcial VALUES (2463);");
+			$this->execute("INSERT INTO autorizausuarioexcluirpgtoparcial VALUES (2086);");
+			$this->execute("INSERT INTO autorizausuarioexcluirpgtoparcial VALUES (2046);");
+			$this->execute("INSERT INTO autorizausuarioexcluirpgtoparcial VALUES (2766);");
 		}
 
-		$sqlPmBuritizeiro 	= $this->query("select 1 from db_config where prefeitura = true and db21_codcli = (89)");
-		$resPmBuritizeiro	= $sqlPmBuritizeiro->fetchAll(\PDO::FETCH_ASSOC);
-
-		if (!empty($resPmBuritizeiro)){
-			$this->execute("INSERT INTO autorizacgmexcluirpgtoparcial VALUES (1163);");
-			$this->execute("INSERT INTO autorizacgmexcluirpgtoparcial VALUES (1164);");
-			$this->execute("INSERT INTO autorizacgmexcluirpgtoparcial VALUES (1193);");
+		if (!empty($this->checkInstituicaoExists(self::PMBURITIZEIRO))){
+			$this->execute("INSERT INTO autorizausuarioexcluirpgtoparcial VALUES (1163);");
+			$this->execute("INSERT INTO autorizausuarioexcluirpgtoparcial VALUES (1164);");
+			$this->execute("INSERT INTO autorizausuarioexcluirpgtoparcial VALUES (1193);");
 		}
     }
 }
