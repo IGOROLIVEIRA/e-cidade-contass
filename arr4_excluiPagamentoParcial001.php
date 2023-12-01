5bd2eb61312fb62fb0f8ddc0644268edc0e65023
Raimundo Barbosa <raimundo2704@gmail.com>
2023-12-01 08:46:11 -0300

    OC21467 - Liberação de rotina de exclusão de pagamento parcial

--- arr4_excluiPagamentoParcial001.php
+++ arr4_excluiPagamentoParcial001.php
@@ -1,29 +1,4 @@
 <?php
-/*
- *     E-cidade Software Publico para Gestao Municipal
- *  Copyright (C) 2014  DBSeller Servicos de Informatica
- *                            www.dbseller.com.br
- *                         e-cidade@dbseller.com.br
- *
- *  Este programa e software livre; voce pode redistribui-lo e/ou
- *  modifica-lo sob os termos da Licenca Publica Geral GNU, conforme
- *  publicada pela Free Software Foundation; tanto a versao 2 da
- *  Licenca como (a seu criterio) qualquer versao mais nova.
- *
- *  Este programa e distribuido na expectativa de ser util, mas SEM
- *  QUALQUER GARANTIA; sem mesmo a garantia implicita de
- *  COMERCIALIZACAO ou de ADEQUACAO A QUALQUER PROPOSITO EM
- *  PARTICULAR. Consulte a Licenca Publica Geral GNU para obter mais
- *  detalhes.
- *
- *  Voce deve ter recebido uma copia da Licenca Publica Geral GNU
- *  junto com este programa; se nao, escreva para a Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
- *  02111-1307, USA.
- *
- *  Copia da licenca no diretorio licenca/licenca_en.txt
- *                                licenca/licenca_pt.txt
- */
 
 require_once("libs/db_stdlib.php");
 require_once("libs/db_conecta.php");
@@ -50,12 +25,13 @@ include_once("libs/db_utils.php");
 <body bgcolor="#cccccc" onload="js_pesquisaAbatimento()">
 <br><br>
 <?php
-$cldb_config = new cl_db_config;
-$rsConfig = $cldb_config->sql_record($cldb_config->sql_query_file(db_getsession('DB_instit'),"db21_codcli"));
-$oConfig  = db_utils::fieldsMemory($rsConfig,0);
-$arrayusuarios = array('2050','2463','2086','2046','2766');
-if (db_getsession("DB_id_usuario") == 1 || ( $oConfig->db21_codcli == Instituicao::COD_CLI_PMPIRAPORA && 
-	in_array(db_getsession("DB_id_usuario"), $arrayusuarios) ) ) {
+$cldb_config 	= new cl_db_config;
+$rsConfig 		= $cldb_config->sql_record($cldb_config->sql_query_file(db_getsession('DB_instit'),"db21_codcli"));
+$oConfig  		= db_utils::fieldsMemory($rsConfig,0);
+$sqlUsuarios 	= db_query("select 1 from autorizacgmexcluirpgtoparcial where id_usuario =" .db_getsession("DB_id_usuario"));
+$resUsuarios	= pg_num_rows($sqlUsuarios);
+
+if ( db_getsession("DB_id_usuario") == 1 || ( $resUsuarios > 0) ) {
 ?>
 <div align="center">
 <fieldset style="width: 300px;">
