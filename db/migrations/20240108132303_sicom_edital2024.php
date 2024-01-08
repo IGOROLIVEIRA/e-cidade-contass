ed83362296a21dd751f2ddfc4c9c40177eadfb94
Mario <Mario@contassconsultoria.com.br>
2024-01-08 10:24:22 -0300

    SicomEdital2024 - Ralic

---
+++ db/migrations/20240108132303_sicom_edital2024.php
@@ -0,0 +1,52 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class SicomEdital2024 extends AbstractMigration
+{
+
+    public function up()
+    {
+        $sql = "
+                ALTER TABLE ralic102024
+                ALTER COLUMN si180_emailcontato TYPE INTEGER USING si180_emailcontato::INTEGER;
+
+                alter table ralic102024 add column si180_emailcontato varchar(200);
+
+                alter table ralic112024 add column si181_utilizacaoplanilhamodelo int8;
+
+                CREATE SEQUENCE ideedital2024_si186_sequencial_seq
+                INCREMENT 1
+                MINVALUE 1
+                MAXVALUE 9223372036854775807
+                START 1
+                CACHE 1;
+
+                CREATE SEQUENCE ralic102024_si180_sequencial_seq
+                INCREMENT 1
+                MINVALUE 1
+                MAXVALUE 9223372036854775807
+                START 1
+                CACHE 1;
+
+
+                CREATE SEQUENCE ralic112024_si181_sequencial_seq
+                INCREMENT 1
+                MINVALUE 1
+                MAXVALUE 9223372036854775807
+                START 1
+                CACHE 1;
+
+
+
+                CREATE SEQUENCE ralic122024_si182_sequencial_seq
+                INCREMENT 1
+                MINVALUE 1
+                MAXVALUE 9223372036854775807
+                START 1
+                CACHE 1;
+        ";
+
+        $this->execute($sql);
+    }
+}
