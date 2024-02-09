1664aa495a06130ae2b0bcb08e8cd522aaee8756
Raimundo Barbosa <raimundo2704@gmail.com>
2024-02-09 15:59:04 -0300

    OC21396 - Correção identação da migration

--- db/migrations/20240126084900_oc21396.php
+++ db/migrations/20240126084900_oc21396.php
@@ -5,9 +5,9 @@ use Classes\PostgresMigration;
 class Oc21396 extends PostgresMigration
 {
     public function up()
-    {	
-		//Cria a tabela issconfiguracaogruposervicousuario, para inclusão dos cgm para permissão de alteração da descrição da atividade
-		$this->execute("CREATE TABLE IF NOT EXISTS issqn.issconfiguracaogruposervicousuario (id_usuario int8 NOT NULL, 
-                        CONSTRAINT issconfiguracaogruposervicousuario_id_usuario_pk PRIMARY KEY (id_usuario));");     
+    {
+        //Cria a tabela issconfiguracaogruposervicousuario, para inclusão dos cgm para permissão de alteração da descrição da atividade
+        $this->execute("CREATE TABLE IF NOT EXISTS issqn.issconfiguracaogruposervicousuario (id_usuario int8 NOT NULL,
+                        CONSTRAINT issconfiguracaogruposervicousuario_id_usuario_pk PRIMARY KEY (id_usuario));");
     }
 }
