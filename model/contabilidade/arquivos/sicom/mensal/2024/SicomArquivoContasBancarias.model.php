dc9e3f853b2d958eb5b9ddcce61b83d04fbfd629
Elvio A. Amaral <elvio@contassconsultoria.com.br>
2024-03-26 14:51:58 -0300

    OC 22107 - Novas correcoes do arquivo CTB, referente a reestruturacao do arquivo.

--- model/contabilidade/arquivos/sicom/mensal/2024/SicomArquivoContasBancarias.model.php
+++ model/contabilidade/arquivos/sicom/mensal/2024/SicomArquivoContasBancarias.model.php
@@ -133,24 +133,26 @@ class SicomArquivoContasBancarias extends SicomArquivoBase implements iPadArquiv
         */
         $aCtb20Agrupado = array();
         $aCtb21 = array();
-        $aVerficador = array();
 
         foreach ($aBancosAgrupados as $oContaAgrupada) {
 
             foreach ($oContaAgrupada->contas as $oConta) {
 
+                $codtce = false;
+
                 if (!empty($oConta->codtce)){
                     $oConta->codctb = $oConta->codtce;
+                    $codtce = true;
                 }
 
                 if (($oConta->codtce != $codTce20) || empty($oConta->codtce)){
 
-                    $sSql20Fonte = $cCtb20->sql_Reg20Fonte($oConta->codctb, db_getsession("DB_anousu"), $mes);
+                    $sSql20Fonte = $cCtb20->sql_Reg20Fonte($oConta->codctb, db_getsession("DB_anousu"), $mes, $codtce);
                     $rsReg20Fonte = $cCtb20->sql_record($sSql20Fonte) or die($sSql20Fonte);
 
                     for ($iCont20 = 0; $iCont20 < pg_num_rows($rsReg20Fonte); $iCont20++) {
 
-                        list($iFonte, $sHash20, $oCtb20, $aCtb20Agrupado) = $this->gerarRegistro20($rsReg20Fonte, $iCont20, $oConta, $aCtb20Agrupado, $oContaAgrupada);
+                        list($iFonte, $sHash20, $oCtb20, $aCtb20Agrupado) = $this->gerarRegistro20($rsReg20Fonte, $iCont20, $oConta, $aCtb20Agrupado, $oContaAgrupada, $codtce);
 
                         if (isset($aCtb21[$sHash20])) {
                             continue;
@@ -161,7 +163,7 @@ class SicomArquivoContasBancarias extends SicomArquivoBase implements iPadArquiv
                          */
                         $clDeParaFonte = new DeParaRecurso;
 
-                        $rsMovi21 = $cCtb21->sql_Reg21($this->sDataFinal, $ano, $mes, $oCtb20->si96_codctb, $iFonte, $clDeParaFonte, $instit);
+                        $rsMovi21 = $cCtb21->sql_Reg21($this->sDataFinal, $ano, $mes, $oCtb20->si96_codctb, $iFonte, $clDeParaFonte, $instit, $codtce);
 
                         $aCtb21[$sHash20] = db_utils::fieldsMemory($rsMovi21, 0);
 
@@ -355,7 +357,7 @@ class SicomArquivoContasBancarias extends SicomArquivoBase implements iPadArquiv
      * @param $oContaAgrupada
      * @return array
      */
-    public function gerarRegistro20($rsReg20Fonte, int $iCont20, $oConta, array $aCtb20Agrupado, $oContaAgrupada): array
+    public function gerarRegistro20($rsReg20Fonte, int $iCont20, $oConta, array $aCtb20Agrupado, $oContaAgrupada, $codtce): array
     {
         $clDeParaFonte = new DeParaRecurso;
 
@@ -363,6 +365,10 @@ class SicomArquivoContasBancarias extends SicomArquivoBase implements iPadArquiv
         $codCtb = empty($codCtbReg20->c61_codtce) ? $codCtbReg20->c61_reduz : $codCtbReg20->c61_codtce;
 
         $iFonte = db_utils::fieldsMemory($rsReg20Fonte, $iCont20)->fontemovimento;
+
+        if (strlen($iFonte) == 4){
+            $iFonte = db_utils::fieldsMemory($rsReg20Fonte, $iCont20)->o15_codtri;
+        }
         $iFonte = substr($clDeParaFonte->getDePara($iFonte), 0, 7);
 
         $sinalSaldoInicial = db_utils::fieldsMemory($rsReg20Fonte, $iCont20)->nat_vlr_si;
@@ -375,6 +381,10 @@ class SicomArquivoContasBancarias extends SicomArquivoBase implements iPadArquiv
         $saldoFinal = $sinalSaldoFinal == 'C' ? $saldoFinal *-1 : $saldoFinal;
 
         $sHash20 = $codCtb . $iFonte;
+
+        if ($codtce){
+            $sHash20 = $oConta->codtce . $iFonte;
+        }
 
         if (!$aCtb20Agrupado[$sHash20]) {
 
