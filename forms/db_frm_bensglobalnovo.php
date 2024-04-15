2bbb188141839daa25fc32f33d89445f493ee950
marcus <marcus.peixoto@contassconsultoria.com.br>
2024-04-15 10:08:10 -0300

    OC20702 - Botão Inlcuir so é inabilitado, apos preencher completamente o formulario

--- forms/db_frm_bensglobalnovo.php
+++ forms/db_frm_bensglobalnovo.php
@@ -1020,9 +1020,14 @@ function js_mostraDepreciacao1(chave1, chave2) {
 /** ***********************************************************************************************************
  * Salva os Dados do Formulário
  */
+
+function js_retornoBuscaSalvar(response) {
+   
+    document.getElementById('db_opcao').disabled = true;
+}
 function salvarDados() {
 
-  document.getElementById('db_opcao').disabled = true;
+  
 
    var url     = 'pat1_benslotenovo.RPC.php';
    var oObject = new Object();
