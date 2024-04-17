dd191b7eb4d171d627fa6d9076f130c9e85b3937
marcus <marcus.peixoto@contassconsultoria.com.br>
2024-04-17 14:27:59 -0300

    hotfix_OC22232  fix - Removendo uma variavel que estava exibindo na tela

--- forms/db_frmveicmanutcstitens.php
+++ forms/db_frmveicmanutcstitens.php
@@ -152,9 +152,6 @@ db_app::load("estilos.css, grid.style.css");
                                     <strong>Hora:</strong>
                                     <?php db_input('ve62_hora', 30, $Ive62_hora, true, 'time', 1, '', '', '', '','width:30%;', null);?>
                                 </td>
-                                ve62_hora
-                                $Ive62_hora
-
                             </tr>
 
 
